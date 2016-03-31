a663e68a2e371222e88a4f15930ad869b41dbc60
shakuu <todorov.h@gmail.com>
2016-03-31 17:37:21 +0300

    Homework Update
    Add New Project

---
+++ ConsoleIO/CirclePA/CirclePA.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CirclePA
+{
+    class CirclePA
+    {
+        static void Main(string[] args)
+        {
+            double circleR = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0} {1}",
+                (2 * Math.PI * circleR).ToString("0.00"),
+                (Math.PI * Math.Pow(circleR,2)).ToString("0.00"));
+        }
+    }
+}
