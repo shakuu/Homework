e2ccae17accd94ea3e0aa62198990d929c2f92e4
shakuu <todorov.h@gmail.com>
2016-04-03 06:14:33 +0300

    hw update

---
+++ 6. Loops/Calculate3/Calculate3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate3
+{
+    class Program
+    {
+        static void Main()
+        {
+            double numbN = double.Parse(Console.ReadLine());
+            double numbK = double.Parse(Console.ReadLine());
+
+            double result = 1;
+
+            for (int i = 1; i < numbN - numbK + 1; i++)
+            {
+
+                result *= (i + (numbN - (numbN - numbK))) / i;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
