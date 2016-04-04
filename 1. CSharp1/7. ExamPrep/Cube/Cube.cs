09549136e5ad1cf62403d2750daf945a44a1a177
shakuu <todorov.h@gmail.com>
2016-04-04 04:43:08 +0300

    1

---
+++ 1. CSharp1/7. ExamPrep/Cube/Cube.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube
+{
+    class Cube
+    {
+        static void Main()
+        {
+
+            Console.WindowWidth = 50;
+            Console.WindowHeight = 30;
+            int cube = int.Parse(Console.ReadLine());
+
+            Console.BufferHeight =  cube * cube;
+            Console.BufferWidth =  cube * cube;
+            
+
+            //PRINT FACE
+            for (int row = cube - 1; row < 2 * cube - 1; row++)
+            {
+                //TOP-BOT ROW
+                if (row == cube - 1 || row == 2 * cube - 2)
+                {
+                    Console.SetCursorPosition(0, row);
+                    for (int col = 0; col < cube; col++)
+                    {
+                        Console.Write(":");
+                    }
+                }//END TOP-BOT ROW
+                else//FILLER
+                {
+                    Console.SetCursorPosition(0, row);
+                    Console.Write(":");
+                    for (int col = 1; col < cube - 1; col++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(":");
+                }
+            }//END FACE
+
+            //PRINT TOP
+            for (int row = 0; row < cube - 1; row++)
+            {
+                //TOP
+                if (row == 0)
+                {
+                    for (int col = 2 * cube - 2; col > cube - 2; col--)
+                    {
+                        Console.SetCursorPosition(col, row);
+                        Console.Write(":");
+                    }
+                }//END TOP
+                else //FILLER
+                {
+                    Console.SetCursorPosition((2 * cube - 2) - row, row);
+                    Console.Write(":");
+                    for (int col = (2 * cube - 1) - row - 2; col > cube - row - 1; col--)
+                    {
+                        Console.SetCursorPosition(col, row);
+                        Console.Write("/");
+                    }
+                    Console.SetCursorPosition((2 * cube - 1) - row - cube, row);
+                    Console.Write(":");
+                }
+            }//END TOP
+
+            //PRINT SIDE - TOP
+            for (int row = 1; row < cube; row++)
+            {
+                if (row == 1)
+                {
+                    Console.SetCursorPosition(2 * cube - 2, row);
+                    Console.Write(":");
+                }
+                else
+                {
+                    Console.SetCursorPosition(2 * cube - 2, row);
+                    Console.Write(":");
+
+                    for (int col = 1; col < row; col++)
+                    {
+                        Console.SetCursorPosition(2 * cube - 2 - col, row);
+                        Console.Write("X");
+                    }
+                }
+            }//END SIDE TOP
+
+            //PRINT SIDE BOT
+            int modifier = 2;
+            for (int row = cube; row < 2 * cube - 2; row++)
+            {
+                if ( row != 2*cube-3)
+                {
+                    Console.SetCursorPosition(cube, row);
+                    for( int col = cube; col < 2*cube-1-modifier; col++)
+                    {
+                        Console.Write("X");
+                    }
+                    Console.Write(":");
+                }
+                else
+                {
+                    Console.SetCursorPosition(cube, row);
+                    Console.Write(":");
+                }
+                modifier++;
+            }
+        }
+    }
+}
--- 1. CSharp1/7. ExamPrep/CubeArray/CubeArray.cs
+++ 1. CSharp1/7. ExamPrep/CubeArray/CubeArray.cs
@@ -3,22 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Cube
 {
-    class Program
+    class Cube
     {
         static void Main()
         {
 
-            Console.WindowWidth = 50;
-            Console.WindowHeight = 30;
-            int cube = int.Parse(Console.ReadLine());
+            long cube1 = long.Parse(Console.ReadLine());
+            int cube = Convert.ToInt32(cube1);
+            int cubeSize = cube * 2 - 1;
 
-            Console.BufferHeight =  cube * cube;
-            Console.BufferWidth =  cube * cube;
-            
+            string[,] cubeArray = new string[cubeSize, cubeSize];
 
+     
             //PRINT FACE
             for (int row = cube - 1; row < 2 * cube - 1; row++)
             {
@@ -28,87 +28,93 @@ namespace Cube
                     Console.SetCursorPosition(0, row);
                     for (int col = 0; col < cube; col++)
                     {
-                        Console.Write(":");
+                        cubeArray[col, row] = ":";
                     }
                 }//END TOP-BOT ROW
                 else//FILLER
                 {
-                    Console.SetCursorPosition(0, row);
-                    Console.Write(":");
+                    cubeArray[0, row] = ":";
                     for (int col = 1; col < cube - 1; col++)
                     {
-                        Console.Write(" ");
+                        cubeArray[col, row] = " ";
                     }
-                    Console.Write(":");
+                    cubeArray[cube - 1, row] = ":";
                 }
             }//END FACE
 
-            //PRINT TOP
+            ////PRINT TOP
             for (int row = 0; row < cube - 1; row++)
             {
                 //TOP
                 if (row == 0)
                 {
-                    for (int col = 2 * cube - 2; col > cube - 2; col--)
+                    for (int col = cube-1; col < cube*2 - 1; col++)
                     {
-                        Console.SetCursorPosition(col, row);
-                        Console.Write(":");
+                        cubeArray[ col, row] = ":";
                     }
                 }//END TOP
                 else //FILLER
                 {
-                    Console.SetCursorPosition((2 * cube - 2) - row, row);
-                    Console.Write(":");
+                    cubeArray[(2 * cube - 2) - row, row ] = ":"; 
                     for (int col = (2 * cube - 1) - row - 2; col > cube - row - 1; col--)
                     {
-                        Console.SetCursorPosition(col, row);
-                        Console.Write("/");
+                        cubeArray[col, row] = "/";
                     }
-                    Console.SetCursorPosition((2 * cube - 1) - row - cube, row);
-                    Console.Write(":");
+                    cubeArray[ (2 * cube - 1) - row - cube , row] = ":";
                 }
             }//END TOP
 
-            //PRINT SIDE - TOP
+            ////PRINT SIDE - TOP
             for (int row = 1; row < cube; row++)
             {
                 if (row == 1)
                 {
-                    Console.SetCursorPosition(2 * cube - 2, row);
-                    Console.Write(":");
+                    cubeArray[2 * cube - 2, row] = ":";
                 }
                 else
                 {
-                    Console.SetCursorPosition(2 * cube - 2, row);
-                    Console.Write(":");
-
+                    cubeArray[2 * cube - 2, row] = ":";
+    
                     for (int col = 1; col < row; col++)
                     {
-                        Console.SetCursorPosition(2 * cube - 2 - col, row);
-                        Console.Write("X");
+                        cubeArray[2 * cube - 2 - col, row] = "X";
                     }
                 }
             }//END SIDE TOP
 
-            //PRINT SIDE BOT
+            ////PRINT SIDE BOT
             int modifier = 2;
             for (int row = cube; row < 2 * cube - 2; row++)
             {
-                if ( row != 2*cube-3)
+                if (row != 2 * cube - 3)
                 {
-                    Console.SetCursorPosition(cube, row);
-                    for( int col = cube; col < 2*cube-1-modifier; col++)
+
+                    
+                    for (int col = cube; col < 2 * cube - 1 - modifier; col++)
                     {
-                        Console.Write("X");
+                        cubeArray[col, row] = "X";
                     }
-                    Console.Write(":");
+                    cubeArray[2 * cube - 1 - modifier, row] = ":";
                 }
                 else
                 {
-                    Console.SetCursorPosition(cube, row);
-                    Console.Write(":");
+                    cubeArray[cube, row] = ":";
                 }
                 modifier++;
+            }
+
+
+            for ( int x = 0; x< cube*2-1; x++)
+            {
+                for(int y = 0; y < cube*2-1; y++)
+                {
+                    if (cubeArray[y, x] == null)
+                    {
+                        cubeArray[y, x] = " ";
+                    }
+                    Console.Write(cubeArray[y, x]);
+                }
+                Console.WriteLine();
             }
         }
     }
