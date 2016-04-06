33fb5a42c7f8496e102dcccee43f5a1062052b35
shakuu <todorov.h@gmail.com>
2016-04-06 22:28:52 +0300

    modify bit update

---
+++ 1. CSharp1/3. Homework-Operators-and-Expressions/ModifyBitv3/ModifyBitv3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ModifyBitv3
+{
+    class ModifyBitv3
+    {
+        static void Main(string[] args)
+        {
+            ulong numN = ulong.Parse(Console.ReadLine());
+            int Pos = int.Parse(Console.ReadLine());
+            int newVal = int.Parse(Console.ReadLine());
+
+            ulong mask = (ulong)1 << Pos;
+            ulong currVal = numN & mask;
+            currVal >>= Pos;
+
+            if ( (int)currVal != newVal)
+            {
+                Console.WriteLine((ulong)(numN ^ mask));
+            }
+            else
+            { Console.WriteLine(numN); }
+        }
+    }
+}
