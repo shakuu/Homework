393e5b4c16fad26ed8d9a1eba4d3339499807000
shakuu <todorov.h@gmail.com>
2016-04-08 04:18:19 +0300

    HTML  homework Update

---
+++ 1. CSharp1/3. Homework-Operators-and-Expressions/BitswapKirilPc/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class BitSwap
+{
+    static void Main()
+    {
+        long number = long.Parse(Console.ReadLine());
+        int firstPos = int.Parse(Console.ReadLine());
+        int secondPos = int.Parse(Console.ReadLine());
+        if (firstPos > secondPos)
+        {
+            int holder = firstPos;
+            firstPos = secondPos;
+            secondPos = holder;
+        }
+        int bitNum = int.Parse(Console.ReadLine());
+        long maskLenth = (long)Math.Pow(2, bitNum) - 1;
+
+        long firstMask = maskLenth << firstPos;
+        long secondMask = maskLenth << secondPos;
+        long firstBlankMask = ~(maskLenth << firstPos);
+        long secondBlankMask = ~(maskLenth << secondPos);
+
+
+        firstMask = firstMask & number;
+        secondMask = secondMask & number;
+
+        number = firstBlankMask & number;
+        number = secondBlankMask & number;
+
+        number = (firstMask << (secondPos - firstPos)) | number;
+        number = (secondMask >> (secondPos - firstPos)) | number;
+        Console.WriteLine(number);
+
+    }
+}
