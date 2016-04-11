b0ccabcf6b36d46a8f00301198bb8bd325786b29
shakuu <todorov.h@gmail.com>
2016-04-11 16:37:53 +0300

    ConsoleApp4
    New: working
    todo: less than 10, 10+ separate results

--- 1. CSharp1/7. ExamPrep/ConsoleApp4/ConsoleApp4.cs
+++ 1. CSharp1/7. ExamPrep/ConsoleApp4/ConsoleApp4.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace ConsoleApp4
 {
-    class Program
+    class ConsoleApp4
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            BigInteger RESULT = 1;
+            bool isRunning = true;
+            string currNumber = "";
+
+            while(isRunning)
+            {
+                currNumber = Console.ReadLine().ToUpper();
+                //Stop on END
+                if (currNumber=="END")
+                {
+                    Console.WriteLine(RESULT);
+                    return;
+                }
+                if (Convert.ToInt64(currNumber) > 0)
+                {
+                    foreach (char currDigit in currNumber)
+                    {
+                        if (currDigit>='1' && currDigit<='9')
+                        {
+                            RESULT *= (currDigit - '0');
+                        }
+                    }
+                }
+
+                currNumber = Console.ReadLine().ToUpper();
+
+                if (currNumber == "END")
+                {
+                    Console.WriteLine(RESULT);
+                    return;
+                }
+            }
         }
     }
 }
