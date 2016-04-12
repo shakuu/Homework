d633dcf9ef7454672a965c02fd054567a079944b
shakuu <todorov.h@gmail.com>
2016-04-12 16:25:26 +0300

    GameOfPage
    TODO: ALL

--- 1. CSharp1/13-Dec-06-EveningExam/CodeStreet/CodeStreet.cs
+++ 1. CSharp1/13-Dec-06-EveningExam/CodeStreet/CodeStreet.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CodeStreet
 {
-    class Program
+    class CodeStreet
     {
         static void Main()
         {
--- 1. CSharp1/13-Dec-06-EveningExam/KaspichaniaBoats/KBoats.cs
+++ 1. CSharp1/13-Dec-06-EveningExam/KaspichaniaBoats/KBoats.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace KaspichaniaBoats
 {
-    class Program
+    class KBoats
     {
         static void Main()
         {
