8e2a0ecb2712e0f6b4f373b128b926e73fe2906d
shakuu <todorov.h@gmail.com>
2016-04-12 03:54:39 +0300

    1

---
+++ 1. CSharp1/13-Dec-06-EveningExam/2-4-8/2-4-8.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_4_8
+{
+    class Program
+    {
+        static void Main()
+        {
+            long numA = long.Parse(Console.ReadLine());
+            long numB = long.Parse(Console.ReadLine());
+            long numC = long.Parse(Console.ReadLine());
+            long Result = 0;
+            switch (numB)
+            {
+                case 2:
+                    Result = numA % numC;
+                    break;
+                case 4:
+                    Result = numA + numC;
+                    break;
+                case 8:
+                    Result = numA * numC;
+                    break;
+            }
+
+            if (Result % 4 == 0)
+            {
+                Console.WriteLine(Result / 4);
+                Console.WriteLine(Result);
+            }
+            else 
+            {
+                Console.WriteLine(Result % 4);
+                Console.WriteLine(Result);
+            }
+        }
+    }
+}
---
+++ 1. CSharp1/13-Dec-06-EveningExam/CodeStreet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeStreet
+{
+    class Program
+    {
+        static void Main()
+        {
+            string inputText = Console.ReadLine();
+            long index = 0;
+            long counter = 0;
+            long sum = 0;
+            foreach(char digit in inputText)
+            {
+                if (index%2 !=0)
+                {
+                    counter++;
+                    sum += digit - '0';
+                }
+                index++;
+            }
+
+            Console.WriteLine("{0} {1}", counter, sum);
+        }
+    }
+}
