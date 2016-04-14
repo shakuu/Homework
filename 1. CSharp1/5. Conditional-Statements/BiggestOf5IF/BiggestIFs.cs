abd207624502f125868be55c799be123431cf213
shakuu <todorov.h@gmail.com>
2016-04-14 12:34:41 +0300

    too many brackets

--- 1. CSharp1/5. Conditional-Statements/BiggestOf5IF/BiggestIFs.cs
+++ 1. CSharp1/5. Conditional-Statements/BiggestOf5IF/BiggestIFs.cs
@@ -10,9 +10,9 @@ namespace BiggestOf5IF
             double temp2;
             double MAX = (temp1 = double.Parse(Console.ReadLine()))
                 > (temp2 = double.Parse(Console.ReadLine())) ?
-                temp1 : temp2;
+                    temp1 : temp2;
             MAX = (temp1 = double.Parse(Console.ReadLine())) > MAX ?
-                temp1 : MAX;
+                    temp1 : MAX;
             MAX = (temp1 = double.Parse(Console.ReadLine())) > MAX ?
                     temp1 : MAX;
             Console.WriteLine((temp1 = double.Parse(Console.ReadLine())) > MAX ?
