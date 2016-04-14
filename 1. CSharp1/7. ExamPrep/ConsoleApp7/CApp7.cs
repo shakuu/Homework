90b2ee8af0894fe2c9e60e2ce220f195e6ebe834
shakuu <todorov.h@gmail.com>
2016-04-14 15:00:26 +0300

    console App 7

---
+++ 1. CSharp1/7. ExamPrep/ConsoleApp7/CApp7.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace ConsoleApp7
+{
+    class ConsoleApp7
+    {
+        static void Main()
+        {
+            bool isRunning = true;
+            string currNumber = "";
+
+            //GET NUMBERS <10
+            int[] digits1 = new int[4];
+            int inputCounter = 0;
+            while (isRunning)
+            {
+                currNumber = Console.ReadLine().ToUpper();
+                if (currNumber == "END")
+                {
+                    isRunning = false;
+                    break;
+                }
+                else
+                {
+                    for ( int c = 0; c < currNumber.Length; c++)
+                    {
+                        switch (currNumber[c])
+                        {
+                            case '2':
+                                digits1[0]++; //[0] -> 2s
+                                break;
+                            case '3':
+                                digits1[1]++; // [1] -> 3s
+                                break;
+                            case '4':
+                                digits1[0] += 2;
+                                break;
+                            case '5':
+                                digits1[2]++; //[2] -> 5s
+                                break;
+                            case '6':
+                                digits1[0]++;
+                                digits1[1]++;
+                                break;
+                            case '7':
+                                digits1[3]++; //[3] ->7s
+                                break;
+                            case '8':
+                                digits1[0]+=3;
+                                break;
+                            case '9':
+                                digits1[1]+=2;
+                                break;
+                        }
+                    }
+                    inputCounter++;
+                }
+                currNumber = Console.ReadLine().ToUpper();
+                inputCounter++;
+                if (currNumber == "END")
+                {
+                    isRunning = false;
+                    break;
+                }
+
+                //end on 10
+                if (inputCounter == 10)
+                {
+                    break;
+                }
+            }
+            //EVERYTHING ELSE
+            int[] digits2 = new int[10];
+            while (isRunning)
+            {
+                currNumber = Console.ReadLine().ToUpper();
+                if (currNumber == "END")
+                {
+                    isRunning = false;
+                    break;
+                }
+                else
+                {
+                    for(int c = 0; c < currNumber.Length; c++)
+                    {
+                        switch (currNumber[c])
+                        {
+                            case '2':
+                                digits2[0]++; //[0] -> 2s
+                                break;
+                            case '3':
+                                digits2[1]++; // [1] -> 3s
+                                break;
+                            case '4':
+                                digits2[0] += 2;
+                                break;
+                            case '5':
+                                digits2[2]++; //[2] -> 5s
+                                break;
+                            case '6':
+                                digits2[0]++;
+                                digits2[1]++;
+                                break;
+                            case '7':
+                                digits2[3]++; //[3] ->7s
+                                break;
+                            case '8':
+                                digits2[0] += 3;
+                                break;
+                            case '9':
+                                digits2[1] += 2;
+                                break;
+                        }
+                    }
+                }
+                currNumber = Console.ReadLine().ToUpper();
+                if (currNumber == "END")
+                {
+                    isRunning = false;
+                    break;
+                }
+            }
+
+            int[] values = new int[4] { 2, 3, 5, 7 };
+            BigInteger TestResult = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                TestResult = BigInteger.Multiply(TestResult, BigInteger.Pow(values[i], digits1[i]));
+
+            }
+
+            Console.WriteLine(TestResult);
+            if (inputCounter < 10)
+            { return; }
+
+            BigInteger RESULT10 = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                RESULT10 = BigInteger.Multiply(RESULT10, BigInteger.Pow(values[i], digits2[i]));
+            }
+
+            Console.WriteLine(RESULT10);
+        }
+    }
+}
---
+++ 1. CSharp1/7. ExamPrep/RadoCApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Numerics;
+
+class ConsoleApplication1
+{
+    static void Main(string[] args)
+    {
+        string numbers = Console.ReadLine();
+        int IsOdd = 0;
+        BigInteger products = 1;
+        BigInteger product10 = 1;
+        bool isMoreThanTen = false;
+        while (numbers != "END")
+        {
+
+            BigInteger product = 1;
+            if ((IsOdd == 10) && (!isMoreThanTen))
+            {
+                isMoreThanTen = true;
+                product10 = products;
+                products = 1;
+            }
+            else if ((IsOdd % 2 == 0) && (numbers != "0"))
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+
+                    if (int.Parse(numbers[i].ToString()) == 0) continue;
+                    else product *= int.Parse(numbers[i].ToString());
+                }
+
+            }
+            products *= product;
+            IsOdd++;
+            numbers = Console.ReadLine();
+
+        }
+        if (isMoreThanTen) Console.WriteLine(product10);
+        Console.WriteLine(products);
+    }
+}
