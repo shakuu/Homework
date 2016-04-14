114fd07fecf105817937d888544a5bafc73b4308
shakuu <todorov.h@gmail.com>
2016-04-14 12:32:50 +0300

    format

--- 1. CSharp1/5. Conditional-Statements/BiggestOf5IF/BiggestIFs.cs
+++ 1. CSharp1/5. Conditional-Statements/BiggestOf5IF/BiggestIFs.cs
@@ -6,19 +6,17 @@ namespace BiggestOf5IF
     {
         static void Main()
         {
-            double temp2;
             double temp1;
+            double temp2;
             double MAX = (temp1 = double.Parse(Console.ReadLine()))
                 > (temp2 = double.Parse(Console.ReadLine())) ?
                 temp1 : temp2;
-
             MAX = (temp1 = double.Parse(Console.ReadLine())) > MAX ?
                 temp1 : MAX;
             MAX = (temp1 = double.Parse(Console.ReadLine())) > MAX ?
                     temp1 : MAX;
             Console.WriteLine((temp1 = double.Parse(Console.ReadLine())) > MAX ?
                     temp1 : MAX);
-
         }
     }
 }
