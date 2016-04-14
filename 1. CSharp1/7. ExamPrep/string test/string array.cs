95e58cb2898f508b0ea1f74749c576e90bfa342e
shakuu <todorov.h@gmail.com>
2016-04-14 13:51:41 +0300

    update cApp2

---
+++ 1. CSharp1/7. ExamPrep/string test/string array.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "123456789";
+            Console.WriteLine(test[0]);
+        }
+    }
+}
