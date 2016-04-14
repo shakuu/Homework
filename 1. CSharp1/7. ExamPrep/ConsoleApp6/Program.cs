fe3d1f5cd7cafb65dfd5f04a6e2af84b71c4127a
shakuu <todorov.h@gmail.com>
2016-04-14 12:02:44 +0300

    changes to console app 2

---
+++ 1. CSharp1/7. ExamPrep/ConsoleApp6/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace ConsoleApp2
+{
+    class ConsoleApp6
+    {
+        static void Main()
+        {
+            BigInteger RESULT = 1;
+            bool isRunning = true;
+            string currNumber = "";
+
+            //GET NUMBERS <10
+            int[] digits1 = new int[10];
+            int inputCounter = 0;
+            while (isRunning)
+            {
+                currNumber = Console.ReadLine().ToUpper();
+                if (currNumber == "END")
+                {
+                    isRunning = false;
+                    break;
+                }
+                else
+                {
+                    foreach (char currDigit in currNumber)
+                    {
+                        if (currDigit != '0')
+                        {
+                            digits1[currDigit - '0']++;
+                        }
+                    }
+                    inputCounter++;
+                }
+                currNumber = Console.ReadLine().ToUpper();
+                inputCounter++;
+                if (currNumber == "END")
+                {
+                    isRunning = false;
+                    break;
+                }
+
+                //end on 10
+                if (inputCounter == 10)
+                {
+                    break;
+                }
+            }
+            //EVERYTHING ELSE
+            int[] digits2 = new int[10];
+            while (isRunning)
+            {
+                currNumber = Console.ReadLine().ToUpper();
+                if (currNumber == "END")
+                {
+                    isRunning = false;
+                    break;
+                }
+                else
+                {
+                    foreach (char currDigit in currNumber)
+                    {
+                        if (currDigit != '0')
+                        {
+                            digits2[currDigit - '0']++;
+                        }
+                    }
+                }
+                currNumber = Console.ReadLine().ToUpper();
+                if (currNumber == "END")
+                {
+                    isRunning = false;
+                    break;
+                }
+            }
+
+            
+            BigInteger TestResult = 1;
+            for (int i = 2; i < 10; i++)
+            {
+                TestResult = BigInteger.Multiply(TestResult, BigInteger.Pow(i, digits1[i]));
+
+            }
+            
+            Console.WriteLine(TestResult);
+            if ( inputCounter < 10)
+            { return; }
+            
+            BigInteger RESULT10 = 1;
+            for (int i = 2; i < 10; i++)
+            {
+                RESULT10 = BigInteger.Multiply(RESULT10, BigInteger.Pow(i, digits2[i]));
+            }
+           
+            Console.WriteLine(RESULT10);
+        }
+    }
+}
