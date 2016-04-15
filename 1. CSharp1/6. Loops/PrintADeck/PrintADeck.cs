a668fb16c2077115c3373889d880773b00c8aef7
shakuu <todorov.h@gmail.com>
2016-04-16 02:56:38 +0300

    read 1 char

--- 1. CSharp1/6. Loops/PrintADeck/PrintADeck.cs
+++ 1. CSharp1/6. Loops/PrintADeck/PrintADeck.cs
@@ -10,25 +10,26 @@ namespace PrintADeck
     {
         static void Main()
         {
-            string playerCard = (Console.ReadLine()).ToUpper() ;
+            string playerCard = (Console.ReadLine()).ToUpper();
+            //char playerCard = char.Parse((Console.ReadLine()).Substring(0,1));
 
             string allCards = "2 3 4 5 6 7 8 9 10 J Q K A";
             string allSuits = "spades clubs hearts diamonds";
 
             string[] Cards = allCards.Split(' ');
             string[] Suits = allSuits.Split(' ');
-            
+
             for (int cardIndex = 0; cardIndex < Cards.Length; cardIndex++)
             {
-                for(int suitIndex = 0; suitIndex<Suits.Length; suitIndex++)
+                for (int suitIndex = 0; suitIndex < Suits.Length; suitIndex++)
                 {
                     Console.Write("{0} of {1}", Cards[cardIndex], Suits[suitIndex]);
-                    if ( suitIndex != Suits.Length-1)
+                    if (suitIndex != Suits.Length - 1)
                     { Console.Write(", "); }
                 }
                 Console.Write("\n");
 
-                if ( playerCard.ToString() == Cards[cardIndex])
+                if (playerCard.ToString() == Cards[cardIndex])
                 { return; }
             }
 
