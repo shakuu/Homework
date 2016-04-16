680ef6fc0a0a7c152e3a4f0b0cd3c12db29fc036
shakuu <todorov.h@gmail.com>
2016-04-16 21:07:09 +0300

    tribunacci triangle working

---
+++ 1. CSharp1/TA-Exam-12-Dec-27/TribunacciTriangles/TribunacciTriangle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TribunacciTriangles
+{
+    class TribunacciTriangle
+    {
+        static void Main()
+        {
+            List<long> Triangle = new List<long>();
+            //input
+            Triangle.Add(long.Parse(Console.ReadLine()));
+            Triangle.Add(long.Parse(Console.ReadLine()));
+            Triangle.Add(long.Parse(Console.ReadLine()));
+            int inputLength = int.Parse(Console.ReadLine());
+
+            //build a list of numbers
+            int triangleLength = 0;
+            for (int i =0; i< inputLength;i++ )
+            {
+                triangleLength += inputLength- i;
+            }
+
+            for (int i = 3; i < triangleLength; i++)
+            {
+                Triangle.Add(Triangle[i - 1] + Triangle[i - 2] + Triangle[i - 3]);
+            }
+
+            int counter = 0;
+            string toPrint;
+            for (int row = 1; row <= inputLength; row++)
+            {
+                //clear the string
+                toPrint = "";
+                //build the string 
+                for (int i = 0; i < row; i++)
+                {
+                    toPrint += Triangle[counter].ToString();
+                    counter++;
+                    if (i != row - 1)
+                    {
+                        toPrint += " ";
+                    }
+                }
+                Console.WriteLine(toPrint);
+            }
+        }
+    }
+}
