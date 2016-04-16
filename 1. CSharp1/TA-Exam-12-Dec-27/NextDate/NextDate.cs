6ec4755f4b2c63475dc3cc6751867c9306b04407
shakuu <todorov.h@gmail.com>
2016-04-16 19:54:10 +0300

    next day

---
+++ 1. CSharp1/TA-Exam-12-Dec-27/NextDate/NextDate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NextDate
+{
+    class NextDate
+    {
+        static void Main()
+        {
+            string inputDay = Console.ReadLine().PadLeft(2, '0');
+            string inputMonth = Console.ReadLine().PadLeft(2, '0');
+            string inputYear = Console.ReadLine();
+            string inputDateTime = inputDay + "."
+                + inputMonth + "." + inputYear;
+
+            DateTime toPrint = new DateTime();
+
+            toPrint = DateTime.ParseExact(inputDateTime, "dd.MM.yyyy", null);
+            toPrint = toPrint.AddDays(1);
+
+            Console.WriteLine(toPrint.ToString("d.MM.yyyy"));
+        }
+    }
+}
