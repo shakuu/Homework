1fc05c225d4375eb8ad9fa95789764b57e5cff19
shakuu <todorov.h@gmail.com>
2016-04-16 14:50:05 +0300

    500/ 500 READ DESCRIPTION

---
+++ 1. CSharp1/TA Exam 13-Jun-23/NeuronsMap/NeuronsMap.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuronsMap
+{
+    class NeuronsMap
+    {
+        class Neurons
+        {
+            public char Value;
+            public bool isNeuron = false;
+
+            public Neurons(char cVal)
+            {
+                this.Value = cVal;
+            }
+
+            public Neurons(string cStr, int cPos)
+            {
+                this.Value = cStr[cPos];
+            }
+        }
+
+        static void Main()
+        {
+            //given
+            const int stringLength = 32;
+            //Input : N numbers , while NOT negative
+            List<string> inputStrings = new List<string>();
+            long toAdd;
+            while (true)
+            {
+                toAdd = long.Parse(Console.ReadLine());
+                if (toAdd < 0) { break; }
+
+                inputStrings.Add(Convert.ToString(toAdd, 2).PadLeft(stringLength, '0'));
+            }
+            //Create Neurons Grid
+            Neurons[,] neuronGrid
+                = new Neurons[inputStrings.Count, stringLength];
+            //check each bit
+            for (int currRow = 0;
+                currRow<inputStrings.Count; currRow++)
+            {
+                for (int currCol = 0; currCol<stringLength; currCol++)
+                {
+                    bool boundRight = false;
+                    bool boundTop = false;
+                    bool boundBot = false;
+                }
+            }
+        }
+    }
+}
---
+++ 1. CSharp1/TA Exam 13-Jun-23/NeuronsSimple/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NeuronsSimple
+{
+    class Program
+    {
+        static void Main()
+        {
+            //get strings 
+            List<string> inStrings = new List<string>();
+            long toAdd;
+            while (true)
+            {
+                toAdd = long.Parse(Console.ReadLine());
+                if (toAdd < 0) { break; }
+
+                inStrings.Add(Convert.ToString(toAdd, 2).PadLeft(32, '0'));
+            }
+
+            for (int currIndex = 0; currIndex < inStrings.Count; currIndex++)
+            {
+                //if current string contains 1 -> replace 1s with 0s and 0s with 1s
+                if (inStrings[currIndex].Contains('1'))
+                {
+                    int currPos = inStrings[currIndex].IndexOf('1');
+                    string currNeuron = inStrings[currIndex].Substring(
+                        inStrings[currIndex].IndexOf('1'),
+                        inStrings[currIndex].LastIndexOf('1') -
+                        inStrings[currIndex].IndexOf('1') + 1);
+                    currNeuron = currNeuron.Replace('0', '+');
+                    currNeuron = currNeuron.Replace('1', '0');
+                    currNeuron = currNeuron.Replace('+', '1');
+
+                    inStrings[currIndex] = inStrings[currIndex].Insert(
+                        currPos, currNeuron);
+                    inStrings[currIndex] = inStrings[currIndex].Remove(
+                        currPos + currNeuron.Length, currNeuron.Length);
+                }
+                //print string
+                Console.WriteLine(Convert.ToInt64(inStrings[currIndex], 2));
+            }
+        }
+    }
+}
