57ac4d32f807debbae68f81e2488f63639f306df
shakuu <todorov.h@gmail.com>
2016-04-17 20:02:21 +0300

    new: Garden - simple - working
    TODO: make a dictionary for prices, make a seed class

---
+++ 1. CSharp1/TA-Exam-13-June-24/Garden/Garden.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garden
+{
+    class Garden
+    {
+        static void Main()
+        {
+            Dictionary<string, double> SeedPrices = new Dictionary<string, double>()
+            {
+                {"tomato", 0.5 },
+                {"cucumber", 0.4 },
+                {"potato" , 0.25},
+                {"carrot", 0.6 },
+                { "cabbage", 0.3 },
+                {"beans", 0.4 }
+            };
+
+            //input by definition
+            const int GarderArea = 250;
+            double[] Prices = new double[6] { 0.5, 0.4, 0.25, 0.6, 0.3, 0.4 };
+            //SIMPLE 
+            int areaUsed = 0;
+            double Cost = 0;
+            for (int i = 0; i<5; i++)
+            {
+                //seeds cost
+                Cost += int.Parse(Console.ReadLine()) * Prices[i];
+                //get area
+                areaUsed += int.Parse(Console.ReadLine());
+            }
+            //add Potato Cost
+            Cost += int.Parse(Console.ReadLine()) * Prices[5];
+
+            //output
+            Console.WriteLine("Total costs: {0:F2}", Cost);
+            if (GarderArea > areaUsed)
+            {
+                Console.WriteLine("Beans area: {0}", GarderArea - areaUsed);
+            }
+            else if ( GarderArea==areaUsed)
+            {
+                Console.WriteLine("No area for beans");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient area");
+            }
+
+        }
+    }
+}
