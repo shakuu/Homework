f8bd3bbeb13830e65b7ee33fbfbfda1e97b7af26
shakuu <todorov.h@gmail.com>
2016-04-18 14:53:57 +0300

    Goiko Tower 100/ 100 Note: care with checks

---
+++ 1. CSharp1/TA-Exam-13-June-24/GoikoTower/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace GoikoTower
+{
+    class Program
+    {
+        static void Main()
+        {
+            int towerH = int.Parse(Console.ReadLine());
+            int towerW = towerH * 2;
+
+            char[,] theTower = new char[towerH, towerW];
+
+            //fill with dots
+            for (int row = 0; row < towerH; row++)
+            {
+                for (int col = 0; col < towerW; col++)
+                {
+                    theTower[row, col] = '.';
+                }
+            }
+
+            int currBeamRow = 1;
+            //build tower
+            for (int row = 0; row < towerH; row++)
+            {
+                theTower[row, towerW / 2 - 1 - row] = '/';
+                theTower[row, towerW / 2 + row] = '\\';
+            }
+            //build beams
+            int currRow = 0;
+            for (int rowD = 1; currRow + rowD < towerH; rowD++)
+            {
+                currRow += rowD;
+                for (int col = towerW / 2 - currRow;
+                    col < towerW / 2 + currRow; col++)
+                {
+                    theTower[currRow, col] = '-';
+                }
+            }
+
+            //print tower
+            for (int row = 0; row < towerH; row++)
+            {
+                for (int col = 0; col < towerW; col++)
+                {
+                    Console.Write(theTower[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
