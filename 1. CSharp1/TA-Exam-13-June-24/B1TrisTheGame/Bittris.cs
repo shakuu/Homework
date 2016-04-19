b178c0a8e510bc146796bdf68789770f36a187ce
shakuu <todorov.h@gmail.com>
2016-04-19 03:29:22 +0300

    B1t new: input TODO: move down, checks out of bounds add to static list

--- 1. CSharp1/TA-Exam-13-June-24/B1TrisTheGame/Bittris.cs
+++ 1. CSharp1/TA-Exam-13-June-24/B1TrisTheGame/Bittris.cs
@@ -9,52 +9,195 @@ namespace B1TrisTheGame
 {
     class Bittris
     {
-        class Pieces
+        public class PlayerArea
         {
-            public int posX = 0; //row
-            public int posY = 0; //col
+            public int CanvasWidth;
+            public int CanvasHeight;
+
+            public int Width;
+            public int Height;
+
+            public int TopRow;
+            public int BotRow;
+
+            public PlayerArea()
+            {
+                this.CanvasWidth = 36;
+                this.CanvasHeight = 36;
+
+                this.Width = CanvasWidth * 1;
+                this.Height = CanvasHeight * (5 / 6);
+
+                this.TopRow = (CanvasHeight - Height) / 2;
+                this.BotRow = CanvasHeight - (CanvasHeight - Height) / 2;
+            }
+
+        }
+
+
+
+        public class Pieces
+        {
+            public int posRow = 0; //row
+            public int posCol = 20; //col
             public int vSpeed = 1;
             public int hSpeed = 0;
             public string toPrint = "";
             public int stringLength;
-            public int hasOnes;
             public int Score;
             public ConsoleColor Color;
             public bool isMoving = true;
 
-            public Pieces(string currStr, ConsoleColor currColor)
+            public Pieces(int rndNumber)
             {
-                this.toPrint = currStr;
-                this.Color = currColor;
-                this.stringLength = currStr.Length;
-                
+                //TODO: ADD COLORS, Start Position
+                this.toPrint = (Convert.ToString(rndNumber, 2).
+                    PadLeft(32, '0')).
+                    Substring(24, 8); // conver to binary
+                this.toPrint = this.toPrint.TrimStart('0');
+                this.toPrint = this.toPrint.TrimEnd('0');
+
+                //get score ( number of ones )
+                foreach (char digit in this.toPrint)
+                { if (digit == '1') { this.Score++; } }
+
             }
 
+        }
+
+        class Player
+        {
+            public string Name = "";
+            public int Score = 0;
+            public ConsoleKeyInfo Input = new ConsoleKeyInfo();
+            public Pieces activePiece;
+
+            public Player(string cName)
+            {
+                this.Name = cName;
+            }
+
+            public Player()
+            {
+                this.Name = "Player1";
+            }
         }
 
         static void Main()
         {
             //settings
-            const int CanvastWidth = 40;
-            const int CanvasHeight = 20;
+            Console.Clear();
 
-            Console.BufferWidth = Console.WindowWidth = CanvastWidth;
-            Console.BufferHeight = Console.WindowHeight = CanvasHeight;
+            PlayerArea playArea = new PlayerArea();
 
-            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.BufferWidth = Console.WindowWidth = playArea.CanvasWidth;
+            Console.BufferHeight = Console.WindowHeight = playArea.CanvasHeight;
+
+            Console.Title = "B1tTris The Game";
+
+            Console.BackgroundColor = ConsoleColor.Black;
             Console.CursorVisible = false;
 
             Console.Clear();
 
-            int appSpeed = 150;
+            int appSpeed = 200;
             bool appIsRunning = true;
 
-            while(appIsRunning)
-            {
+            Random mainRandomizer = new Random();
+            // end settings
 
-                Thread.Sleep(appSpeed)
+            //variables
+            List<Pieces> staticPieces = new List<Pieces>();
+
+            Player player1 = new Player();
+            //end variables
+
+            while (appIsRunning)
+            {
+                //Create a new Shape
+                player1.activePiece = new Pieces(mainRandomizer.Next(0, Int32.MaxValue));
+
+                //MAIN
+                while (player1.activePiece.isMoving)
+                {
+                    //read player input
+                    while (Console.KeyAvailable)
+                    {
+                        player1.Input = Console.ReadKey();
+                    }
+                    //Change Position
+                    switch (player1.Input.Key)
+                    {
+                        case ConsoleKey.LeftArrow:
+                            player1.activePiece.hSpeed = -1;
+                            player1.Input = new ConsoleKeyInfo();
+                            break;
+                        case ConsoleKey.RightArrow:
+                            player1.activePiece.hSpeed = 1;
+                            player1.Input = new ConsoleKeyInfo();
+                            break;
+                    }
+                    //check if position left or right is free
+                    if (CheckSides(player1.activePiece, staticPieces))
+                    {
+                        player1.activePiece.posCol += player1.activePiece.hSpeed; //move the piece
+                        player1.activePiece.hSpeed = 0;
+                    }
+                    else
+                    {
+                        player1.activePiece.hSpeed = 0; //ignore input
+                    }
+
+                    Console.SetCursorPosition(player1.activePiece.posCol, player1.activePiece.posRow);
+                    Console.Write(player1.activePiece.toPrint);
+
+                    Thread.Sleep(appSpeed);
+                }
+
+                Thread.Sleep(appSpeed);
             }
 
         }
+
+        public static bool CheckSides(Pieces currElement, List<Pieces> existingPieces)
+        {
+            //TODO 
+            //1. Check Outside of player Area
+
+            bool result = true;
+
+            // each existing piece
+            if (currElement.hSpeed < 0) //check Left
+            {
+                foreach (Pieces piece in existingPieces)
+                {
+                    if (piece.posRow == currElement.posRow)
+                    {
+                        if (piece.posCol + piece.stringLength >=
+                            currElement.posCol + currElement.hSpeed) ;
+                        {
+                            result = false;
+                        }
+                    }
+                }
+            }
+            else //Check Right
+            {
+                foreach (Pieces piece in existingPieces)
+                {
+                    if (piece.posRow == currElement.posRow)
+                    {
+                        if (piece.posCol <=
+                            currElement.posCol + currElement.stringLength
+                            + currElement.hSpeed) ;
+                        {
+                            result = false;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
     }
 }
