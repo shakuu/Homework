e9b9163975295725cac9bd98a6ee1439ee900cd2
shakuu <todorov.h@gmail.com>
2016-04-22 21:18:11 +0300

    2 / 5 todo... everything else

---
+++ 1. CSharp1/TA-Exam-15-Feb-3-Morning/Decoding/Decoding.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decoding
+{
+    class Decoding
+    {
+        static void Main()
+        {
+            //input SALT -> Modifier, TEXT -> inputString
+            var SALT = int.Parse(Console.ReadLine());
+            var Text = Console.ReadLine();
+
+            List<double> EncodeValues = new List<double>(); // not really needed 
+
+            double currValue = 0;
+
+            for (int chr = 0; chr < Text.Length; chr++)
+            {
+                //reset Value
+                currValue = 0;
+
+                //Check 1: letter/ digit/ else
+                //stop on @
+                if (Text[chr] == '@')
+                {
+                    break;
+                }
+                // if LETTER
+                else if (char.IsLetter(Text[chr]))
+                {
+                    currValue = Text[chr] * SALT + 1000;
+                }
+                else if (char.IsDigit(Text[chr]))
+                {
+                    currValue = Text[chr] + SALT + 500;
+                }
+                else
+                {
+                    currValue = Text[chr] - SALT;
+                }
+
+                //Check 2: odd / even position 
+                if (chr % 2 == 0)
+                {
+                    currValue /= 100;
+                    Console.WriteLine(currValue.ToString("0.00"));
+                }
+                else
+                {
+                    currValue *= 100;
+                    Console.WriteLine(currValue); // no additional 00s
+                }
+            }
+        }
+    }
+}
---
+++ 1. CSharp1/TA-Exam-15-Feb-3-Morning/ThreeNumbers/ThreeNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ThreeNumbers
+{
+    class ThreeNumbers
+    {
+        static void Main()
+        {
+            //input
+            double[] numbs = new double[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                numbs[i] = double.Parse(Console.ReadLine());
+            }
+
+            //output
+            Console.WriteLine(numbs.Max());
+            Console.WriteLine(numbs.Min());
+            Console.WriteLine(numbs.Average().ToString("0.00"));
+        }
+    }
+}
