1374d928a3396d9d43c9678bca62b53199b3d25c
shakuu <todorov.h@gmail.com>
2016-04-22 14:57:11 +0300

    1

---
+++ 1. CSharp1/TA-Exam-11-Dec-6/FirTree/FirTree.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FirTree
+{
+    class FirTree
+    {
+        static void Main()
+        {
+            // characters to print
+            string Tree = "*";
+            string Else = ".";
+
+            //input
+            int inputSizeN = int.Parse(Console.ReadLine());
+
+            //format the input
+            int height = inputSizeN;
+            int width = ((inputSizeN - 1) * 2) - 1;
+            int midPointW = (inputSizeN - 1) -1; // extra -1 to count from 0
+
+            //Print
+            for (int row = 0; row < height; row++)
+            {
+                for (int col = 0; col < width; col++)
+                {
+                    // Step 1: Print the tree
+                    if (midPointW - row <= col && col <= midPointW + row
+                        && row != height -1)
+                    {
+                        Console.Write(Tree);
+                    }
+                    // Step 2: Print the bottom
+                    else if (row == height-1 && col == midPointW)
+                    {
+                        Console.Write(Tree);
+                    }
+                    else
+                    {
+                        Console.Write(Else);
+                    }
+                }
+                //new line
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 1. CSharp1/TA-Exam-11-Dec-6/IheartBits/IHeartBits.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IheartBits
+{
+    class IHeartBits
+    {
+        static void Main()
+        {
+            //input -> amount of numbers to process
+            int numInput = int.Parse(Console.ReadLine());
+
+            // arrays to store numbers and rresults
+            uint[] inputP = new uint[numInput];
+            uint[] notP = new uint[numInput]; //P̃
+            uint[] invertP = new uint[numInput]; //P̈
+            uint[] result = new uint[numInput];
+
+            // for every number
+            for (int index = 0; index < numInput; index++)
+            {
+                // get a new number
+                inputP[index] = uint.Parse(Console.ReadLine());
+
+                // Step 1: reverse ONLY bits within the number
+                //notP[index] = (~inputP[index]); inverts 32 bits 
+                uint tempMask = 1;
+                uint tempNumb = inputP[index];
+
+                // working 
+                while (tempNumb > 0)
+                {
+                    if ((tempNumb & tempMask) == 1) // if 1 add 0
+                    {
+                        notP[index] = notP[index] << 1;
+                    }
+                    else                                            // if 0 add 1
+                    {
+                        notP[index] = notP[index] << 1;
+                        notP[index] = notP[index] | tempMask;
+                    }
+                    //1 bit right and go again
+                    tempNumb = tempNumb >> 1;
+                }
+
+                // Step 2; get reveres bit sequence
+                //read bits -> +1 to add 1, << to add 0
+                tempMask = 1;
+                tempNumb = inputP[index];
+
+                while (tempNumb > 0)
+                {
+                    if ((tempNumb & tempMask) == 1) // if 1 add 1
+                    {
+                        invertP[index] = invertP[index] << 1;
+                        invertP[index] = invertP[index] | tempMask;
+                    }
+                    else
+                    {
+                        invertP[index] = invertP[index] << 1; // if 0 add 0
+                    }
+                    //1 bit right and go again
+                    tempNumb = tempNumb >> 1;
+                }
+
+                // get result
+                result[index] = (inputP[index] ^ notP[index]);
+                result[index] = result[index] & invertP[index];
+
+                //print
+                Console.WriteLine(invertP[index]);
+            }
+        }
+    }
+}
