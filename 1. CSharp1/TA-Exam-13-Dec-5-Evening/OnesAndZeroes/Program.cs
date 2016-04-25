82d78ea6ecc168e237fc6488f48847650b7ded49
shakuu <todorov.h@gmail.com>
2016-04-25 14:14:41 +0300

    Ones And Zeros 100/ 100

---
+++ 1. CSharp1/TA-Exam-13-Dec-5-Evening/OnesAndZeroes/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace OnesAndZeroes
+{
+    class Program
+    {
+        static void Main()
+        {
+            //given
+            string[] printZero = new string[]
+            {
+                "###",
+                "#.#",
+                "#.#",
+                "#.#",
+                "###"
+            };
+
+            string[] printOne = new string[]
+            {
+                ".#.",
+                "##.",
+                ".#.",
+                ".#.",
+                "###"
+            };
+
+            string Space = ".";
+
+            int maxBit = 16;
+            int stringRows = 5;
+
+            //input non negative int
+            uint InputNumber = uint.Parse(Console.ReadLine());
+            
+            // variables
+            string[] printRows = new string[]
+            {
+                "",
+                "",
+                "",
+                "",
+                ""
+            };
+        
+            //Build the strings
+            for (int col = 0; col < maxBit; col++)      //For the Rightmost 16 positions
+            {
+                //Step 1: Get Current Bit Value - Left to Rgiht
+                uint currBit = (InputNumber & (uint)(1 << (maxBit - 1 - col))) >> (maxBit - 1 - col);
+
+                // Step 2: Build the String
+                for (int row = 0; row < stringRows; row++)
+                {
+                    //Print 0 or 1
+                    if (currBit == 1)
+                    {
+                        printRows[row] += printOne[row];
+                    }
+                    else if (currBit == 0)
+                    {
+                        printRows[row] += printZero[row];
+                    }
+
+                    // Add empty
+                    printRows[row] += Space;
+                }
+            }
+
+            for (int row = 0; row < stringRows; row++)
+            {
+                //remove extra empty spaces in the end
+                printRows[row] = printRows[row].Remove(printRows[row].Length - 1, 1);
+
+                //print
+                Console.WriteLine(printRows[row]);
+            }
+        }
+    }
+}
