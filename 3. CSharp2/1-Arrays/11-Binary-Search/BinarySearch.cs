fb5b1f37f55a57882294189f86178ebebb4b98cc
shakuu <todorov.h@gmail.com>
2016-04-28 18:53:22 +0300

    array hw

---
+++ 3. CSharp2/1-Arrays/11-Binary-Search/BinarySearch.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace _11_Binary_Search
+{
+    class BinarySearch
+    {
+        static void Main()
+        {
+            //input 
+            int arraySize = int.Parse(Console.ReadLine());
+
+            // get array
+            int[] arrayA = new int[arraySize];
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                arrayA[i] = int.Parse(Console.ReadLine());
+            }
+
+            // get Number X to find
+            int toFind = int.Parse(Console.ReadLine());
+
+            // vars
+            int Left = 0;
+            int Right = arraySize - 1;
+            int Middle = (Left + Right) / 2;
+
+            int currCheck = Middle;
+
+            // Step 1: Validate
+            if (Left > Right)
+            {
+                return;
+            }
+
+            // Step 1.5: Sort array ascending ( possibly not needed )
+
+            // Step 2:
+            while (true)
+            {
+                // Step 3: Check Numer at current Position
+                if (arrayA[currCheck] == toFind)
+                {
+                    // successful match
+                    // print the current position
+                    Console.WriteLine(currCheck);
+                    return;
+                }
+                else if (toFind < arrayA[currCheck])
+                {
+                    // value toFind is to the LEFT of MID
+                    // Move the search position LEFT
+                    currCheck--;
+                }
+                else if (arrayA[currCheck] < toFind)
+                {
+                    // value toFind is to the RIGHT of MID
+                    // Move the search position RIGHT
+                    currCheck++;
+                }
+
+                // Stop on end of array
+                if (currCheck == Left || currCheck == Right)
+                {
+                    //Print not found
+                    Console.WriteLine("-1");
+                    return;
+                }
+            }
+        }
+    }
+}
