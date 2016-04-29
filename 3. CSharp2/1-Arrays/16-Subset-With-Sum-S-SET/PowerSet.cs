5b8a4ed81db4fcff585a36a50ce97d626e6186c1
shakuu <todorov.h@gmail.com>
2016-04-29 22:43:44 +0300

    PowerSet

---
+++ 3. CSharp2/1-Arrays/16-Subset-With-Sum-S-SET/PowerSet.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//http://www.rosettacode.org/wiki/Power_set#C.23
+// Power Set
+
+namespace _16_Subset_With_Sum_S_SET
+{
+    class PowerSet
+    {
+        static void Main()      // INCORRECT - subSEQUENCE NOT subSET
+        {
+            // input
+            int arraySize = int.Parse(Console.ReadLine());
+
+            // fill the array
+            int[] toSearch = new int[arraySize];
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                toSearch[i] = int.Parse(Console.ReadLine());
+            }
+
+            // get the sum
+            int toFindSum = int.Parse(Console.ReadLine());
+
+            var currQuery =
+                from subsetCount in Enumerable.Range(0, 1<<toSearch.Count()) // 2^count subsets
+                    select
+                         from numb in Enumerable.Range(0, toSearch.Count()) // each subset containing a subset
+                         where (subsetCount & (1 << numb)) != 0             // of individial items ( numbers )
+                         select toSearch[numb];
+
+            //Console.Write(string.Join(Environment.NewLine,
+            //            currQuery.Select(subset =>
+            //                    string.Join(",", subset.Select(clr => clr.ToString()).ToArray())).ToArray()));
+
+            foreach (var item in currQuery)
+            {
+                if (item.Sum() == toFindSum)
+                {
+                    Console.WriteLine("yes");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ 3. CSharp2/1-Arrays/16-Subset-With-Sum-S/SubsetWithSumS.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Subset_With_Sum_S
+{
+    class SubsetWithSumS
+    {
+        static void Main()      // INCORRECT - subSEQUENCE NOT subSET
+        {
+            // input
+            int arraySize = int.Parse(Console.ReadLine());
+
+            // fill the array
+            int[] toSearch = new int[arraySize];
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                toSearch[i] = int.Parse(Console.ReadLine());
+            }
+
+            // get the sum
+            int toFindSum = int.Parse(Console.ReadLine());
+
+            var currQuery =
+                from toStart in Enumerable.Range(0, toSearch.Length)
+                from Length in Enumerable.Range(0, toSearch.Length-toStart)
+
+                let SubSets = toSearch.Skip(toStart).Take(Length)
+
+                orderby SubSets.Count() descending
+
+                where SubSets.Sum() == toFindSum
+
+                select SubSets;
+
+            if (currQuery.Count() > 0)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+            // test
+            foreach (var item in currQuery.First())
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
