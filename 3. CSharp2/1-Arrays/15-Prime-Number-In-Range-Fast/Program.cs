d1db49f2f0b249fd6bebc85f1de470a004a98ead
shakuu <todorov.h@gmail.com>
2016-04-29 20:45:33 +0300

    fast prime search

---
+++ 3. CSharp2/1-Arrays/15-Prime-Number-In-Range-Fast/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _15_Prime_Number_In_Range_Fast
+{
+    class SieveOfErathosthene
+    {
+        static void Main()
+        {
+            // input Range N 
+            int Range = int.Parse(Console.ReadLine());
+
+            // List of Odd Numbers
+            List<int> ListOfPrimes = Enumerable.Range(0, Range + 1).ToList();
+            List<int> Flags = Enumerable.Range(0, Range + 1).Select(num => num * 0).ToList();
+            
+            // Variables
+            int currStep = 2;
+
+            while (true)
+            {
+                // stop if square of current step is larger than range
+                if (currStep * currStep > Range)
+                {
+                    break;
+                }
+
+                // Each element
+                for (int i = currStep * currStep; i < ListOfPrimes.Count; i+= currStep)
+                {
+                    Flags[i] = 1;
+                }
+
+                // find the next unflagged number
+                for (int i = currStep+1; i < ListOfPrimes.Count; i++)
+                {
+                    if (Flags[i] == 0)
+                    {
+                        currStep = ListOfPrimes[i];
+                        break;
+                    }
+                }
+            }
+
+            // Find the First Unflagged Number
+            int toPrint = -1;
+
+            for (int i = ListOfPrimes.Count-1; i >= 0; i--)
+            {
+                if (Flags[i] == 0)
+                {
+                    toPrint = ListOfPrimes[i];
+                    break;
+                }
+            }
+            
+            Console.WriteLine(toPrint);
+        }
+    }
+}
---
+++ 3. CSharp2/1-Arrays/15-Prime-Number-In-Range/SieveOfErathosthene.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _15_Prime_Number_In_Range
+{
+    class SieveOfErathosthene
+    {
+        static void Main()
+        {
+            // input Range N 
+            int Range = int.Parse(Console.ReadLine());
+
+            // List of Odd Numbers
+            List<int> ListOfPrimes = new List<int>();
+            ListOfPrimes.Add(0);
+            ListOfPrimes.Add(1);
+            ListOfPrimes.Add(2);
+
+            // generate a list
+            for (int i = 3; i <= Range; i+=2)
+            {
+                ListOfPrimes.Add(i);
+            }
+            
+            // Variables
+            int currStep = 3;
+            int currStepIndex = 3;
+
+            while (true)
+            {
+                if (currStep * currStep > Range)
+                {
+                    break;
+                }
+
+                // Each element
+                for (int index = currStepIndex + 1; index < ListOfPrimes.Count; index++)
+                {
+                    // check if it can be divided by the current step
+                    if (ListOfPrimes[index] % currStep == 0)
+                    {
+                        // if YES .. remove it , NOT Prime
+                        ListOfPrimes.RemoveAt(index);
+                        index--;
+                    }
+                }
+
+                // increment within the range of the List
+                if (!(currStepIndex + 1 >= ListOfPrimes.Count))
+                {
+                    currStepIndex++;
+                }
+                else
+                {
+                    break;
+                }
+
+                // increment the step
+                currStep = ListOfPrimes[currStepIndex];
+            }
+
+            Console.WriteLine(ListOfPrimes.Last());
+        }
+    }
+}
