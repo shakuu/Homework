332d708b7227ab488e2811c89ac0880adc6f22ab
shakuu <todorov.h@gmail.com>
2016-04-30 13:56:10 +0300

    All Variations

---
+++ 3. CSharp2/1-Arrays/20-Variations-Of-Set/VariationsOfSet.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _20_Variations_Of_Set
+{
+    class VariationsOfSet
+    {
+        static void Main()
+        {
+            // input
+            int arraySize = int.Parse(Console.ReadLine());
+
+            int VariationSize = int.Parse(Console.ReadLine());
+
+            // get the array
+            List<int> toSearch = new List<int>();
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                toSearch.Add(i);
+            }
+
+            // Recursively Build a sequence
+            // with Length -> VariationSize
+            GetVariations(toSearch, 0, VariationSize);
+
+        }
+
+        public static void GetVariations(List<int> toSearch, int Left, int LengthToBuild)
+        {
+            // If This is the last element in the squence
+            // get all possible variations and return
+            if (LengthToBuild==1)     
+            {
+                // Step 1: Print Variations
+                for (int position = Left; position < toSearch.Count; position++)
+                {
+                    // Step 1: Print Current
+                    PrintCurrentVariation(toSearch, Left);
+
+                    // Step 2: Shuffle
+                    toSearch.Insert(Left, toSearch[toSearch.Count - 1]);
+                    toSearch.RemoveAt(toSearch.Count - 1);
+                }
+
+                Console.WriteLine("variation");
+
+                // Step 2: Go back up
+                return;
+            }
+
+            // OtherWise pick the next element in the sequence 
+            // and Send down
+            for (int currElement = 0; currElement < toSearch.Count; currElement++)
+            {
+                // Step 1: Get Variatopms
+                GetVariations(toSearch, Left + 1, LengthToBuild-1);
+
+                // Step 2: Shuffle
+                // last element goes first
+                toSearch.Insert(Left, toSearch[toSearch.Count - 1]);
+                toSearch.RemoveAt(toSearch.Count - 1);
+            }
+
+            return;
+        }
+
+        public static void PrintCurrentVariation(List<int> toPrint, int Left)
+        {
+            // Current Variaton 0 to <= Left 
+            List<int> curVariation = new List<int>();
+
+            for (int index = 0; index <= Left; index++)
+            {
+                curVariation.Add(toPrint[index]);
+            }
+
+            string Print = string.Join(", ", curVariation);
+
+            Console.WriteLine("{" + Print + "}");
+
+            return;
+        }
+    }
+}
---
+++ 3. CSharp2/1-Arrays/20-Variatons-Of-Set-With-Same-Number/VariationsWithSameNumber.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _20_Variatons_Of_Set_With_Same_Number
+{
+    class VariationsWithSameNumber
+    {
+        static void Main()
+        {
+            // input
+            int arraySize = int.Parse(Console.ReadLine());
+
+            int VariationSize = int.Parse(Console.ReadLine());
+
+            // get the array
+            List<int> toSearch = new List<int>();
+
+            for (int i = 1; i < arraySize+1; i++)
+            {
+                toSearch.Add(i);
+            }
+
+            List<int> curVariation = new List<int>();
+            for (int i = 0; i < VariationSize; i++)
+            {
+                curVariation.Add(0);
+            }
+
+            // Recursively Build a sequence
+            // with Length -> VariationSize
+            GetVariations(toSearch, 0, VariationSize, curVariation);
+
+        }
+
+        public static void GetVariations(List<int> toSearch, int Left, int LengthToBuild, List<int> curVariation)
+        {
+            // If This is the last element in the squence
+            // get all possible variations and return
+            if (LengthToBuild == 1)
+            {
+                // Step 1: Print Variations
+                for (int element = 0; element < toSearch.Count; element++)
+                {
+                    // Fill The last Element of the Variation
+                    // with each element of the input array
+                    curVariation[Left] = toSearch[element];
+
+                    // Print the current Variation
+                    PrintCurrentVariation(curVariation);
+                }
+
+                // Step 2: Go back up
+                return;
+            }
+
+            // OtherWise pick the next element in the sequence 
+            // and Send down
+            for (int currElement = 0; currElement < toSearch.Count; currElement++)
+            {
+                // Step 1: Get current Element
+                // out of ALL elements
+                curVariation[Left] = toSearch[currElement];
+
+                // Step 2: Get The Next Element
+                GetVariations(toSearch, Left + 1, LengthToBuild - 1, curVariation);
+            }
+
+            return;
+        }
+
+        public static void PrintCurrentVariation(List<int> toPrint)
+        {
+            // Format and Print
+            string Print = string.Join(", ", toPrint);
+
+            Console.WriteLine("{" + Print + "}");
+
+            return;
+        }
+    }
+}
