e3f49a5b9d267c3f211550ed96332aad2d04af83
shakuu <todorov.h@gmail.com>
2016-05-02 14:02:39 +0300

    Reversed Number

---
+++ [C#]-02-CSharp2/03-Methods/07-Reverse-Number/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _07_Reverse_Number
+{
+    class Program
+    {
+        static void Main()
+        {
+            // TODO: Case Negative
+
+            //input
+            string toReverse = Console.ReadLine();
+
+            // variables
+            bool isNegative = false;
+
+            // Check for negative
+            if (toReverse.Contains("-"))
+            {
+                isNegative = true;
+
+                toReverse = toReverse.Remove('-');
+            }
+
+            // output
+            if (!isNegative)
+            {
+                Console.WriteLine(GetReverse(toReverse));
+            }
+            else
+            {
+                Console.WriteLine("-" + GetReverse(toReverse));
+            }
+
+        }
+
+        public static string GetReverse(string toReverse)
+        {
+            // store the reversed string
+            string ReversedInput = "";
+
+            for (int curDigit = toReverse.Length - 1;   // Read input
+                     curDigit >= 0;                     // Last to First
+                     curDigit--)                        //
+            {
+                ReversedInput += toReverse[curDigit];   // write in a new string
+            }
+            // return the new , reversed string
+            return ReversedInput;
+        }
+    }
+}
