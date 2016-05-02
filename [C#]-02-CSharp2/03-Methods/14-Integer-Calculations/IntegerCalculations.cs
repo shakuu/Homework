783b359ddedaf6e5755ccf9fa2683aaed140de38
shakuu <todorov.h@gmail.com>
2016-05-02 21:36:44 +0300

    Integer Calculations add .Trim to reading strings to avoid exception on trailing white space

---
+++ [C#]-02-CSharp2/03-Methods/14-Integer-Calculations/IntegerCalculations.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Integer_Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            int[] Numbers = Console.ReadLine()
+                            .Trim(' ')          // trim trailing and leading white spaces
+                            .Split(' ')
+                            .Select(number => int.Parse(number))
+                            .ToArray();
+
+            GetMin(Numbers);
+            GetMax(Numbers);
+            GetAverage(Numbers);
+            GetSum(Numbers);
+            GetProduct(Numbers);
+        }
+
+        // Get Minimum
+        public static void GetMin(params int[] Numbers)
+        {
+            int MinNumber = int.MaxValue;
+
+            for (int curNumber = 0; curNumber < Numbers.Length; curNumber++)
+            {
+                if (Numbers[curNumber] < MinNumber)
+                {
+                    MinNumber = Numbers[curNumber];
+                }
+            }
+
+            Console.WriteLine(MinNumber);
+        }
+
+        // Get Maximum
+        public static void GetMax(params int[] Numbers)
+
+
+        {
+            int MaxNumber = int.MinValue;
+
+            for (int curNumber = 0; curNumber < Numbers.Length; curNumber++)
+            {
+                if (Numbers[curNumber] > MaxNumber)
+                {
+                    MaxNumber = Numbers[curNumber];
+                }
+            }
+
+            Console.WriteLine(MaxNumber);
+        }
+
+        // Get Average
+        public static void GetAverage(params int[] Numbers)
+        {
+            int NumbersSum = 0;
+
+            for (int curNumber = 0; curNumber < Numbers.Length; curNumber++)
+            {
+                NumbersSum += Numbers[curNumber];
+            }
+
+            Console.WriteLine((double)NumbersSum/(double)Numbers.Length);
+        }
+
+        // Get Sum
+        public static void GetSum(params int[] Numbers)
+        {
+            int NumbersSum = 0;
+
+            for (int curNumber = 0; curNumber < Numbers.Length; curNumber++)
+            {
+                NumbersSum += Numbers[curNumber];
+            }
+
+            Console.WriteLine(NumbersSum);
+        }
+
+        // Get Product
+        public static void GetProduct(params int[] Numbers)
+        {
+            int NumbersProduct = 1;
+
+            for (int curNumber = 0; curNumber < Numbers.Length; curNumber++)
+            {
+                NumbersProduct *= Numbers[curNumber];
+            }
+
+            Console.WriteLine(NumbersProduct);
+        }
+    }
+}
