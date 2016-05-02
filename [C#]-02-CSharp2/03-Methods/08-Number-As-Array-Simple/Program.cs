b56a994fe2cfa3bef4e6522bcc48f1cd8f5a26b2
shakuu <todorov.h@gmail.com>
2016-05-02 15:14:41 +0300

    Numbers as Array simple

---
+++ [C#]-02-CSharp2/03-Methods/08-Number-As-Array-Simple/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Number_As_Array_Simple
+{
+    class Program
+    {
+        static void Main()
+        {
+            // input
+            Console.ReadLine();     // input line 1 array size
+
+            // Convert input lines to numbers and add them
+            // Convert ouput to require output
+        }
+    }
+}
