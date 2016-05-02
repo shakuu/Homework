dd9da41615711a9a1fc9e92ef46ad1e6685f2cf1
shakuu <todorov.h@gmail.com>
2016-05-02 12:48:51 +0300

    English Digit

---
+++ [C#]-02-CSharp2/03-Methods/03-English-Digit/EnglishDigit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03_English_Digit
+{
+    class EnglishDigit
+    {
+        static void Main()
+        {
+            //input
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            // output
+            Console.WriteLine(GetTheWord(inputNumber));
+        }
+
+        public static string GetTheWord(int Number)
+        {
+            // split the names of digits into an array for easy access
+            string[] Words = "zero one two three four five six seven eight nine".Split(' ');
+
+            // return the appropriate string
+            return Words[Number % 10];
+        }
+    }
+}
