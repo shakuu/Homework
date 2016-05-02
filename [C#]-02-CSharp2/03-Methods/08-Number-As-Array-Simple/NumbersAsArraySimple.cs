76f794c66d5bfb52628237baaa80690a18168c3b
shakuu <todorov.h@gmail.com>
2016-05-02 15:43:01 +0300

    Number as Array wihtout manual sum - convert input to numbers - sum - convert number to required output format

---
+++ [C#]-02-CSharp2/03-Methods/08-Number-As-Array-Simple/NumbersAsArraySimple.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _08_Number_As_Array_Simple
+{
+    class NumbersAsArraySimple
+    {
+        static void Main()
+        {
+            // input
+            Console.ReadLine();     // input line 1 array size
+
+            // Convert input lines to numbers and add them
+            // Convert ouput to require output
+
+            string NumberOne = Console.ReadLine();
+            string NumberTwo = Console.ReadLine();
+
+            // find the sum
+            int Result = GetNumber(NumberOne) + GetNumber(NumberTwo);
+
+            //output
+            Console.WriteLine(GetString(Result));
+
+        }
+
+        public static int GetNumber(string InputFormat)
+        {
+            string tempString = "";
+
+            InputFormat = InputFormat.Replace(" ", "");
+
+            for (int digit = InputFormat.Length - 1;
+                     digit >= 0;
+                     digit--)
+            {
+                tempString += InputFormat[digit];
+            }
+
+            return int.Parse(tempString);
+        }
+
+        public static string GetString(int Number)
+        {
+            string OutputFormat = "";
+
+            string NumToString = Number.ToString();
+
+            for (int digit = NumToString.Length - 1;
+                     digit >= 0; 
+                     digit--)
+            {
+                OutputFormat += NumToString[digit];
+                OutputFormat += " ";
+            }
+
+            OutputFormat = OutputFormat.TrimEnd(' ');
+
+            return OutputFormat;
+        }
+    }
+}
