175a551936cc4ecb282521ddaf4c341e5686202b
shakuu <todorov.h@gmail.com>
2016-05-02 21:13:21 +0300

    Adding Polynomials TODO: Double Check Description, input, output, lots unclear

---
+++ [C#]-02-CSharp2/03-Methods/11-Adding-Polynomals/AddingPolynomials.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Adding_Polynomals
+{
+    class AddingPolynomials
+    {
+        static void Main()
+        {
+            // TODO: DOUBLE CHECK INPUT AND OUTPUT
+
+            // input
+            Console.ReadLine(); // additional number N ? 
+
+            // polynomials as an array of their coefficients
+            int[] PolynomialOne = Console.ReadLine()
+                                  .Split(' ')
+                                  .Select(int.Parse)
+                                  .ToArray();
+
+            int[] PolynomialTwo = Console.ReadLine()
+                                  .Split(' ')
+                                  .Select(coefficient => int.Parse(coefficient))
+                                  .ToArray();
+
+            // Sum the two arrays
+            int[] PolynomialSum = SumTheArrays(PolynomialOne, PolynomialTwo);
+
+            // Output
+            string toPrint = string.Join(", ", PolynomialSum);
+
+            Console.WriteLine("{" + toPrint + "}");
+        }
+
+        public static int[] SumTheArrays(int[] ArrayA, int[] ArrayB)
+        {
+            // helpers
+            int[] Result = new int[Math.Max(
+                                ArrayA.Length,
+                                ArrayB.Length)];
+            var digitA = 0;
+            var digitB = 0;
+
+            for (int curDigit = 0;
+                     curDigit < Math.Max(
+                                ArrayA.Length, 
+                                ArrayB.Length); 
+                     curDigit++)
+            {
+                try { digitA = ArrayA[curDigit];}    // if index
+                catch (IndexOutOfRangeException)     // out of range
+                { digitA = 0;}                       // return 0
+
+                try { digitB = ArrayB[curDigit]; }
+                catch (IndexOutOfRangeException)
+                { digitB = 0; }
+
+                Result[curDigit] = digitA + digitB;  // sum
+            }
+
+            return Result;
+        }
+    }
+}
