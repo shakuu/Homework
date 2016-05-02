2bdeaee7cf250329ae23032b7be584af7013a0e7
shakuu <todorov.h@gmail.com>
2016-05-02 12:17:36 +0300

    say Hello

---
+++ [C#]-02-CSharp2/03-Methods/01-Say-Hello/SayHello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01_Say_Hello
+{
+    class SayHello
+    {
+        static void Main()
+        {
+            //print output
+            Console.WriteLine("Hello, {0}!", GetName());
+        }
+
+        public static string GetName()
+        {
+            // read the Name off the console
+            return Console.ReadLine();
+        }
+    }
+}
