22e612278ba09cfe62e00128883e7d76688e2406
shakuu <todorov.h@gmail.com>
2016-05-03 11:22:21 +0300

    binary short

---
+++ [C#]-02-CSharp2/04-Numeral-Systems/08-Binary-Short/BinaryShort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _08_Binary_Short
+{
+    class Program
+    {
+        static void Main()
+        {
+            // input Short 16 bit number
+            short toDisplay = short.Parse(Console.ReadLine());
+
+            string toPrint = "";
+            string[] BinKey = "0 1".Split(' ');
+
+            for (int i = 0; i < 16; i++)
+            {
+                // get current 1/ 0
+                toPrint = BinKey[(toDisplay & 1)] + toPrint;
+
+                // shift right
+                toDisplay >>= 1;
+            }
+
+            //print
+            Console.WriteLine(toPrint.PadLeft(16, '0'));
+        }
+    }
+}
