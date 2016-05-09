7c8bff21e9bad8e666bf6d27a5ab92b3052c3457
shakuu <todorov.h@gmail.com>
2016-05-09 19:32:32 +0300

    07-Sum-Integers

---
+++ 3. CSharp2/05-Using-Classes-And-Objects/07-Add-Integers/SumIntegers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _07_Add_Integers
+{
+    class SumIntegers
+    {
+        static void Main()
+        {
+            Console.WriteLine
+                (
+                    Console.ReadLine()
+                           .Trim(' ')
+                           .Split(' ')
+                           .Select(int.Parse)
+                           .ToList()
+                           .Sum()
+                );
+        }
+    }
+}
