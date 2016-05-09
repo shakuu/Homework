56d5e037500172d82385b022ab56bbcadf49e169
shakuu <todorov.h@gmail.com>
2016-05-09 18:55:54 +0300

    05-Triangle Surface by 3 Sides

---
+++ 3. CSharp2/05-Using-Classes-And-Objects/05-Triangle-Surface-3-Sides/TriangleS3Sides.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05_Triangle_Surface_3_Sides
+{
+    class Program
+    {
+        static void Main()
+        {
+            double sideA, sideB, sideC;
+
+            double halfP=
+            (
+                (((sideA = double.Parse(Console.ReadLine())) +
+                  (sideB = double.Parse(Console.ReadLine())) +
+                  (sideC = double.Parse(Console.ReadLine()))) / 2)
+            );
+
+            Console.WriteLine
+            (
+                Math.Sqrt
+                (
+                   halfP *
+                   (halfP - sideA) *
+                   (halfP - sideB) *
+                   (halfP - sideC) 
+                )
+                .ToString("F2")
+            );
+        }
+    }
+}
