e941a3098d4f9368ba949757477eb033b0299e74
shakuu <todorov.h@gmail.com>
2016-05-09 18:39:37 +0300

    03-Day-Of-Week

---
+++ 3. CSharp2/05-Using-Classes-And-Objects/03-Day-Of-Week/DayOfWeek.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03_Day_Of_Week
+{
+    class DayOfWeek
+    {
+        static void Main()
+        {
+            Console.WriteLine(DateTime.Today.DayOfWeek.ToString());
+        }
+    }
+}
