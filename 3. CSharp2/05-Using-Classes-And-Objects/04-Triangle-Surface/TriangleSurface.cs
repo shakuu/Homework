fb08e720432dd9fc8316a4beb887518333b40076
shakuu <todorov.h@gmail.com>
2016-05-09 18:44:19 +0300

    04-Triangle-Area-Side-Alt

---
+++ 3. CSharp2/05-Using-Classes-And-Objects/04-Triangle-Surface/TriangleSurface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04_Triangle_Surface
+{
+    class TriangleSurface
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine   // (h * side) / 2
+                (
+                    ((double.Parse(Console.ReadLine()) *
+                      double.Parse(Console.ReadLine())) / 2)
+                      .ToString("F2")
+                );
+        }
+    }
+}
