2e498148e444d4f889b3fd5a693e8429cb9a4b0c
shakuu <todorov.h@gmail.com>
2016-05-11 02:11:02 +0300

    StringBuilder Test

---
+++ [C#]-02-CSharp2/06-Strings-And-Text-Processing/02-Reverse-String/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace _02_Reverse_String
+{
+    class Program
+    {
+        static void Main()
+        {
+            var toReverse = Console.ReadLine();
+
+            StringBuilder ReverseInput = 
+                new StringBuilder(toReverse.Length);
+
+            foreach (var Letter in toReverse)
+            {
+                ReverseInput.Insert(0, Letter);
+            }
+
+            Console.WriteLine(ReverseInput);
+        }
+    }
+}
