b4177989ec489c6919cc2f37c967ed0f8ea9fb15
shakuu <todorov.h@gmail.com>
2016-05-11 14:51:51 +0300

    update

---
+++ [C#]-02-CSharp2/06-Strings-And-Text-Processing/08-Extract-Sentences/ExtractSentence.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _08_Extract_Sentences
+{
+    class ExtractSentence
+    {
+        static void Main()
+        {
+            // Given Sentence Separator : DOT .
+            var Separator = '.';
+
+            // Options:
+            // .Split into array -> easier
+            // .Substring 
+
+            // Alternatively can split each
+            // sentence into words. 
+            var wordToFind = "in";    //Console.ReadLine();
+
+            // Each separate word is surrounded
+            // by empty spaces.
+            var formatToFind = " {0} ";
+
+            var toFind = 
+                string.Format(
+                    formatToFind, 
+                    wordToFind);
+
+            var Sentences = Console
+                .ReadLine()
+                .Trim()
+                .Split(Separator)
+                .ToArray();
+
+            var output = new StringBuilder();
+
+            var sentenceFormat = "{0}{1}";
+
+            foreach (var sentence in Sentences)
+            {
+                if (sentence.Contains(toFind))
+                {
+                    output.Append(
+                        string.Format(
+                            sentenceFormat,
+                            sentence,
+                            Separator));
+                }
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ [C#]-02-CSharp2/06-Strings-And-Text-Processing/09-Forbidden-Words/ForbiddenWords.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Forbidden_Words
+{
+    class ForbiddenWords
+    {
+        static void Main()
+        {
+            // TODO: Alternative Solution
+            // Build each replacing string when
+            // it is needed.
+
+            var replaceWith = "*";
+
+            // Step 1: Build Dictionary
+            var ForbiddenWords = new Dictionary<string, string>();
+
+            var inputWords = Console
+                .ReadLine()
+                .Trim()
+                .Split(new char[] { ' ', ',' }, 
+                       StringSplitOptions
+                       .RemoveEmptyEntries)
+                .ToArray();
+
+            // TODO: 
+            // Add keys for: 
+            // as is 
+            // upper case
+            // lower case
+            foreach (var word in inputWords)
+            {
+                var value = new StringBuilder();
+
+                foreach (var letter in word)
+                {
+                    value.Append(replaceWith);
+                }
+
+                // Possible exception in case of
+                // duplicate keys
+                try
+                {
+                    ForbiddenWords.Add(word, value.ToString());
+                }
+                catch (System.ArgumentException)
+                {
+                    continue;
+                }
+                
+            }
+
+            // Step 2: Read the input text and 
+            // redact it with the dictionary
+            var toRedact = Console.ReadLine();
+
+            foreach (var element in ForbiddenWords)
+            {
+                toRedact = toRedact
+                    .Replace(
+                        element.Key, 
+                        element.Value);
+            }
+
+            Console.WriteLine(toRedact);
+        }
+    }
+}
