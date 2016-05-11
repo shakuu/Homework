151649f82fd3c959d2d1abcc64fbade2d86aeb59
shakuu <todorov.h@gmail.com>
2016-05-11 22:01:30 +0300

    Replace Tags

---
+++ [C#]-02-CSharp2/06-Strings-And-Text-Processing/15-Replace-Tags/ReplaceTags.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _15_Replace_Tags
+{
+    class ReplaceTags
+    {
+        static void Main()
+        {
+            // Step 1: Create a dictionary 
+            // with items and their replacements
+            var Replace = new Dictionary<string, string>()
+            {
+                { "<a href=\"", "[URL="  }, //
+                { "\">"       , "]"      }, //a
+                { "</a>"      , "[/URL]" }  //
+            };
+
+            var toParse = Console.ReadLine();
+
+            foreach (var tag in Replace)
+            {
+                toParse = toParse
+                    .Replace(
+                        tag.Key,
+                        tag.Value);
+            }
+
+            Console.WriteLine(toParse);
+        }
+    }
+}
