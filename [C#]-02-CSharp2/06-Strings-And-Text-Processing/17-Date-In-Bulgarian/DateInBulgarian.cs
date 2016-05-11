fc73a2efbe02706ce33622fbb261cf7baef9f0ce
shakuu <todorov.h@gmail.com>
2016-05-11 22:57:05 +0300

    More Strings

---
+++ [C#]-02-CSharp2/06-Strings-And-Text-Processing/17-Date-In-Bulgarian/DateInBulgarian.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Globalization;
+
+namespace _17_Date_In_Bulgarian
+{
+    class DateInBulgarian
+    {
+        static void Main()
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture =
+                CultureInfo.CreateSpecificCulture("BG");
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // Input.
+            var Date = DateTime
+                .ParseExact(
+                    Console.ReadLine(),
+                    "d.MM.yyyy H:mm:ss",
+                    CultureInfo.InvariantCulture);
+
+            Date = Date.AddHours(6);
+            Date = Date.AddMinutes(30);
+
+            Console.WriteLine(Date.ToString("d.MM.yyyy h:mm:ss dddd"));
+        }
+    }
+}
---
+++ [C#]-02-CSharp2/06-Strings-And-Text-Processing/18-Extract-Emails/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _18_Extract_Emails
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+
+            // Step 1: Create a Pattern
+            var emailPattern = @"([\w\d.]+@[\w\d.]+\.[\w\d.]+)";
+            // Step 2: Create a Regex with the Pattern
+            var emailRegex = new Regex(emailPattern);
+            //Step 3: Find Match OR MatchES for all results
+            var extractEmails = emailRegex.Matches(input);
+
+            foreach (var email in extractEmails)
+            {
+                Console.WriteLine(email.ToString());
+            }
+        }
+    }
+}
