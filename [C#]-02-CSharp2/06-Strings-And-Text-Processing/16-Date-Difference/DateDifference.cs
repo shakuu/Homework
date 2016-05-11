9aba6699d868d321d10be063e2fb95c8fd677399
shakuu <todorov.h@gmail.com>
2016-05-11 22:07:20 +0300

    Date Difference

---
+++ [C#]-02-CSharp2/06-Strings-And-Text-Processing/16-Date-Difference/DateDifference.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace _16_Date_Difference
+{
+    class DateDifference
+    {
+        static void Main()
+        {
+            // Input : 
+            // Line 1 - Date 1
+            // Line 2 - Date 2
+            var Date1 = DateTime
+                .ParseExact(
+                    Console.ReadLine(),
+                    "d.MM.yyyy",
+                    CultureInfo.InvariantCulture);
+
+            var Date2 = DateTime
+                .ParseExact(
+                    Console.ReadLine(), 
+                    "d.MM.yyyy", 
+                    CultureInfo.InvariantCulture);
+
+
+            TimeSpan TimeDifference = Date2 - Date1;
+
+            Console.WriteLine(TimeDifference.Days);
+        }
+    }
+}
