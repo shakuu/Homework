e23d25086058e4e63c40e08233c8832ca4c4757a
shakuu <todorov.h@gmail.com>
2016-05-11 20:55:23 +0300

    word dictionary

---
+++ [C#]-02-CSharp2/06-Strings-And-Text-Processing/14-Word-Dictionary/WordDictionary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14_Word_Dictionary
+{
+    class WordDictionary
+    {
+        static void Main()
+        {
+            var NewDictionary = new Dictionary<string, string>()
+            {
+                { ".NET", "platform for applications from Microsoft" },
+                { "CLR", "managed execution environment for .NET" },
+                { "namespace", "hierarchical organization of classes" }
+            };
+
+            var input = Console.ReadLine();
+
+            Console.WriteLine(NewDictionary[input]);
+        }
+    }
+}
