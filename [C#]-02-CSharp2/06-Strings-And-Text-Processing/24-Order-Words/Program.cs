e175bc815c8d5b46a3c55eeea4c33e61318abd42
shakuu <todorov.h@gmail.com>
2016-05-12 14:43:27 +0300

    Order Words

---
+++ [C#]-02-CSharp2/06-Strings-And-Text-Processing/24-Order-Words/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_Order_Words
+{
+    class Program
+    {
+        static void Main()
+        {
+            // TODO: Might need manual sorting
+
+            // input 
+            var words = Console
+                .ReadLine()
+                .Trim()
+                .Split(' ')
+                .ToArray();
+
+            Array.Sort(words);
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
