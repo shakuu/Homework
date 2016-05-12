1b9f1076e39db231b4076829594f0dd9a223e5b0
shakuu <todorov.h@gmail.com>
2016-05-12 13:52:03 +0300

    Series Of Letters

---
+++ [C#]-02-CSharp2/06-Strings-And-Text-Processing/23-Series-Of-Letters/SeriesOfLetters.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace _23_Series_Of_Letters
+{
+    class SeriesOfLetters
+    {
+        static void Main()
+        {
+            // Input - string of letters
+            var input = Console.ReadLine();
+
+            var output = new StringBuilder();
+
+            output.Append(input[0]);
+
+            foreach (var letter in input)
+            {
+                var prevLetter = output[output.Length - 1];
+
+                // if current letter is not 
+                // actually a letter skip
+                // maybe ? 
+                if (!char.IsLetter(letter))
+                {
+                    continue;
+                }
+
+                if (letter == prevLetter)
+                {
+                    continue;
+                }
+                else
+                {
+                    output.Append(letter);
+                }
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
