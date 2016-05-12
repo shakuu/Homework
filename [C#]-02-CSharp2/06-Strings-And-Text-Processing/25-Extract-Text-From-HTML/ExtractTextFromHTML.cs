d1e8cbd7e0dc410947de9cba847118d0ba69dc00
shakuu <todorov.h@gmail.com>
2016-05-12 17:32:49 +0300

    1

---
+++ [C#]-02-CSharp2/06-Strings-And-Text-Processing/25-Extract-Text-From-HTML/ExtractTextFromHTML.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace _25_Extract_Text_From_HTML
+{
+    class ExtractTextFromHTML
+    {
+        static void Main()
+        {
+            // Read text, extract 
+            // Title
+            // Body
+
+            // Step 1: Read Text
+            // multiple rows
+            var input = new StringBuilder();
+
+            string curLine;
+
+            do
+            {
+                curLine = Console.ReadLine();
+
+                input.Append(curLine);
+                input.AppendLine();
+
+            } while (curLine != "");
+
+            var strInput = input.ToString();
+
+            // Read Title , if any.
+
+            // Then Read everything else
+            var titlePattern = @".title.([\w\d.]+)./title.";
+
+            var titleRegex = new Regex(titlePattern);
+
+            var titleMatch = titleRegex.Matches(strInput);
+
+            foreach (var match in titleMatch)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
