4aa4139940009bd289972ece44c06bf725bb2869
shakuu <todorov.h@gmail.com>
2016-05-16 15:17:09 +0300

    Enter Numbers x2

---
+++ [C#]-02-CSharp2/07-Exception-Handling/02-Enter-Numbers-2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace _02_Enter_Numbers_2
+{
+    class Program
+    {
+        static string exceptionMessage = "Exception";
+
+        static int[] Numbers;
+
+        static void Main()
+        {
+            var start = 0;
+            var end = 100;
+
+            // size of input + start + end.
+            var arrSize = 10 + 2;
+
+            Numbers = new int[arrSize];
+            Numbers[0] = start;
+            Numbers[Numbers.Length - 1] = end; 
+
+            // Fill the array.
+            try
+            {
+                for (int element = 1;
+                     element < arrSize - 1;
+                     element++)
+                {
+                    Numbers[element] = int.Parse(Console.ReadLine());
+                }
+            }
+            catch (System.ArgumentException)
+            {
+                Console.WriteLine(exceptionMessage);
+                return;
+            }
+
+            // Check the array.
+            for (int element = 1; 
+                     element < arrSize - 1; 
+                     element++)
+            {
+                if (ReadNumber(element - 1, arrSize - 1))
+                {
+                    continue;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            // print
+            Console.WriteLine(string.Join(" < ", Numbers));
+        }
+
+        static bool ReadNumber(int start, int end)
+        {
+            try
+            {
+                if (!(Numbers[start] < Numbers[start+1] && 
+                      Numbers[start+1] < Numbers[end]))
+                {
+                    throw new Exception(exceptionMessage);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+
+
+            return true;
+        }
+    }
+}
---
+++ [C#]-02-CSharp2/07-Exception-Handling/02-Enter-Numbers/EnterNumbers.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Enter_Numbers
+{
+    class EnterNumbers
+    {
+        static string exceptionMessage = "Exception";
+
+        static void Main()
+        {
+            var start = 0;
+            var end = 100;
+
+            ReadNumber(start, end);
+        }
+
+        static void ReadNumber(int start, int end)
+        {
+            var elementsToAdd = 10;
+
+            var join = " < ";
+
+            var output = new StringBuilder();
+
+            var prevNumber = start;
+
+            // Add first element.
+            output.Append(start);
+            output.Append(join);
+
+            // Add Input.
+            for (int element = 0; 
+                     element < elementsToAdd; 
+                     element++)
+            {
+                try
+                {
+                    var numberToAdd = int.Parse(Console.ReadLine());
+
+                    if (prevNumber < numberToAdd)
+                    {
+                        output.Append(numberToAdd);
+                        output.Append(join);
+                    }
+                    else
+                    {
+                        throw new Exception(exceptionMessage);
+                    }
+
+                    prevNumber = numberToAdd;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            }
+
+            // Add last element.
+            output.Append(end);
+
+            Console.WriteLine(output);
+        }
+    }
+}
