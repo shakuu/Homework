90871a1c6f2896a47f275a7263cae99f0d7b76db
shakuu <todorov.h@gmail.com>
2016-05-16 21:02:12 +0300

    read file contents with all exceptions

--- [C#]-02-CSharp2/07-Exception-Handling/03-Read-File-Contents/ReadFileContents.cs
+++ [C#]-02-CSharp2/07-Exception-Handling/03-Read-File-Contents/ReadFileContents.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Security;
 
 namespace _03_Read_File_Contents
 {
@@ -10,9 +8,70 @@ namespace _03_Read_File_Contents
     {
         static void Main()
         {
+            // https://msdn.microsoft.com/en-us/library/ms143368(v=vs.110).aspx
+            // List of all possible exceptions 
+            // related to File.Readalltext()
+
             var path = Console.ReadLine();
 
+            string fileContent;
 
+            try
+            {
+                fileContent = File.ReadAllText(path);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            catch (PathTooLongException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            catch (NotSupportedException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            catch (SecurityException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            if (fileContent == null)
+            {
+                return;
+            }
+
+            Console.WriteLine(fileContent);
         }
     }
 }
