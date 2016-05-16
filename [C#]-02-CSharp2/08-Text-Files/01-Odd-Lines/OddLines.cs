4c1efb7bc6a74cfcde190b768dbede0520d9db0b
shakuu <todorov.h@gmail.com>
2016-05-16 21:55:57 +0300

    text files 01-Odd Lines

---
+++ [C#]-02-CSharp2/08-Text-Files/01-Odd-Lines/OddLines.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using System.Threading;
+using System.Text;
+
+namespace _01_Odd_Lines
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            var testFile = @"D:\GitHub\notfound.html"; // Console.ReadLine();
+
+            // 
+            using (var stream = new StreamReader(testFile))
+            {
+                var currentLine = stream.ReadLine();
+                var counter = 0;
+
+                do
+                {
+                    if ((counter++ & 1) != 0)
+                    {
+                        Console.WriteLine(currentLine);
+                    }
+
+                    currentLine = stream.ReadLine();
+
+                } while (currentLine != null);
+            }
+        }
+    }
+}
