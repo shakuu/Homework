7eef2f5eafe191258375812197c1a23dd4e9cc18
shakuu <todorov.h@gmail.com>
2016-05-16 14:39:51 +0300

    exception handlling HW 01

---
+++ [C#]-02-CSharp2/07-Exception-Handling/01-Sqare-Root/SquareRoot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01_Sqare_Root
+{
+    class SquareRoot
+    {
+        static void Main()
+        {
+            // Output strings
+            var byebye = "Good bye";
+            var error = "Invalid number";
+
+            try
+            {
+                var input = double.Parse(Console.ReadLine());
+                var sqrt = Math.Sqrt(input).ToString("F3");
+
+                if (sqrt == "NaN")
+                {
+                    throw new System.FormatException();
+                }
+                else
+                {
+                    Console.WriteLine(sqrt);
+                }
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine(error);
+            }
+            finally
+            {
+                Console.WriteLine(byebye);
+            }            
+        }
+    }
+}
---
+++ [C#]-02-CSharp2/07-Exception-Handling/02-Enter-Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Enter_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var exceptionMessage = "Exception";
+
+            var arraySize = 10;
+            var start = 0;
+            var end = 100;
+
+
+            try
+            {
+                throw new Exception(exceptionMessage);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
