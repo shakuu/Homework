27b34b8a985ab440e51c4c40abab19c9b27dc4ca
shakuu <todorov.h@gmail.com>
2016-05-16 21:21:01 +0300

    download a file

---
+++ [C#]-02-CSharp2/07-Exception-Handling/04-Download-File/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace _04_Download_File
+{
+    class Program
+    {
+        static void Main()
+        {
+            var toDownload = Console.ReadLine();
+            var targetFolder = Console.ReadLine();
+
+            // https://msdn.microsoft.com/en-us/library/ez801hhe(v=vs.110).aspx
+            // List of exceptions related to
+            // WebClient.DownloadFile
+            try
+            {
+                using (var webclient = new WebClient())
+                {
+                    webclient.DownloadFile(toDownload, targetFolder);
+                }
+            }
+            catch (ArgumentNullException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (WebException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch(NotSupportedException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
