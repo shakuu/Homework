c47271f1e91a3352df4121d24bdf4628859dbada
shakuu <todorov.h@gmail.com>
2016-05-17 00:23:58 +0300

    more files

---
+++ [C#]-02-CSharp2/08-Text-Files/04-Compare-Text-Files/CompareTextFiles.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _04_Compare_Text_Files
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Get paths to the 2 files to compare.
+            // Hardcode for testing.
+            var inputFileA = @"D:\GitHub\notfound.html"; //Console.ReadLine();
+            var inputFileB = @"D:\GitHub\notfound-with-linenumbers.html"; //Console.ReadLine();
+
+            // Input streams for both files.
+            var readerFileA = new StreamReader(inputFileA);
+            var readerFileB = new StreamReader(inputFileB);
+
+            // Read row 1 of each.
+            var rowFileA = readerFileA.ReadLine();
+            var rowFileB = readerFileB.ReadLine();
+
+            // Counters for input.
+            var equalCounter = 0;
+            var differentCounter = 0;
+
+            while (rowFileA != null && rowFileB != null)
+            {
+                if (rowFileA == rowFileB)
+                {
+                    equalCounter++;
+                }
+                else
+                {
+                    differentCounter++;
+                }
+
+                // Read next line. ( or infiloop )
+                rowFileA = readerFileA.ReadLine();
+                rowFileB = readerFileB.ReadLine();
+            }
+
+            // Remember to close the streams.
+            readerFileA.Close();
+            readerFileB.Close();
+
+            Console.WriteLine($"Equal Rows: {equalCounter}\nDifferent Rows: {differentCounter}");
+        }
+    }
+}
