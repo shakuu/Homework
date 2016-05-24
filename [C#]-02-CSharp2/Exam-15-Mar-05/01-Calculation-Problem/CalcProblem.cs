b99bad676b14f232c23fea6a3c126e85e1b30bd3
shakuu <todorov.h@gmail.com>
2016-05-25 01:22:58 +0300

    calc problem

---
+++ [C#]-02-CSharp2/Exam-15-Mar-05/01-Calculation-Problem/CalcProblem.cs
@@ -0,0 +1,69 @@
+﻿
+namespace _01_Calculation_Problem
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    class CalcProblem
+    {
+        // Static Helpers for ease of access.
+        static int fromBase = 23;
+
+        static string Catphabet = "abcdefghijklmnopqrstuvwxyz"
+                .Substring(0, fromBase);
+
+        static void Main()
+        {
+            // Get Input.
+            var input = Console.ReadLine()
+                .Trim()
+                .Split(' ')
+                .Select(x => x.ToLower())
+                .ToArray();
+
+            // Step 1: Get Sum
+            // Step 2: Convert to decimal
+            var decimalSum = OriginalToDec(input);
+            var catphabetSum = DecToCatphabet(decimalSum);
+
+            Console.WriteLine("{0} = {1}", catphabetSum, decimalSum);
+
+        }
+
+        static string DecToCatphabet(int decimalSum)
+        {
+            var toReturn = new StringBuilder();
+
+            while (decimalSum > 0)
+            {
+                var curChar = Catphabet[decimalSum % fromBase];
+
+                toReturn.Insert(0, curChar);
+
+                decimalSum /= fromBase;
+            }
+
+            return toReturn.ToString();
+        }
+
+        static int OriginalToDec(string[] input)
+        {
+            var totalSum = 0;
+
+            foreach (var word in input)
+            {
+                var curSum = 0;
+
+                foreach (var letter in word)
+                {
+                    curSum = Catphabet.IndexOf(letter) + curSum * fromBase;
+                }
+
+                totalSum += curSum;
+            }
+
+            return totalSum;
+        }
+    }
+}
