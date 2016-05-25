e14c0b29d2aa1c0cd9652ac1b0eb76146897f73c
shakuu <todorov.h@gmail.com>
2016-05-26 00:55:15 +0300

    hw

---
+++ [C#]-02-CSharp2/06-Strings-And-Text-Processing/08-Extract-Sentences-8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _08_Extract_Sentences_8
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            var wordToFind = Console.ReadLine();
+            var input = Console.ReadLine();
+
+            var sentences = input.Split(new[] {'.'}, StringSplitOptions.RemoveEmptyEntries);
+            var separators = input.Where(x => !char.IsLetter(x)).Distinct().ToArray();
+
+            var wordCapitalized = wordToFind.Replace(wordToFind[0], char.ToUpper(wordToFind[0]));
+            var wordNormalized = wordToFind.Replace(wordToFind[0], char.ToLower(wordToFind[0]));
+
+            foreach (var sentence in sentences)
+            {
+                var words = sentence.Split(separators, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (words[i].Trim() == wordToFind)
+                    {
+                        Console.Write(sentence.Trim() + ". ");
+                        break;
+                    }
+                }
+            }
+            
+        }
+    }
+}
+
---
+++ [C#]-02-CSharp2/06-Strings-And-Text-Processing/10-Unicode-Characters-4/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace _10_Unicode_Characters_4
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
