7e04e838bcb1b8d1c56b500ee423afd1c4b0933d
shakuu <todorov.h@gmail.com>
2016-05-25 03:07:53 +0300

    next exam

---
+++ [C#]-02-CSharp2/Exam-15-Mar-05/02-Even-Differences/EvenDifferences.cs
@@ -0,0 +1,45 @@
+﻿
+namespace _02_Even_Differences
+{
+    using System;
+    using System.Linq;
+    using System.Numerics;
+
+    class EvenDifferences
+    {
+        static void Main()
+        {
+            // Get the Input 
+            var input = Console.ReadLine()
+                .Trim()
+                .Split(' ')
+                .Select(BigInteger.Parse)
+                .ToArray();
+
+            // Start at Index 1 per definition
+            var inputSize = input.Length;
+            var startIndex = 1;
+
+            BigInteger outputSum = 0;
+
+            for (int curIndex = startIndex; 
+                     curIndex < inputSize; 
+                     curIndex++)
+            {
+                var absoluteDifference = BigInteger.Parse(
+                     (input[curIndex] - input[curIndex - 1])
+                     .ToString().Replace("-", string.Empty));
+
+                // Default step is ONE -> Odd jump
+                // If even jump increment index by 1 
+                if ((absoluteDifference & 1) == 0)
+                {
+                    curIndex++;
+                    outputSum += absoluteDifference;
+                }
+            }
+
+            Console.WriteLine(outputSum);
+        }
+    }
+}
---
+++ [C#]-02-CSharp2/Exam-15-Mar-05/03-Lover-of-Three/Program.cs
@@ -0,0 +1,121 @@
+﻿
+namespace _03_Lover_of_Three
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Numerics;
+
+    class Program
+    {
+        static void Main()
+        {
+            var directions = new Dictionary<string, string>()
+            {
+                { "RU", "-1 1" },
+                { "UR", "-1 1" },
+                { "LU", "-1 -1" },
+                { "UL", "-1 -1" },
+                { "DL", "1 -1" },
+                { "LD", "1 -1" },
+                { "DR", "1 1" },
+                { "RD", "1 1" },
+            };
+
+            // Input Matrix Size 0 row 1 col
+            var matrixSizes = Console.ReadLine()
+                .Trim()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            var matrix = FillTheMatrix(matrixSizes);
+
+            var inputLines = int.Parse(Console.ReadLine());
+
+            // Start at botom left corner
+            var posRow = matrixSizes[0] - 1;
+            var posCol = 0;
+
+            BigInteger outputSum = 0;
+            var lastCell = 0;
+
+            for (int line = 0; line < inputLines; line++)
+            {
+                var inputLine = Console.ReadLine()
+                    .Trim()
+                    .Split(' ')
+                    .ToArray();
+
+                var speed = directions[inputLine[0]]
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var moves = int.Parse(inputLine[1]);
+
+                // Move the pawn
+                // Step 1: Add current Position again
+                //outputSum += lastCell;
+
+                for (int move = 0; move < moves; move++)
+                {
+                    try
+                    {
+                        lastCell = matrix[posRow, posCol];
+                        matrix[posRow, posCol] = 0;
+
+                        outputSum += lastCell;
+
+                        if (move != moves-1)
+                        {
+                            posRow += speed[0];
+                            posCol += speed[1];
+                        }
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        posRow -= speed[0];
+                        posCol -= speed[1];
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(outputSum);
+        }
+
+        // Get the matrix in the correct format
+        static int[,] FillTheMatrix(int[] size)
+
+        {
+            var matrix = new int[size[0], size[1]];
+
+            var step = 3;
+            var fill = 0;
+
+            // Row 1
+            for (int col = 0; col < size[1]; col++)
+            {
+                matrix[size[0] - 1, col] = fill;
+                fill += step;
+            }
+
+            // The rest
+            for (int col = 0; col < size[1]; col++)
+            {
+                fill = matrix[size[0] - 1, col] + step;
+
+                for (int row = size[0] - 2; row >= 0; row--)
+                {
+                    matrix[row, col] = fill;
+                    fill += step;
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
