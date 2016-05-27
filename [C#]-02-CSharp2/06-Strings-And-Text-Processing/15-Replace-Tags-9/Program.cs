bfcbc7113a2f6248401fdcaf06c067555cdad54b
shakuu <todorov.h@gmail.com>
2016-05-27 13:42:58 +0300

    replace

---
+++ [C#]-02-CSharp2/06-Strings-And-Text-Processing/15-Replace-Tags-9/Program.cs
@@ -0,0 +1,39 @@
+﻿
+namespace _15_Replace_Tags_9
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static string openTag = "<a href=\"";
+        static string closeTag = "</a>";
+        static string midTag = "\">";
+
+        static void Main()
+        {
+            var toParse = Console.ReadLine().Split(new[] { openTag, closeTag }, StringSplitOptions.RemoveEmptyEntries);
+
+            var startIndex = toParse[0].Contains(midTag) ?
+                             0 : 1;
+
+            for (int strIndex = 0; strIndex < toParse.Length; strIndex++)
+            {
+                if (toParse[strIndex].Contains(midTag))
+                {
+                    // Replace
+                    var replacement = toParse[strIndex].Split( new[] { midTag }, StringSplitOptions.RemoveEmptyEntries);
+
+                    Console.Write("[{0}]({1})", replacement[1], replacement[0]);
+                }
+                else
+                {
+                    Console.Write(toParse[strIndex]);
+                }
+            }
+        }
+    }
+}
