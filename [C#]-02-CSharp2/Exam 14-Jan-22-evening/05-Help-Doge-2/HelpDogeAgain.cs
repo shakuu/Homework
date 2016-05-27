0e45df4c0d2e8bbe88a3a4f7f1109b94e7e8da37
shakuu <todorov.h@gmail.com>
2016-05-28 02:24:41 +0300

    help doge slow

---
+++ [C#]-02-CSharp2/Exam 14-Jan-22-evening/05-Help-Doge-2/HelpDogeAgain.cs
@@ -0,0 +1,93 @@
+﻿
+// Too slow 
+
+namespace _05_Help_Doge_2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class HelpDogeAgain
+    {
+        // Variables.
+        static int[] matrixSize;
+        static byte[,] matrix;
+
+        // Input fill
+        static byte enemy = 1;
+        static byte food = 2;
+
+        // Tracking vars.
+        static int successfulRuns = 0;
+
+        static void Input()
+        {
+            // Matrix Size 
+            matrixSize = Console.ReadLine()
+                .Trim()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            matrix = new byte[matrixSize[0], matrixSize[1]];
+
+            // Food coordinates.
+            var posFood = Console.ReadLine()
+                .Trim()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            matrix[posFood[0], posFood[1]] = (byte)food;
+
+            // Enemies
+            var numEnemies = int.Parse(Console.ReadLine());
+
+            for (int enemies = 0; enemies < numEnemies; enemies++)
+            {
+                var posEnemy = Console.ReadLine()
+                    .Trim()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                matrix[posEnemy[0], posEnemy[1]] = (byte)enemy;
+            }
+        }
+
+        static void CheckCell(int row, int col)
+        {
+            // Def bool state = false;
+
+            // enemy
+            if (matrix[row, col] == enemy) return;
+
+            // Food
+            else if (matrix[row, col] == food)
+            {
+                successfulRuns++;
+                return;
+            }
+
+            // Can Move ?
+
+            if (col + 1 < matrixSize[1]) { CheckCell(row, col + 1); }
+            if (row + 1 < matrixSize[0]) { CheckCell(row + 1, col); }
+
+            return;
+        }
+
+        static void Main()
+        {
+            Input();
+
+            // Send Doge on his journey
+            CheckCell(0, 1);
+            CheckCell(1, 0);
+
+            Console.WriteLine(successfulRuns);
+        }
+    }
+}
---
+++ [C#]-02-CSharp2/Exam 14-Jan-22-evening/05-Help-Doge/HelpDogeOriginal.cs
@@ -0,0 +1,88 @@
+﻿
+namespace _05_Help_Doge
+{
+    using System;
+    using System.Linq;
+
+    class HelpDogeOriginal
+    {
+        // Variables.
+        static int[] matrixSize;
+        static byte[,] matrix;
+
+        // Input fill
+        static byte enemy = 1;
+        static byte food = 2;
+
+        // Tracking vars.
+        static int successfulRuns = 0;
+
+        static void Input()
+        {
+            // Matrix Size 
+            matrixSize = Console.ReadLine()
+                .Trim()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            matrix = new byte[matrixSize[0], matrixSize[1]];
+
+            // Food coordinates.
+            var posFood = Console.ReadLine()
+                .Trim()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            matrix[posFood[0], posFood[1]] = (byte)food;
+
+            // Enemies
+            var numEnemies = int.Parse(Console.ReadLine());
+
+            for (int enemies = 0; enemies < numEnemies; enemies++)
+            {
+                var posEnemy = Console.ReadLine()
+                    .Trim()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                matrix[posEnemy[0], posEnemy[1]] = (byte)enemy;
+            }
+        }
+
+        static void CheckCell(int row, int col)
+        {
+            // Def bool state = false;
+
+            // enemy
+            if (matrix[row, col] == enemy) return;
+            
+            // Food
+            else if (matrix[row, col] == food)
+            {
+                successfulRuns++;
+                return;
+            }
+
+            // Can Move ?
+
+            if (col + 1 < matrixSize[1]) { CheckCell(row, col + 1); }
+            if (row + 1 < matrixSize[0]) { CheckCell(row + 1, col); }
+
+            return;
+        }
+
+        static void Main()
+        {
+            Input();
+
+            // Send Doge on his journey
+            CheckCell(0, 1);
+            CheckCell(1, 0);
+
+            Console.WriteLine(successfulRuns);
+        }
+    }
+}
