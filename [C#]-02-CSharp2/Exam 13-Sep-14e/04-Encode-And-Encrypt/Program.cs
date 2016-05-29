009ec2c3d127fe2c298187f23111d8f3ce50d8e9
shakuu <todorov.h@gmail.com>
2016-05-29 20:03:57 +0300

    Encode and Encrypt

---
+++ [C#]-02-CSharp2/Exam 13-Sep-14e/04-Encode-And-Encrypt/Program.cs
@@ -0,0 +1,139 @@
+﻿
+namespace _04_Encode_And_Encrypt
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        // By description
+        static char addChrA = 'A';
+
+        static StringBuilder text;
+        static StringBuilder cypher;
+        static StringBuilder temp;
+        static StringBuilder output;
+
+        static void EncryptWithTextLongerThanCypher(StringBuilder shorter, 
+                            StringBuilder longer)
+        {
+            temp = new StringBuilder();
+
+            var shorterLength = shorter.Length;
+
+            for (int sym = 0; sym < longer.Length; sym++)
+            {
+                var shorterSym = sym % shorterLength;
+
+                var symShorter = (shorter[shorterSym] - addChrA);
+                var symLonger = (longer[sym] - addChrA);
+
+                var newSymbol = (symLonger ^ symShorter) + addChrA;
+
+                temp.Append((char)newSymbol);
+            }
+        }
+
+        static void EncryptWithCypherLongerThanText(StringBuilder shorter,
+                             StringBuilder longer)
+        {
+            temp = new StringBuilder();
+
+            for (int sym = 0; sym < shorter.Length; sym++)
+            {
+                var symShorter = (shorter[sym] - addChrA);
+                var symLonger = (longer[sym] - addChrA);
+
+                var newSymbol = (symLonger ^ symShorter) + addChrA;
+
+                temp.Append((char)newSymbol);
+            }
+
+            var tempLength = temp.Length;
+
+            for (int sym = shorter.Length; sym < longer.Length; sym++)
+            {
+                var tempSym = sym % tempLength;
+
+                var symShorter = (temp[tempSym] - addChrA);
+                var symLonger = (longer[sym] - addChrA);
+
+                var newSymbol = (symLonger ^ symShorter) + addChrA;
+
+                temp.Insert(tempSym, (char)newSymbol);
+                temp.Remove(tempSym + 1, 1);
+            }
+        }
+
+        static void Encode()
+        {
+            var cypherLenght = cypher.Length;
+
+            // Add the cypher for encoding
+            temp.Append(cypher);
+
+            output = new StringBuilder();
+
+            var counter = 1;
+            var prevSym = temp[0];
+
+            for (int sym = 1; sym < temp.Length; sym++)
+            {
+                if (temp[sym] == prevSym) counter++;
+                else
+                {
+                    AppendSymbols(counter, prevSym);
+
+                    prevSym = temp[sym];
+                    counter = 1;
+                }
+            }
+
+            // Flush the remaining symbol
+            AppendSymbols(counter, prevSym);
+
+            // Add counter
+            output.Append(cypherLenght);
+        }
+
+        static void AppendSymbols(int counter, char prevSym)
+        {
+            if (counter > 2)
+            {
+                output.Append(counter);
+                output.Append(prevSym);
+            }
+            else if (counter == 2)
+            {
+                output.Append(prevSym);
+                output.Append(prevSym);
+            }
+            else
+            {
+                output.Append(prevSym);
+            }
+        }
+
+        static void Input()
+        {
+            text = new StringBuilder( Console.ReadLine());
+            cypher = new StringBuilder(Console.ReadLine());
+        }
+
+        static void Main()
+        {
+            Input();
+
+            if (text.Length < cypher.Length)
+                 EncryptWithCypherLongerThanText(text, cypher);
+            else EncryptWithTextLongerThanCypher(cypher, text);
+
+            Encode();
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ [C#]-02-CSharp2/Exam 13-Sep-14e/04-Encode-And-Encrypt/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("04-Encode-And-Encrypt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("04-Encode-And-Encrypt")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3cb6d1fe-4f47-4be3-95f0-8d6863e3d779")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
