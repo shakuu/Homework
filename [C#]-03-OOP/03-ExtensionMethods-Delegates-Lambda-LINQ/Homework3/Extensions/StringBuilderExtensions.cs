a050d8dfbfdf8d3af4c13975c48b16f0c6569a28
shakuu <todorov.h@gmail.com>
2016-06-06 13:47:27 +0300

    1

---
+++ [C#]-03-OOP/03-ExtensionMethods-Delegates-Lambda-LINQ/Homework3/Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Homework3.Extensions
+{
+    using System;
+    using System.Text;
+
+    public static class StringBuilderExtensions
+    {
+        public static StringBuilder Substring
+            (this StringBuilder stringBuilder, int index, int length)
+        {
+            if (index+length > stringBuilder.Length
+                || index >= stringBuilder.Length
+                || index < 0 )
+            {
+                throw new IndexOutOfRangeException("");
+            }
+
+            var output = new StringBuilder();
+
+            for (int i = index; i < index+length; i++)
+            {
+                output.Append(stringBuilder[i]);
+            }
+
+            return output;
+        }
+
+        public static StringBuilder Substring
+            (this StringBuilder stringBuilder, int index)
+        {
+            if (index >= stringBuilder.Length
+                || index < 0)
+            {
+                throw new IndexOutOfRangeException("");
+            }
+
+            var output = new StringBuilder();
+
+            for (int i = index; i < stringBuilder.Length; i++)
+            {
+                output.Append(stringBuilder[i]);
+            }
+
+            return output;
+        }
+    }
+}
---
+++ [C#]-03-OOP/03-ExtensionMethods-Delegates-Lambda-LINQ/Homework3/StartUp.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Homework3
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Extensions;
+    class StartUp
+    {
+        static Random rng = new Random();
+
+        static void Main()
+        {
+            var test = new StringBuilder().Append("0123456789");
+
+            Console.WriteLine(test.Substring(10));
+        }
+    }
+}
