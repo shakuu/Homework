554a5b0447033884691a3283d1bb980a8babe3db
shakuu <todorov.h@gmail.com>
2016-06-11 00:15:30 +0300

    js homework

--- [JavaScript] Basics/10-ArraysMethods/person.js
+++ [JavaScript] Basics/10-ArraysMethods/person.js
@@ -1,8 +1,8 @@
 // jshint esversion:6
 
 let Person = function (first, last, age, isfemale) {
-  this.firstname = first;
-  this.lastname = last;
+  this.firstname = String(first);
+  this.lastname = String(last);
   this.age = Number(age);
   this.isFemale = !!isfemale;
 };
@@ -10,14 +10,16 @@ Person.prototype.constructor = Person;
 let people = [];
 
 // testing 
-let p1 = new Person('pesho', 'peshev', '20', 'true');
-let p2 = new Person('goshe', 'peshev', '14', 'true');
-people.push(p1);
-people.push(p2);
+let p1 = new Person('gesho', 'peshev', '20', 'true');
+let p2 = new Person('goshe', 'peshev', '13', 'true');
+let p3 = new Person('pesho', 'geshev', '20', 'true');
+people = [p1, p2, p3]
 
 PeopleOfAge();
 GetUnderAged();
 AverageAgeOfFemales();
+YoungestPerson();
+Groups();
 
 function PeopleOfAge() {
   let result = people
@@ -43,4 +45,29 @@ function AverageAgeOfFemales() {
     }, 0) / counter;
 
   console.log('Average Age of isFemale: ' + result);
+}
+
+function YoungestPerson() {
+  let min = people[0];
+  let len = people.length - 1;
+  let result = people.find(function (per, index) {
+    if (per.age < min.age) {
+      min = per;
+    }
+    return false;
+  });
+  console.log('Youngest person is: ' + min.firstname + ' age: ' + min.age);
+}
+
+function Groups() {
+  let groups = {};
+  var output = people.reduce(function (groups, per) {
+    if (!groups[per.firstname[0]]){
+      groups[per.firstname[0]] = [per];
+    }else{
+      groups[per.firstname[0]].push(per);
+    }
+    return groups;
+  }, {});
+  console.log(output);
 }
