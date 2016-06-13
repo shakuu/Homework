76c53d56e68f048e11795b6572b99fd8db4527ad
shakuu <todorov.h@gmail.com>
2016-06-13 10:58:42 +0300

    BitArray 64: Indexer, IEnumerable TODO: overrides

--- [C#]-03-OOP/Homework-06/BitArray64Assembly/BitArray64Models/BitArray64.cs
+++ [C#]-03-OOP/Homework-06/BitArray64Assembly/BitArray64Models/BitArray64.cs
@@ -1,13 +1,16 @@
 ﻿namespace BitArray64Assembly.BitArray64Models
 {
     using System;
+    using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
-    public class BitArray64
+    public class BitArray64 : IEnumerable<int>
     {
+        private const int Size = 64;
+
         private ulong container;
 
         public BitArray64()
@@ -29,6 +32,38 @@
 
                 return result;
             }
+            set
+            {
+                if (value < 0 || 1 < value)
+                {
+                    throw new ArgumentOutOfRangeException("Bit value can be 1 or 0");
+                }
+
+                var mask = (ulong)(1 << index);
+                if (value == 1) this.container |= mask;
+                else this.container &= ~mask;
+            }
         }
+
+        #region Overrides
+
+
+        #endregion
+
+        #region Interface implementations
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int index = 0; index < Size; index++)
+            {
+                var mask = (ulong)(1 << index);
+                yield return (int)((this.container & mask) >> index);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+        #endregion
     }
 }
