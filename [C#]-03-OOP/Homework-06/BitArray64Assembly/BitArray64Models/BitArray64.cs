835f430f4324acff2df3c60b8775def11232a001
shakuu <todorov.h@gmail.com>
2016-06-13 10:30:43 +0300

    BitArray64 TODO:

---
+++ [C#]-03-OOP/Homework-06/BitArray64Assembly/BitArray64Models/BitArray64.cs
@@ -0,0 +1,34 @@
+﻿namespace BitArray64Assembly.BitArray64Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class BitArray64
+    {
+        private ulong container;
+
+        public BitArray64()
+        {
+            this.container = 0;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (!(0 <= index && index <= 63))
+                {
+                    throw new IndexOutOfRangeException("Index must be: 0 <= index <= 63");
+                }
+
+                var mask = (ulong)(1 << index);
+                var result = (int)((this.container & mask) >> index);
+
+                return result;
+            }
+        }
+    }
+}
