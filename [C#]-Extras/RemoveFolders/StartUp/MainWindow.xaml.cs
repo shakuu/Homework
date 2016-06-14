5447c57b876fcaf9b98ef60f841dd0c8fcb0eafb
shakuu <todorov.h@gmail.com>
2016-06-15 01:42:23 +0300

    Remove Folders looking better TODO: Better code

--- [C#]-Extras/RemoveFolders/StartUp/MainWindow.xaml.cs
+++ [C#]-Extras/RemoveFolders/StartUp/MainWindow.xaml.cs
@@ -1,46 +1,64 @@
 ﻿namespace StartUp
 {
     using System.Windows;
-    using System.Windows.Forms;
+    using WinForms = System.Windows.Forms;
     using RemoveFilesModels;
-
+    using System.Windows.Forms;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string Path { get; set; }
+        private const string DefaultPath = "D:\\GitHub";
+
+        public string PathToDeleteFrom { get; set; }
+        public string LastSelectedPath { get; private set; }
 
         public MainWindow()
         {
             InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            DeleteBtn.Visibility = Visibility.Collapsed;
+            DirNameTextBox.Text = string.Format("Default directory: {0}", DefaultPath);
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void FolderBtn_Click(object sender, RoutedEventArgs e)
         {
             var dialog = new FolderBrowserDialog();
-            dialog.ShowDialog();
 
-            this.Path = dialog.SelectedPath;
-            textBlock.Text = string.Format($"Current Folder: {this.Path}");
-            
+            dialog.SelectedPath = DefaultPath; // Read from file
+
+            var userInput = dialog.ShowDialog();
+            this.PathToDeleteFrom = dialog.SelectedPath;
+
+            if (userInput != WinForms.DialogResult.Cancel)
+            {
+                DeleteBtn.Visibility = Visibility.Visible;
+                this.PathToDeleteFrom = dialog.SelectedPath;
+                DirNameTextBox.Text = string
+                    .Format("{0}", this.PathToDeleteFrom);
+            }
+            else
+            {
+                return;
+            }
         }
 
-        private void delbtn_Click(object sender, RoutedEventArgs e)
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
         {
-            var box =
-                 System.Windows.Forms.MessageBox.Show("Are you sure sure?", "Confirm", MessageBoxButtons.OKCancel);
+            var userInput = WinForms.MessageBox
+                .Show(string.Format( "Are you sure sure you want to delete all /obj and /bin folders in {0}?", 
+                this.PathToDeleteFrom), 
+                "Confirm", MessageBoxButtons.OKCancel);
 
-            if (box.ToString() == "OK")
+            if (userInput == WinForms.DialogResult.OK)
             {
-                if (string.IsNullOrEmpty(this.Path))
-                {
-                    textBlock.Text = "No folder is selected";
-                }
-                else
-                {
-                    textBlock.Text = RemoveFiles.Remove(this.Path);
-                }
+                DisplayDeletedFolders.Text =
+                    RemoveFiles.Remove(this.PathToDeleteFrom);
+            }
+            else
+            {
+                DisplayDeletedFolders.Text = "Aborting...";
             }
         }
     }
