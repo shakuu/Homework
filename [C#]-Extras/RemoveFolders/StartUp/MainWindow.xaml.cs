835ad0d543f4c21c534a038b61a5d3cfbcf2c2c1
shakuu <todorov.h@gmail.com>
2016-06-14 17:49:51 +0300

    Remove Files with Ookie dialogs

--- [C#]-Extras/RemoveFolders/StartUp/MainWindow.xaml.cs
+++ [C#]-Extras/RemoveFolders/StartUp/MainWindow.xaml.cs
@@ -1,20 +1,7 @@
 ﻿namespace StartUp
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using System.Windows;
-    using System.Windows.Controls;
-    using System.Windows.Data;
-    using System.Windows.Documents;
     using System.Windows.Forms;
-    using System.Windows.Input;
-    using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
-    using System.Windows.Shapes;
     using RemoveFilesModels;
 
     /// <summary>
@@ -31,8 +18,8 @@
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            var dialog = new FolderBrowserDialog();
-            var result = dialog.ShowDialog();
+            var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+            dialog.ShowDialog();
 
             this.Path = dialog.SelectedPath;
             textBlock.Text = string.Format($"Current Folder: {this.Path}");
