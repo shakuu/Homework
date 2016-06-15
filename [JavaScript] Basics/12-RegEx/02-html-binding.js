2019886b6a839d32ff59ce599e59f52499f57e27
shakuu <todorov.h@gmail.com>
2016-06-15 23:38:32 +0300

    JS RegEx hw cleanup

--- [JavaScript] Basics/12-RegEx/02-html-binding.js
+++ [JavaScript] Basics/12-RegEx/02-html-binding.js
@@ -16,17 +16,19 @@ String.prototype.bind = function (args) {
     var insert = toInsert[result[3]];
 
     if (String(result[2]) === 'content') {
-      str = str.substr(0, index + 1) + insert + str.substr(index + 1);
+      str = str.slice(0, index + 1) + insert + str.slice(index + 1);
     } else {
       var replaced = String(result[1]).replace(result[3], insert);
       str = str.slice(0, index) + ' ' + replaced + str.slice(index);
     }
   }
 
-  // console.log(str);
   return str;
 };
 
 var str = '<div data-bind-content="name"></div>';
 str = str.bind({ name: 'Steven' });
 console.log(str);
+
+var bindingString = '<a data-bind-content="name" data-bind-href="link" data-bind-class="name"></а>';
+console.log(bindingString.bind({ name: 'Elena', link: 'http://telerikacademy.com' })); 
