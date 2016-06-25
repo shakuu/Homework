169bb09d20c48d6c98a0c19b84d8dbdc59260058
shakuu <todorov.h@gmail.com>
2016-06-25 09:34:18 +0300

    more JS hw

---
+++ [JavaScript] Basics/09-UsingObjects/01-planar.js
@@ -0,0 +1,40 @@
+function solve(args) {
+  var input = args
+    .map(Number),
+    step = 4,
+    sizes = [];
+
+  function distance(index) {
+    var arr = input;
+
+    var a = Math.abs(arr[index] - arr[index + 2]);
+    var b = Math.abs(arr[index + 1] - arr[index + 3]);
+
+    return Math.sqrt((a * a) + (b * b));
+  }
+
+  for (var i = 0; i < input.length; i += step) {
+    sizes.push(+distance(i));
+  }
+
+
+  var output;
+  if (sizes[0] + sizes[1] > sizes[2] && sizes[1] + sizes[2] > sizes[0] && sizes[0] + sizes[2] > sizes[1]) {
+    output = 'Triangle can be built';
+  } else {
+    output = 'Triangle can not be built';
+  }
+
+  console.log(
+    sizes
+      .map(Number)
+      .map(
+      function (a) {
+        return a.toFixed(2)
+      })
+      .join('\n'));
+  console.log(output);
+}
+
+
+solve(['5', '6', '7', '8', '1', '2', '3', '4', '9', '10', '11', '12']);
---
+++ [JavaScript] Basics/09-UsingObjects/input.js
@@ -0,0 +1,10 @@
+// a + b > c
+// b + c > a
+// a + c > b
+
+
+function solve(args){
+  console.log(args);
+}
+
+solve([ '5', '6', '7', '8', '1', '2', '3', '4', '9', '10', '11', '12' ]);
