4ee5888bf732fb189bfa5aca10bce9c0fee91e60
shakuu <todorov.h@gmail.com>
2016-06-30 20:35:35 +0300

    more JS stuff

--- [JavaScript] Basics/12-RegEx/02-html-binding.js
+++ [JavaScript] Basics/12-RegEx/02-html-binding.js
@@ -1,35 +1,43 @@
-String.prototype.bind = function (binding, data) {
-  // Assuming a single tag
-  var str = String(binding);
-  var toInsert = data;
+function solve(args) {
 
-  var matchDataBind = new RegExp('data-bind-(([a-z]*)="([a-z]*)")', 'g');
-  var allDataBinds = str.match(matchDataBind);
+  String.prototype.bind = function (data) {
+    // Assuming a single tag
+    var str = String(this);
+    var toInsert = data;
 
-  var innerMatching = new RegExp('data-bind-(([a-z]*)="([a-z]*)")');
-  var len = allDataBinds.length;
-  for (let i = 0; i < len; i += 1) {
-    var current = String(allDataBinds[i]);
+    var matchDataBind = new RegExp('data-bind-(([a-z]*)="([a-z]*)")', 'g');
+    var allDataBinds = str.match(matchDataBind);
 
-    var result = current.match(innerMatching);
-    var index = str.indexOf('>');
-    var insert = toInsert[result[3]];
+    var innerMatching = new RegExp('data-bind-(([a-z]*)="([a-z]*)")');
+    var len = allDataBinds.length;
 
-    if (String(result[2]) === 'content') {
-      str = str.slice(0, index + 1) + insert + str.slice(index + 1);
-    } else {
-      var replaced = String(result[1]).replace(result[3], insert);
-      str = str.slice(0, index) + ' ' + replaced + str.slice(index);
+    for (let i = 0; i < len; i += 1) {
+      var current = String(allDataBinds[i]);
+
+      var result = current.match(innerMatching);
+      var index = str.indexOf('>');
+      var insert = toInsert[result[3]];
+
+      if (String(result[2]) === 'content') {
+        str = str.slice(0, index + 1) + insert + str.slice(index + 1);
+      } else {
+        var replaced = String(result[1]).replace(result[3], insert);
+        str = str.slice(0, index) + ' ' + replaced + str.slice(index);
+      }
     }
-  }
 
-  return str;
-};
+    return str;
+  };
 
-var str = '<div data-bind-content="name"></div>';
-var binding = {name: 'Steven'};
-str = str.bind(str, {name: 'Steven'});
-console.log(str);
+  var data = JSON.parse(args[0]);
+  var str = args[1] + '';
 
-var bindingString = '<a data-bind-content="name" data-bind-href="link" data-bind-class="name"></а>';
-console.log(bindingString.bind(bindingString, { name: 'Elena', link: 'http://telerikacademy.com' })); 
+  console.log(str.bind(data));
+}
+
+var test2 = [
+  '{ "name": "Elena", "link": "http://telerikacademy.com" }',
+  '<a data-bind-content="name" data-bind-href="link" data-bind-class="name"></а>'
+];
+
+solve(test2);
