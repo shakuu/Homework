9747f1fc0bc3265c8569699f84b932967f9b6cc4
shakuu <todorov.h@gmail.com>
2016-07-04 00:48:33 +0300

    more JS stuff

--- [JavaScript] Basics/exam 14-may-20-evening/03-angular.js
+++ [JavaScript] Basics/exam 14-may-20-evening/03-angular.js
@@ -18,30 +18,156 @@ function solve(args) {
     }
 
     // Parse the text
+    // Match each possible command 
     var matchModel = /<nk-model>(.*?)<\/nk-model>/,
         matchNewTemplate = /<nk-template name="(.*?)">/,
         matchEndNewTemplate = /<\/nk-template>/,
         matchRenderTemplate = /<nk-template render="(.*?)" \/>/,
-        matchRepeatOpen = /<nk-repeat for="(.*?)">/,
+        matchRepeatOpen = /<nk-repeat for="(.*?) in (.*?)">/,
         matchRepeatClose = /<\/nk-repeat>/,
         matchConditionOpen = /<nk-if condition="(.*?)">/,
-        matchConditionClose = /<\/nk-if>/;
+        matchConditionClose = /<\/nk-if>/,
+        matchEscaped = /\{\{<nk-.*?>\}\}/;
 
-    for (i = 1; i < +args[0]; i += 1) {
-        var line = args[i] + '';
+    // States
+    var isNewTemaplate = false,
+        isRepeat = false,
+        isCondition = false,
+        toDisplay = true;
 
-        if (matchModel.test(line)) {
-            // Insert model.
-        } else if (matchNewTemplate.test(line)) {
-            // Parse new template.
-        } else if (matchRenderTemplate.test(line)) {
-            // Display Template
-        } else if (matchRepeatOpen.test(line)) {
-            // Display Repeat.
-        } else if (matchConditionOpen.test(line)) {
-            // Display Condition.
+    // Store
+    var currentCondition = [],
+        currentRepeat = [],
+        modelsRepeat = [],
+        currentRepeatModel = '',
+        currentTemplate = '',
+        toDisplayLines = [];
+
+    // Replace Mode tag
+    function replaceModel(line) {
+        var match = (line + '').match(matchModel);
+
+        if (!models[match[1]]) {
+            return line;
+        } else {
+            return (line + '').replace(matchModel, models[match[1]]);
         }
     }
+
+    // Insert Template
+    function insertTemplate(line) {
+
+        var match = (line + '').match(matchRenderTemplate);
+        var template = templates[match[1]],
+            index, result = [];
+
+        for (index in template) {
+            result.push(template[index]);
+        }
+
+        return result;
+    }
+
+    // Evaluate the condition. Return toDisplay true or false.
+    function evaluateCondition(line) {
+        var match = (line + '').match(matchConditionOpen);
+        return models[match[1]][0] === 'true';
+    }
+
+    // Evaluate repeat tag, add new model referring to the appropriate array
+    function evaluateRepeat(line) {
+        var match = (line + '').match(matchRepeatOpen);
+        currentRepeatModel = match[1];
+        modelsRepeat[match[1]] = models[match[2]];
+    }
+
+    // Insert all repeated lines into output
+    function insertRepeatSection(lines) {
+        var i, j, result = [];
+
+        for (i in modelsRepeat[currentRepeatModel]) {
+            for (j in lines) {
+                var curLine = lines[j] + '';
+
+                if (matchModel.test(curLine) && !matchEscaped.test(curLine)) {
+                    curLine = curLine.replace(matchModel, modelsRepeat[currentRepeatModel][i]);
+                }
+
+                result.push(curLine);
+            }
+        }
+
+        return result;
+    }
+
+    // Parse the tags and look for matches
+    for (i = 1; i <= +args[0]; i += 1) {
+        var line = args[i] + '';
+
+        // If the result of the condition was false.
+        // Skip lines until the end of the condition tag.
+        if (matchConditionClose.test(line)) {
+            toDisplay = true;
+            continue;
+        } else if (!toDisplay) {
+            continue;
+        }
+
+        if (matchEscaped.test(line)) {
+            // Remove the escaped symbols and push the line.
+            line = line.replace(/\{/g, '');
+            line = line.replace(/\}/g, '');
+            toDisplayLines.push(line);
+        } else if (matchModel.test(line)) {
+            // Insert model.
+            toDisplayLines.push(replaceModel(line));
+        } else if (matchNewTemplate.test(line)) {
+            // Parse new template.
+            currentTemplate = line.match(matchNewTemplate)[1];
+            templates[currentTemplate] = [];
+            isNewTemaplate = true;
+        } else if (matchRenderTemplate.test(line)) {
+            // Display Template
+            toDisplayLines = insertTemplate(line);
+        } else if (matchRepeatOpen.test(line)) {
+            // Evaluate repeat , add new model
+            evaluateRepeat(line);
+            isRepeat = true;
+        } else if (matchConditionOpen.test(line)) {
+            // Evaluate the condition.
+            toDisplay = evaluateCondition(line);
+        } else if (matchEndNewTemplate.test(line)) {
+            // Close the current template.
+            isNewTemaplate = false;
+            currentTemplate = '';
+        } else if (isNewTemaplate) {
+            // Add lines to the template until the EndNewTemplate tag is met.
+            templates[currentTemplate].push(line);
+        } else if (matchRepeatClose.test(line)) {
+            // Evaluate gathered lines
+            toDisplayLines = insertRepeatSection(currentRepeat);
+            currentRepeatModel = '';
+            currentRepeat = [];
+            isRepeat = false;
+        } else {
+            toDisplayLines.push(line);
+        }
+
+        // Display toDisplayLines[] - add them to output.
+        if (isRepeat) {
+            for (j in toDisplayLines) {
+                currentRepeat.push(toDisplayLines[j]);
+            }
+        } else {
+            for (j in toDisplayLines) {
+                output.push(toDisplayLines[j]);
+            }
+        }
+
+        toDisplayLines = [];
+    }
+
+    console.log(output.join('\r\n'));
 }
 
 module.exports = {
