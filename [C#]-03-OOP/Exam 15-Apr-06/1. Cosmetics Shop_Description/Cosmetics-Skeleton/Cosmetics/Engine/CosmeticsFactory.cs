8a762de1a41de1fbf8a1a0568bb5ace76d958c49
shakuu <todorov.h@gmail.com>
2016-07-10 21:32:44 +0300

    Cosmetics again

---
+++ [C#]-03-OOP/Exam 15-Apr-06/1. Cosmetics Shop_Description/Cosmetics-Skeleton/Cosmetics/Engine/CosmeticsFactory.cs
@@ -0,0 +1,31 @@
+﻿namespace Cosmetics.Engine
+{
+    using System.Collections.Generic;
+
+    using Cosmetics.Common;
+    using Cosmetics.Contracts;
+    using Cosmetics.Products;
+
+    public class CosmeticsFactory : ICosmeticsFactory
+    {
+        public ICategory CreateCategory(string name)
+        {
+            return new Category(name);
+        }
+
+        public IShampoo CreateShampoo(string name, string brand, decimal price, GenderType gender, uint milliliters, UsageType usage)
+        {
+            // TODO: create shampoo
+        }
+
+        public IToothpaste CreateToothpaste(string name, string brand, decimal price, GenderType gender, IList<string> ingredients)
+        {
+            // TODO: create toothpaste
+        }
+
+        public IShoppingCart ShoppingCart()
+        {
+            // TODO: create shopping cart
+        }
+    }
+}
