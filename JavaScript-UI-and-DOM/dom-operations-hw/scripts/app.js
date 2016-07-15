7395ac7755f4fdeabe328a2fbdd6408a6c24ce7b
shakuu <todorov.h@gmail.com>
2016-07-16 01:23:40 +0300

    dom element homework, tests complete

--- JavaScript-UI-and-DOM/dom-operations-hw/scripts/app.js
+++ JavaScript-UI-and-DOM/dom-operations-hw/scripts/app.js
@@ -4,11 +4,36 @@
 // Each div's content must be one of the items from the contents array
 // The function must remove all previous content from the DOM element provided
 
-var notStringError = Error('parameter not a string');
-
 function populateElelemnt(element, contentArray) {
-    console.log('called');
-    if (typeof element !== 'string') {
-        throw Error('bad string');
+    var selectedElement;
+
+    if (!element) {
+        throw Error('parameter [element] must be present');
+    }
+
+    if (!contentArray) {
+        throw Error('parameter [contentArray] must be present');
+    }
+
+    if (!Array.isArray(contentArray)) {
+        throw Error('contentArray must be an array');
+    }
+
+    if (typeof element !== 'string' && !element.tagName) {
+        throw Error('element not a string or DOM element');
+    }
+
+    if (element.tagName && !document.getElementById('#' + element.getAttribute('id'))) {
+        throw Error('not an existing DOM element');
+    }
+
+    if ($(element).length === 0) {
+        throw Error('element with this ID does not exist');
+    }
+
+    for (let i in contentArray) {
+        if (typeof contentArray[i] !== 'string' && typeof contentArray[i] !== 'number') {
+            throw Error('content must be of type string or number');
+        }
     }
 }
--- JavaScript-UI-and-DOM/dom-operations-hw/test/tests.js
+++ JavaScript-UI-and-DOM/dom-operations-hw/test/tests.js
@@ -10,34 +10,53 @@ describe('testing DOM element', function () {
 
     it('should find my test div', function () {
         var testInput = $('#test-div').text();
-
         chai.assert.equal(testInput, 'test');
     });
 
     it('should throw if the provided parameter is not string', function () {
-        chai.expect(function(){
-            populateElelemnt(5);
-        }).to.throw(/^bad string$/);
+        chai.expect(function () {
+            populateElelemnt(5, []);
+        }).to.throw(/^element not a string or DOM element$/);
     });
 
-    it('should throw if the provided parameter is not an existing DOM element', function () {
+    it('should point to an existing DOM element', function () {
+        // TODO: create a new element and pass it as an argument
+        var testElement = document
+            .createElement('p');
 
+        testElement.setAttribute('id', 'super-duper-random');
+
+        chai.expect(function () {
+            populateElelemnt(testElement, []);
+        }).to.throw(/not an existing DOM element/);
     });
 
     it('should throw if the provided id does not select a DOM element', function () {
-
+        chai.expect(function () {
+            populateElelemnt('#super-duper-random', []);
+        }).to.throw(/element with this ID does not exist/);
     });
 
     it('should throw if any of the function params is missing', function () {
+        chai.expect(function () {
+            populateElelemnt();
+        }).to.throw(/parameter \[\w+\] must be present/);
 
+        chai.expect(function () {
+            populateElelemnt('a');
+        }).to.throw(/parameter \[\w+\] must be present/);
     });
 
     it('should throw if any of the function params is not as described', function () {
-
+        chai.expect(function () {
+            populateElelemnt('5', 5);
+        }).to.throw(/contentArray must be an array/);
     });
 
     it('should throw if the content is not string or number', function () {
-
+        chai.expect(function () {
+            populateElelemnt('#test-div', [{ x: 5 }]);
+        }).to.throw(/content must be of type string or number/);
     });
 
 }); 
