0971fa09c9dee76d652e877c9e44a3fdaac30264
shakuu <todorov.h@gmail.com>
2016-07-15 22:30:40 +0300

    chai testing

--- JavaScript-UI-and-DOM/typings/index.d.ts
+++ JavaScript-UI-and-DOM/typings/index.d.ts
@@ -1,2 +1,4 @@
+/// <reference path="globals/chai/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/stylus/index.d.ts" />
