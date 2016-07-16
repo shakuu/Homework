c4c391b31b690208805a3a76f7617c8e14b9d45d
shakuu <todorov.h@gmail.com>
2016-07-16 13:41:06 +0300

    event model homework - done
    using JS
    TODO: with jquery

---
+++ JavaScript-UI-and-DOM/event-model-hw/scripts/app.js
@@ -0,0 +1,65 @@
+function getElement(element) {
+    var selectedElement;
+
+    if (!element.tagName) {
+        if (typeof element !== 'string') {
+            throw Error('Input parameter must be a string or a DOM element.');
+        }
+
+        selectedElement = document.getElementById(element);
+    } else {
+        selectedElement = document.getElementById(element.getAttribute('id'));
+    }
+
+    if (!selectedElement) {
+        throw Error('Provided element must already exist.');
+    }
+
+    return selectedElement;
+}
+
+function toggleVisibilityOfContent(sender) {
+    var nextElement = sender.nextElementSibling;
+
+    while (true) {
+
+        if (nextElement) {
+            if (nextElement.className === 'button') {
+                break;
+            }
+
+            if (nextElement.className === 'content') {
+                // toggle visibility
+                if (nextElement.style.display === 'none') {
+                    nextElement.style.display = 'block';
+                    sender.innerText = 'hide';
+                } else {
+                    nextElement.style.display = 'none';
+                    sender.innerText = 'show';
+                }
+
+                break;
+            }
+        }
+        else {
+            break;
+        }
+
+        nextElement = nextElement.nextElementSibling;
+    }
+}
+
+function eventModel(element) {
+    var selectedElement = getElement(element);
+
+    var buttonElements = selectedElement
+        .getElementsByClassName('button');
+
+    for (let i = 0; i < buttonElements.length; i += 1) {
+        buttonElements[i].innerText = 'hide';
+
+        buttonElements[i].addEventListener('click', function () {
+            toggleVisibilityOfContent(this);
+        });
+    }
+}
---
+++ JavaScript-UI-and-DOM/event-model-hw/test/tests.js
@@ -0,0 +1,18 @@
+describe('Event model homework', function () {
+
+    it('should throw if the provided DOM element is non-existant', function () {
+        chai.expect(function () {
+            eventModel(5);
+        }).to.throw(/^Input parameter must be a string or a DOM element.$/);
+    });
+
+    it('should throw if the id is either not a string or does not select any DOM element', function () {
+        chai.expect(function () {
+            eventModel('some-random-identifier')
+        }).to.throw(/^Provided element must already exist.$/);
+    });
+
+    it('should work', function () {
+        eventModel('test-div');
+    });
+});
