68e7fab5b15c81a7775a3cca33c331af0e9c7402
shakuu <todorov.h@gmail.com>
2016-07-19 14:42:26 +0300

    basic styling structure

--- JavaScript-UI-and-DOM/typings/index.d.ts
+++ JavaScript-UI-and-DOM/typings/index.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="globals/handlebars/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/stylus/index.d.ts" />
