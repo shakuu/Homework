5679c1c3d443d0b13ded2d9d62552755ffe15a34
shakuu <todorov.h@gmail.com>
2016-07-20 19:40:09 +0300

    updated homework

--- JavaScript-UI-and-DOM/dom-operations-hw/scripts/app.js
+++ JavaScript-UI-and-DOM/dom-operations-hw/scripts/app.js
@@ -3,59 +3,39 @@
 // Add divs to the element
 // Each div's content must be one of the items from the contents array
 // The function must remove all previous content from the DOM element provided
-function populateElelemnt(element, contentArray) {
-    var selectedElement,
-        outputContent = '';
 
-    validateInput(element, contentArray);
+function solve() {
+    function task1(el, content) {
 
-    if (typeof element === 'string') {
 
-        selectedElement = document.getElementById(element);
-    } else {
-        
-        selectedElement = element;
-    }
+        if (typeof el === 'string') {
+            el = document.getElementById(el);
 
-    for (let i in contentArray) {
-        outputContent += `<div>${contentArray[i]}</div>`;
-    }
+            if (el === null) {
+                throw new Error();
+            }
 
-    selectedElement.innerHTML = outputContent;
-
-    return selectedElement;
-}
-
-function validateInput(element, contentArray) {
-
-    if (!element) {
-        throw Error('parameter [element] must be present');
-    }
-
-    if (!contentArray) {
-        throw Error('parameter [contentArray] must be present');
-    }
-
-    if (!Array.isArray(contentArray)) {
-        throw Error('contentArray must be an array');
-    }
-
-    if (typeof element !== 'string' && !element.tagName) {
-        throw Error('element must be a string or DOM element');
-    }
-
-    if (element.tagName && !document.getElementById('#' + element.getAttribute('id'))) {
-        throw Error('element must already exist');
-    }
-
-    if (!document.getElementById(element)) {
-        throw Error('element id must be of an existing DOM element');
-    }
-
-    for (let i in contentArray) {
-
-        if (typeof contentArray[i] !== 'string' && typeof contentArray[i] !== 'number') {
-            throw Error('content must be of type string or number');
+        } else if (el instanceof HTMLElement) {
+            // nothing
+        } else {
+            throw new Error();
         }
+
+        if (!Array.isArray(content)) {
+            throw new Error();
+        }
+
+        var outputContent = '';
+        for (var i = 0; i < content.length; i += 1) {
+
+            if (typeof content[i] !== 'string' && typeof content[i] !== 'number') {
+                throw new Error();
+            }
+
+            outputContent += '<div>' + content[i] + '</div>';
+        }
+        el.innerHTML = outputContent;
     }
+
+    return task1;
 }
