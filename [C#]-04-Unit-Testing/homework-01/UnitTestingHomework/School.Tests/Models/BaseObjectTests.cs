899539a4c5d86d680ed105dece218be3588df48e
shakuu <todorov.h@gmail.com>
2016-07-21 22:44:31 +0300

    update : name validation

---
+++ [C#]-04-Unit-Testing/homework-01/UnitTestingHomework/School.Tests/Models/BaseObjectTests.cs
@@ -0,0 +1,46 @@
+﻿namespace School.Tests.Models
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using School.Contracts;
+    using School.Models;
+
+    [TestClass]
+    public class BaseObjectTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), AllowDerivedTypes = false)]
+        public void NameSetter_shouldThrow_ifPassedValueIsANullString()
+        {
+            string testNameValue = null;
+            var testIntValue = 100;
+
+            IName testBaseObject = new Course(testNameValue, testIntValue);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), AllowDerivedTypes = false)]
+        public void NameSetter_shouldThrow_ifPassedValueIsAnEmptyString()
+        {
+            string testNameValue = string.Empty;
+            var testIntValue = 100;
+
+            IName testBaseObject = new Course(testNameValue, testIntValue);
+        }
+
+        [TestMethod]
+        public void NameSetter_objectNamePropertyShouldBeTheSameAsInput_ifThePassedStringIsNotNullOrEmpty()
+        {
+            string expectedNameValue = "course name";
+            var testIntValue = 100;
+
+            IName testBaseObject = new Course(expectedNameValue, testIntValue);
+
+            var actualNameValue = testBaseObject.Name;
+
+            Assert.AreEqual(expectedNameValue, actualNameValue);
+        }
+    }
+}
