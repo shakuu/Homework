2f778b2f0b69b8058fc99e52f7f0816a5cb84c95
shakuu <todorov.h@gmail.com>
2016-08-02 09:41:46 +0300

    udpate

--- [C#]-04-Unit-Testing/Army of Creatures/Source/ArmyOfCreatures.Tests/Logic/Specialties/DoubleDefenseWhenDefendingTests.cs
+++ [C#]-04-Unit-Testing/Army of Creatures/Source/ArmyOfCreatures.Tests/Logic/Specialties/DoubleDefenseWhenDefendingTests.cs
@@ -2,9 +2,11 @@
 {
     using System;
 
+    using Moq;
     using MSTest = Microsoft.VisualStudio.TestTools.UnitTesting;
     using NUnit.Framework;
 
+    using ArmyOfCreatures.Logic.Battles;
     using ArmyOfCreatures.Logic.Specialties;
 
     [TestFixture]
@@ -30,6 +32,32 @@
             var actualValue = (int)doubleDefenseWhenDefendingAsPrivateObject.GetField("rounds");
 
             Assert.AreEqual(rounds, actualValue);
+        }
+
+        [Test]
+        public void ApplyWhenDefending_ShouldThrowTheCorrectErrorMessage_IfDefenderWithSpecialtyParameterIsNull()
+        {
+            ICreaturesInBattle defenderWithSpecialty = null;
+            var attacker = new Mock<ICreaturesInBattle>();
+            var rounds = 5;
+
+            var doubleDefenseWhenDefending = new DoubleDefenseWhenDefending(rounds);
+
+            var resultingException = Assert.Throws<ArgumentNullException>(() => doubleDefenseWhenDefending.ApplyWhenDefending(defenderWithSpecialty, attacker.Object));
+            Assert.IsTrue(resultingException.Message.Contains("defenderWithSpecialty"));
+        }
+
+        [Test]
+        public void ApplyWhenDefending_ShouldThrowTheCorrectErrorMessage_IfAttackerParameterIsNull()
+        {
+            var defenderWithSpecialty = new Mock<ICreaturesInBattle>();
+            ICreaturesInBattle attacker = null;
+            var rounds = 5;
+
+            var doubleDefenseWhenDefending = new DoubleDefenseWhenDefending(rounds);
+
+            var resultingException = Assert.Throws<ArgumentNullException>(() => doubleDefenseWhenDefending.ApplyWhenDefending(defenderWithSpecialty.Object, attacker));
+            Assert.IsTrue(resultingException.Message.Contains("attacker"));
         }
     }
 }
