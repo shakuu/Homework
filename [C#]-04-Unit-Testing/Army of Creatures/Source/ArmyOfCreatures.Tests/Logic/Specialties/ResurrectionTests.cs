13607a3c9dfcd4e66979a07f41ba866c5b75e18f
shakuu <todorov.h@gmail.com>
2016-08-02 19:36:58 +0300

    update

--- [C#]-04-Unit-Testing/Army of Creatures/Source/ArmyOfCreatures.Tests/Logic/Specialties/ResurrectionTests.cs
+++ [C#]-04-Unit-Testing/Army of Creatures/Source/ArmyOfCreatures.Tests/Logic/Specialties/ResurrectionTests.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ArmyOfCreatures.Tests.Logic.Specialties
+﻿namespace ArmyOfCreatures.Tests.Logic.Specialties
 {
-    class ResurrectionTests
+    using System;
+
+    using Moq;
+    using NUnit.Framework;
+
+    using ArmyOfCreatures.Logic.Battles;
+    using ArmyOfCreatures.Logic.Specialties;
+    using ArmyOfCreatures.Tests.Fakes;
+
+    [TestFixture]
+    public class ResurrectionTests
     {
+        [Test]
+        public void ApplyAfterDefending_ShouldThrowWithCorrectMessage_IfDefenderWithSpecialtyParamenterIsNull()
+        {
+            ICreaturesInBattle defenderWithSpecialty = null;
+            var specialty = new Resurrection();
+
+            var exception = Assert.Throws<ArgumentNullException>(() => specialty.ApplyAfterDefending(defenderWithSpecialty));
+            StringAssert.Contains("defenderWithSpecialty", exception.Message);
+        }
+
+        [Test]
+        public void ApplyAfterDefending_ShouldNotSetDefenderWithSpecialtyTotalHitPoints_WhenTotalHitPointsAreLessThanOrEqualToZero()
+        {
+            var defenderWithSpecialty = new Mock<ICreaturesInBattle>();
+            defenderWithSpecialty.SetupGet(mock => mock.TotalHitPoints).Returns(0);
+            defenderWithSpecialty.SetupSet(mock => mock.TotalHitPoints = It.IsAny<int>());
+
+            var specialty = new Resurrection();
+
+            specialty.ApplyAfterDefending(defenderWithSpecialty.Object);
+
+            defenderWithSpecialty.VerifySet(mock => mock.TotalHitPoints = It.IsAny<int>(), Times.Never());
+        }
+
+        [Test]
+        public void AppleyAfterDefending_ShouldSetDefenderWithSpecialtyTotalHitPoiints_WhenTotalHitPointsIsLargerThanZero()
+        {
+            var stubCreature = new FakeCreature();
+            var defenderWithSpecialty = new Mock<ICreaturesInBattle>();
+            defenderWithSpecialty.SetupGet(mock => mock.Count).Returns(1);
+            defenderWithSpecialty.SetupGet(mock => mock.Creature).Returns(stubCreature);
+            defenderWithSpecialty.SetupGet(mock => mock.TotalHitPoints).Returns(10);
+            defenderWithSpecialty.SetupSet(mock => mock.TotalHitPoints = It.IsAny<int>());
+            var specialty = new Resurrection();
+
+            specialty.ApplyAfterDefending(defenderWithSpecialty.Object);
+
+            defenderWithSpecialty.VerifySet(mock => mock.TotalHitPoints = It.IsAny<int>(), Times.Once());
+        }
     }
 }
