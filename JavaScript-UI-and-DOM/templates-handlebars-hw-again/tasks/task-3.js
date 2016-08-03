f173cbce65717eec0b1a084562879754672d149a
Hristo Todorov <todorov.h@gmail.com>
2016-08-03 18:17:32 +0300

    Update task-3.js

--- JavaScript-UI-and-DOM/templates-handlebars-hw-again/tasks/task-3.js
+++ JavaScript-UI-and-DOM/templates-handlebars-hw-again/tasks/task-3.js
@@ -22,21 +22,5 @@ function solve() {
     };
 }
 
-// function solve(){
-// 	return function(){
-// 		$.fn.listview = function(data) {
-// 			var templateSource = $('#' + (this.attr('data-template'))).html();
-// 			var template = handlebars.compile(templateSource);
-// 			var len = data.length,
-// 			i;
-
-// 			for (i = 0; i < len; i+=1) {
-// 				this.append(template(data[i]));
-// 			}
-
-// 			return this;
-// 		};
-// 	};
-// }
 
 module.exports = solve;
