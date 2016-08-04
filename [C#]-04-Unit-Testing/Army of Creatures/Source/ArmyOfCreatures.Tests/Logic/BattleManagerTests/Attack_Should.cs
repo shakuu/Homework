f6a32321ea3f96a96e83ae899fa0d22bbf2c0cbb
shakuu <todorov.h@gmail.com>
2016-08-05 02:04:28 +0300

    more: army of creatures

--- [C#]-04-Unit-Testing/Army of Creatures/Source/ArmyOfCreatures.Tests/Logic/BattleManagerTests/Attack_Should.cs
+++ [C#]-04-Unit-Testing/Army of Creatures/Source/ArmyOfCreatures.Tests/Logic/BattleManagerTests/Attack_Should.cs
@@ -7,21 +7,63 @@
 
     using ArmyOfCreatures.Logic;
     using ArmyOfCreatures.Logic.Battles;
+    using ArmyOfCreatures.Tests.Fakes;
 
     [TestFixture]
     public class Attack_Should
     {
         [Test]
-        public void Throw_WhenAttackerIdentifierIsNull()
+        public void ThrowWithCorrectMessage_WhenAttackerIdentifierIsNull()
         {
             var fakeFactory = new Mock<ICreaturesFactory>();
             var fakeLogger = new Mock<ILogger>();
-            var manager = new BattleManager(fakeFactory.Object, fakeLogger.Object);
+            var battleManager = new BattleManager(fakeFactory.Object, fakeLogger.Object);
 
             CreatureIdentifier attacker = null;
-            var defender = CreatureIdentifier.CreatureIdentifierFromString("fake(2)");
+            var defenderCreatureIdentifier = CreatureIdentifier.CreatureIdentifierFromString("fake(2)");
 
-            Assert.Throws<ArgumentNullException>
+            var actualException = Assert.Throws<ArgumentNullException>(() => battleManager.Attack(attacker, defenderCreatureIdentifier));
+            StringAssert.Contains("identifier", actualException.Message);
+        }
+
+        [Test]
+        public void ThrowWithCorrectMessage_WhenDefenderIdentifierIsNull()
+        {
+            var attackerCreature = new FakeCreature();
+            var fakeFactory = new Mock<ICreaturesFactory>();
+            fakeFactory.Setup(factory => factory.CreateCreature(It.IsAny<string>())).Returns(attackerCreature);
+
+            var fakeLogger = new Mock<ILogger>();
+            var battleManager = new BattleManager(fakeFactory.Object, fakeLogger.Object);
+
+            var attackerCreatureIdentifier = CreatureIdentifier.CreatureIdentifierFromString("FakeCreature(1)"); ;
+            CreatureIdentifier defender = null;
+
+            battleManager.AddCreatures(attackerCreatureIdentifier, 10);
+
+            var actualException = Assert.Throws<ArgumentNullException>(() => battleManager.Attack(attackerCreatureIdentifier, defender));
+            StringAssert.Contains("identifier", actualException.Message);
+        }
+
+        [Test]
+        public void ThrowWithCorrectMessage_IfCreatureArmyNumberIsNotOneOrTwo()
+        {
+            var fakeFactory = new Mock<ICreaturesFactory>();
+            fakeFactory.Setup(factory => factory.CreateCreature(It.IsAny<string>())).Returns(new FakeCreature());
+
+            var fakeLogger = new Mock<ILogger>();
+            var battleManager = new BattleManager(fakeFactory.Object, fakeLogger.Object);
+
+            var attackerCreatureIndentifier = CreatureIdentifier.CreatureIdentifierFromString("FakeCreature(1)");
+            var incorrectAttackerCreatureIdentifier = CreatureIdentifier.CreatureIdentifierFromString("FakeCreature(3)");
+            var defenderCreatureIdentifier = CreatureIdentifier.CreatureIdentifierFromString("FakeCreature(2)");
+
+            battleManager.AddCreatures(attackerCreatureIndentifier, 10);
+            battleManager.AddCreatures(defenderCreatureIdentifier, 100);
+
+            var actualException = Assert.Throws<ArgumentException>(() =>
+                battleManager.Attack(incorrectAttackerCreatureIdentifier, defenderCreatureIdentifier));
+            StringAssert.Contains("Invalid ArmyNumber: 3", actualException.Message);
         }
     }
 }
