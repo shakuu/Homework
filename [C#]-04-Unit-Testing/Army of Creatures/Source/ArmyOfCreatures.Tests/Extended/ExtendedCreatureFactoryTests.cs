3f5957059e6e145e639dcf4acae0f4c374b13bd6
shakuu <todorov.h@gmail.com>
2016-08-08 22:44:36 +0300

    ArmyOfCreatures.Extended.ExtendedCreaturesFactory tests

--- [C#]-04-Unit-Testing/Army of Creatures/Source/ArmyOfCreatures.Tests/Extended/ExtendedCreatureFactoryTests.cs
+++ [C#]-04-Unit-Testing/Army of Creatures/Source/ArmyOfCreatures.Tests/Extended/ExtendedCreatureFactoryTests.cs
@@ -6,6 +6,7 @@
 
     using ArmyOfCreatures.Extended;
     using ArmyOfCreatures.Extended.Creatures;
+    using ArmyOfCreatures.Logic.Creatures;
 
     [TestFixture]
     public class ExtendedCreatureFactoryTests
@@ -22,7 +23,6 @@
             Assert.IsTrue(resultingException.Message.Contains("Creature Name is null or empty"));
         }
 
-        [Test]
         [TestCase("AncientBehemoth", typeof(AncientBehemoth))]
         [TestCase("CyclopsKing", typeof(CyclopsKing))]
         [TestCase("Goblin", typeof(Goblin))]
@@ -39,6 +39,39 @@
             var actualInstanceType = resultingInstance.GetType();
             // Assert
             Assert.AreEqual(expectedType, actualInstanceType);
+        }
+
+        //CreateCreature should throw ArgumentException with message 
+        //that contains the string "Invalid creature type",
+        //when a string representing non-existing creature type 
+        //is passed as a method argument.
+        [Test]
+        public void CreateCreature_ShouldThrowArgumentExceptionWithCorrectMessage_WhenInputParameterIsStringWithNonexistentCreatureType()
+        {
+            var factory = new ExtendedCreaturesFactory();
+            var invalidCreatureTypeName = "!@##$3$#$asdasd12312312";
+
+            Assert.That(() => factory.CreateCreature(invalidCreatureTypeName),
+                Throws.ArgumentException.With.Message.Contains("Invalid creature type"));
+        }
+
+        //CreateCreature should return the corresponding creature type based
+        //on the string that is passed as a method argument.
+        //Test with(AncientBehemoth, CyclopsKing, Goblin, Griffin, WolfRaider
+        //and something else for the default case).
+        [TestCase("AncientBehemoth", typeof(AncientBehemoth))]
+        [TestCase("CyclopsKing", typeof(CyclopsKing))]
+        [TestCase("Goblin", typeof(Goblin))]
+        [TestCase("Griffin", typeof(Griffin))]
+        [TestCase("WolfRaider", typeof(WolfRaider))]
+        [TestCase("ArchDevil", typeof(ArchDevil))]
+        public void CreateCreaure_ShouldInstantiateTheCorrectTypeCorrespondingToNameParameter_WhenNameParameterIsValid(string name, Type expectedType)
+        {
+            var factory = new ExtendedCreaturesFactory();
+
+            var actualCreature = factory.CreateCreature(name);
+
+            Assert.That(actualCreature.GetType(), Is.EqualTo(expectedType));
         }
     }
 }
