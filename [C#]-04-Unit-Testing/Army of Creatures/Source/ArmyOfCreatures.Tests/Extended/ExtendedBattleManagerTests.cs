7236523c7fcd8e4ddbe4bae24202854d00267505
shakuu <todorov.h@gmail.com>
2016-08-08 22:06:48 +0300

    ArmyOfCreatures.Extended.ExtendedBattleManager Tests

---
+++ [C#]-04-Unit-Testing/Army of Creatures/Source/ArmyOfCreatures.Tests/Extended/ExtendedBattleManagerTests.cs
@@ -0,0 +1,37 @@
+﻿namespace ArmyOfCreatures.Tests.Extended
+{
+    using System.Reflection;
+
+    using ArmyOfCreatures.Logic;
+    using ArmyOfCreatures.Logic.Battles;
+    using ArmyOfCreatures.Extended;
+
+    using Moq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ExtendedBattleManagerTests
+    {
+        //Constructor should call base() constructor and instantiate the object
+        //with all properties set up. (Use C# integrated PrivateObject class,
+        //to expose private fields, so that you can assert, that the object was instantiated properly).
+        [Test]
+        public void Constructor_ShouldCallBaseConstructorAndInstntiateAllProperties()
+        {
+            var mockFactory = new Mock<ICreaturesFactory>();
+            var mockLogger = new Mock<ILogger>();
+            var manager = new ExtendedBattleManager(mockFactory.Object, mockLogger.Object);
+
+            var actualFirstArmy = manager.GetType()
+                .BaseType.GetField("firstArmyCreatures", BindingFlags.NonPublic | BindingFlags.Instance)
+                .GetValue((BattleManager)manager);
+
+            var actualSecondArmy = manager.GetType()
+                .BaseType.GetField("secondArmyCreatures", BindingFlags.NonPublic | BindingFlags.Instance)
+                .GetValue((BattleManager)manager);
+            
+            Assert.That(actualFirstArmy, Is.Not.Null);
+            Assert.That(actualSecondArmy, Is.Not.Null);
+        }
+    }
+}
