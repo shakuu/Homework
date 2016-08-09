cc0e033359236813fddcb14db2f44998ce76108e
shakuu <todorov.h@gmail.com>
2016-08-09 09:35:58 +0300

    update

--- [C#]-04-Unit-Testing/Army of Creatures/Source/ArmyOfCreatures.Tests/Logic/Battles/CreatureIdentifierTests.cs
+++ [C#]-04-Unit-Testing/Army of Creatures/Source/ArmyOfCreatures.Tests/Logic/Battles/CreatureIdentifierTests.cs
@@ -1,10 +1,12 @@
 ﻿namespace ArmyOfCreatures.Tests.Logic.Battles
 {
     using System;
+    using System.Reflection;
 
     using ArmyOfCreatures.Logic.Battles;
 
     using NUnit.Framework;
+    using System.Runtime.Serialization;
 
     [TestFixture]
     public class CreatureIdentifierTests
@@ -53,6 +55,64 @@
                 Is.InstanceOf<CreatureIdentifier>()
                     .With.Property("CreatureType").EqualTo("creature")
                     .And.Property("ArmyNumber").EqualTo(5));
+        }
+
+        //Call with null valueToParse should throw ArgumentNullException
+        [Test]
+        public void CreatureIdentifierFromString_ShouldThrowArgumentNullException_WhenStringValueToParseParameterIsNull()
+        {
+            string valueToParse = null;
+
+            Assert.That(() => CreatureIdentifier.CreatureIdentifierFromString(valueToParse),
+                Throws.ArgumentNullException.With.Message.Contains("valueToParse"));
+        }
+
+        //Call with invalid Army number(ex: Angel(test)) test cannot be parsed should throw FormatExcepition
+        [Test]
+        public void CreatureIdentifierFromString_ShouldThrowFormatExceptions_WhenNumberIsNotValid()
+        {
+            var valueToParse = "creature(test)";
+
+            Assert.That(() => CreatureIdentifier.CreatureIdentifierFromString(valueToParse),
+                Throws.InstanceOf<FormatException>());
+        }
+
+        //Call with invalid string (without brackets to split) should throw IndexOutOfRangeException
+        [Test]
+        public void CreateIdentifierFromString_ShouldThrowIndexOutOfRangeException_WhenValueToParseDoesNotContainExpectedCharactersToSplitBy()
+        {
+            var valueToParse = "noParenthesis";
+
+            Assert.That(() => CreatureIdentifier.CreatureIdentifierFromString(valueToParse),
+                Throws.InstanceOf<IndexOutOfRangeException>());
+        }
+
+        //ToString should output expected result
+        [Test]
+        public void ToString_ShouldReturn_StringInTheRequiredFormat()
+        {
+            var creatureIdentifierConstructor = typeof(CreatureIdentifier)
+                .GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, null, new[] { typeof(string), typeof(int) }, null);
+            var creatureIdentifier = creatureIdentifierConstructor.Invoke(new object[] { "creature", 1 });
+
+            var expectedToString = "creature(1)";
+            var actualToString = creatureIdentifier.ToString();
+
+            Assert.That(actualToString, Is.EqualTo(expectedToString));
+        }
+
+        [Test]
+        public void ToString_ShouldReturn_StringInTheRequiredFormatAgain()
+        {
+            var creatureIdentifier = (CreatureIdentifier)FormatterServices
+                .GetUninitializedObject(typeof(CreatureIdentifier));
+            creatureIdentifier.GetType().GetProperty("CreatureType").SetValue(creatureIdentifier, "creature");
+            creatureIdentifier.GetType().GetProperty("ArmyNumber").SetValue(creatureIdentifier, 1);
+
+            var expectedToString = "creature(1)";
+            var actualToString = creatureIdentifier.ToString();
+
+            Assert.That(actualToString, Is.EqualTo(expectedToString));
         }
     }
 }
