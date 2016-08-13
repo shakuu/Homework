f07c740cace4c0f72fb436baa1848aee29ffe123
shakuu <todorov.h@gmail.com>
2016-08-13 17:26:16 +0300

    update: calendar template 100/ 100
    quotes quotes quotes

--- js-ui-dom-exam-prep/CalendarTemplate/tasks/task-3.js
+++ js-ui-dom-exam-prep/CalendarTemplate/tasks/task-3.js
@@ -10,7 +10,7 @@ function solve() {
                 '<div class="date">{{day}}</div>'+
                 '<div class="events">'+
                     '{{#events}}'+
-                   '<div class="event {{importance}}{{#if comment}} title="{{comment}}{{/if}}">'+
+                   '<div class="event {{importance}}"{{#if comment}} title="{{comment}}"{{/if}}>'+
                         '{{#if title}}'+
                         '<div class="title">{{title}}</div>'+
                         '{{else}}'+
