27b4e37c9d4f458b37271cf93d8ee0c67a63afa3
shakuu <todorov.h@gmail.com>
2016-08-15 15:57:32 +0300

    update

--- js-ui-dom-exam-prep/Sample-exam-1/task-2/task-files/scripts.js
+++ js-ui-dom-exam-prep/Sample-exam-1/task-2/task-files/scripts.js
@@ -1,3 +1,28 @@
 $.fn.tabs = function () {
+    var root = $(this),
+        allTabItems,
+        active;
 
+    root.addClass('tabs-container');
+    
+    allTabItems = root.children('.tab-item');
+    allTabItems.children('.tab-item-content').hide();
+
+    active = allTabItems.eq(0);
+    active.addClass('current');
+    active.children('.tab-item-content').show();
+
+    root.on('click', '.tab-item-title', setActiveTab);
+
+    function setActiveTab(event) {
+        active.removeClass('current');
+        active.children('.tab-item-content').hide();
+
+        active = $(event.target).parents('.tab-item').first();
+
+        active.addClass('current');
+        active.children('.tab-item-content').show();
+    }
+
+    return $(this);
 };
