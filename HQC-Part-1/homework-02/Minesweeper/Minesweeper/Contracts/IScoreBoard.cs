62b94f88a4809c58f7b1b187e5560128579fe659
shakuu <todorov.h@gmail.com>
2016-08-26 22:26:53 +0300

    update

--- HQC-Part-1/homework-02/Minesweeper/Minesweeper/Contracts/IScoreBoard.cs
+++ HQC-Part-1/homework-02/Minesweeper/Minesweeper/Contracts/IScoreBoard.cs
@@ -2,10 +2,24 @@
 
 namespace Minesweeper.Contracts
 {
+    /// <summary>
+    /// Stores scores associated with names.
+    /// </summary>
     public interface IScoreBoard
     {
+
+        /// <summary>
+        /// Add a new element to the IScoreBoard.
+        /// </summary>
+        /// <param name="name"> Name to associate the score with. </param>
+        /// <param name="score"> Number of points. </param>
         void AddScoreCard(string name, int score);
 
+        /// <summary>
+        /// Get the top number scores stored.
+        /// </summary>
+        /// <param name="number"> The number of scores to return. </param>
+        /// <returns> IEnumerable containg the number of scores requested. </returns>
         IEnumerable<IScoreCard> GetTopScores(int number);
 
     }
--- HQC-Part-1/homework-02/Minesweeper/Minesweeper/Contracts/IScoreCard.cs
+++ HQC-Part-1/homework-02/Minesweeper/Minesweeper/Contracts/IScoreCard.cs
@@ -1,9 +1,18 @@
 ﻿namespace Minesweeper.Contracts
 {
+    /// <summary>
+    /// Creates a ScoreCard containing the Name of the player and the points score achieved.
+    /// </summary>
     public interface IScoreCard
     {
+        /// <summary>
+        /// Player name associated with this score.
+        /// </summary>
         string Name { get; set; }
 
+        /// <summary>
+        /// Score represented by points.
+        /// </summary>
         int Score { get; set; }
     }
 }
