e55d0ea244c9a02d2993721ce6b438e22a03cfd3
shakuu <todorov.h@gmail.com>
2016-09-01 16:20:01 +0300

    update

---
+++ HQC-Part-1/homework-06-High-Quality-Methods/CSharpExam2/03-Porcupines/Forests/ForestCell.cs
@@ -0,0 +1,53 @@
+﻿using _03_Porcupines.Forests.Contracts;
+using _03_Porcupines.Forests.Enums;
+
+namespace _03_Porcupines.Forests
+{
+    public class ForestCell : IForestCell
+    {
+        private int pointsValue;
+        private bool isCollected;
+        private ForestCellContentType contentType;
+
+        public ForestCell(ForestCellContentType contentType, int pointsValue = 0)
+        {
+            this.pointsValue = pointsValue;
+            this.contentType = contentType;
+            this.isCollected = false;
+        }
+
+        public ForestCellContentType ContentType
+        {
+            get
+            {
+                return this.contentType;
+            }
+
+            set
+            {
+                this.contentType = value;
+            }
+        }
+
+        public bool IsCollected
+        {
+            get
+            {
+                return this.isCollected;
+            }
+
+            set
+            {
+                this.isCollected = value;
+            }
+        }
+
+        public int Points
+        {
+            get
+            {
+                return this.pointsValue;
+            }
+        }
+    }
+}
---
+++ HQC-Part-1/homework-06-High-Quality-Methods/CSharpExam2/03-Porcupines/Forests/ForestCellFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+
+using _03_Porcupines.Forests.Contracts;
+using _03_Porcupines.Forests.Enums;
+
+namespace _03_Porcupines.Forests
+{
+    public class ForestCellFactory : IForestCellFactory
+    {
+        private ConstructorInfo forestCellConstructor;
+
+        public ForestCellFactory(Type type)
+        {
+            var getIForestCell = type.GetInterface("IForestCell");
+            if (getIForestCell == null)
+            {
+                throw new ArgumentException("Invalid type.");
+            }
+
+            this.forestCellConstructor = this.GetConstructor(type);
+        }
+
+        public IForestCell CreateForestCell(ForestCellContentType contentType, int points = 0)
+        {
+            var constructorParameters = new object[] { contentType, points };
+            var newForestCell = (IForestCell)this.forestCellConstructor.Invoke(constructorParameters);
+
+            return newForestCell;
+        }
+
+        private ConstructorInfo GetConstructor(Type type)
+        {
+            var bindingFlags = BindingFlags.Public | BindingFlags.Instance;
+            var constructorParametersTypes = new[] { typeof(ForestCellContentType), typeof(int) };
+            var constructor = type.GetConstructor(bindingFlags, null, constructorParametersTypes, null);
+            if (constructor == null)
+            {
+                throw new ArgumentException("Could not find constructor.");
+            }
+
+            return constructor;
+        }
+    }
+}
