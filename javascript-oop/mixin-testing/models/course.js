1a0b8ecc959adbce4cf11e19e85479263a37584f
shakuu <todorov.h@gmail.com>
2016-09-03 01:20:57 +0300

    update

--- javascript-oop/mixin-testing/models/course.js
+++ javascript-oop/mixin-testing/models/course.js
@@ -2,9 +2,43 @@ const StorageProviderMixin = require('../mixins/storage-provider-mixin');
 const Homework = require('./homework');
 const Student = require('./student');
 
+const STARTS_WITH_WHITESPACE = /^\s+/;
+const ENDS_WITH_WHITESPACE = /\s$/;
+const VALID_NAME = /^[A-Z][a-z]*$/;
+
 class Course extends StorageProviderMixin(Object) {
     constructor(title, presentations) {
         super();
+
+        if (!presentations) {
+            throw new Error();
+        }
+
+        if (!Array.isArray(presentations)) {
+            throw new Error();
+        }
+
+        if (presentations.length === 0) {
+            throw new Error();
+        }
+
+        presentations.forEach(presentation => {
+            if (typeof presentation !== 'string') {
+                throw new Error();
+            }
+
+            if (presentation.length === 0) {
+                throw new Error();
+            }
+
+            if (STARTS_WITH_WHITESPACE.test(presentation)) {
+                throw new Error();
+            }
+
+            if (ENDS_WITH_WHITESPACE.test(presentation)) {
+                throw new Error();
+            }
+        })
 
         this.title = title;
         this.presentations = presentations;
@@ -31,6 +65,16 @@ class Course extends StorageProviderMixin(Object) {
             homework = new Homework(this.presentations.length),
             student = new Student(...names, homework);
 
+        if (names.length < 2) {
+            throw new Error();
+        }
+
+        names.forEach(name => {
+            if (!VALID_NAME.test(name)) {
+                throw new Error();
+            }
+        });
+
         var generatedId = this.addItem(student);
         return generatedId;
     }
@@ -51,9 +95,21 @@ class Course extends StorageProviderMixin(Object) {
     }
 
     submitHomework(studentId, homeworkId) {
-        const student = this.getItemById(studentId, 'Student')[0];
-        student.homework.assignments.filter(hw => hw.presentationId === homeworkId)[0].status = true;
-        this.getItemById(studentId, 'Student', student);
+        let student = this.getItemById(studentId, 'Student');
+        if (student.length === 0 || student.length > 1) {
+            throw new Error();
+        } else {
+            student = student[0];
+        }
+
+        let homework = student.homework.assignments.filter(hw => hw.presentationId === homeworkId);
+        if (homework.length === 0 || homework.length > 1) {
+            throw new Error();
+        } else {
+            homework[0].status = true;
+        }
+
+        this.setItemById(studentId, 'Student', student);
     }
 }
 
