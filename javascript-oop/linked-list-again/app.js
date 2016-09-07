7d780727b772b62dc874537cc01e9b15cba867d7
shakuu <todorov.h@gmail.com>
2016-09-07 19:34:25 +0300

    update

--- javascript-oop/linked-list-again/app.js
+++ javascript-oop/linked-list-again/app.js
@@ -5,6 +5,7 @@ const values = 'babel src --presets es2015 --out-dir ./build -s -w'.split(' ');
 let list = new LinkedList();
 list.append(...values);
 console.log(list.at(0));
+
 // list.prepend(45).append(1, 2, 3).prepend(4, 5).insert(0, 500);
 
 // console.log(list.toString());
