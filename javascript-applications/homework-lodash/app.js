c92d63e88e20adc0ebcab605444e844fd498652c
shakuu <todorov.h@gmail.com>
2016-09-11 20:09:21 +0300

    update

---
+++ javascript-applications/homework-lodash/app.js
@@ -0,0 +1,16 @@
+var students = [{
+			firstName: 'Ester',
+			lastName: 'Reba'
+		}, {
+			firstName: 'Abdullo',
+			lastName: 'Vilma'
+		}, {
+			firstName: 'Germano',
+			lastName: 'Sabina'
+		}, {
+			firstName: 'Naomi',
+			lastName: 'Tichaona'
+		}];
+const task1 = require('./tasks/task-1')();
+
+task1(students);
--- javascript-applications/homework-lodash/tasks/task-1.js
+++ javascript-applications/homework-lodash/tasks/task-1.js
@@ -6,13 +6,23 @@ Create a function that:
 *   **Finds** all students whose `firstName` is before their `lastName` alphabetically
 *   Then **sorts** them in descending order by fullname
     *   fullname is the concatenation of `firstName`, ' ' (empty space) and `lastName`
-*   Then **prints** the fullname of founded students to the console
+*   Then **prints** the fullname of found students to the console
 *   **Use underscore.js for all operations**
 */
+const _ = require('underscore');
 
-function solve(){
-  return function (students) {
-  };
+function solve() {
+    'use strict';
+    return function (students) {
+        students = _.chain(students)
+            .filter(std => std.firstName < std.lastName)
+            .map(std => `${std.firstName} ${std.lastName}`)
+            .sortBy(std => std)
+            .value()
+            .reverse();
+
+        students.forEach(std => console.log(std));
+    };
 }
 
 module.exports = solve;
