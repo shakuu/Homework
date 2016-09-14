60b303d999608fab6d801e614498846828ff1f26
shakuu <todorov.h@gmail.com>
2016-09-14 23:52:38 +0300

    update

--- javascript-applications/homework-promises/GeoLocation/index.js
+++ javascript-applications/homework-promises/GeoLocation/index.js
@@ -1,28 +1,38 @@
-getLocation.getCurrentLocation();
-
-var map;
-function initMap() {
-    function getCurrentLocation() {
-        var x = document.getElementById("demo");
-        console.log(navigator.geolocation);
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-        } else {
-            x.innerHTML = "Geolocation is not supported by this browser.";
-        }
-
-        function showPosition(position) {
-            console.log(x);
-            x.innerHTML = "Latitude: " + position.coords.latitude +
-                "<br>Longitude: " + position.coords.longitude;
-
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: { lat: position.coords.latitude, lng: position.coords.longitude },
-                zoom: 18
-            });
-
-        }
-    }
-
-    getCurrentLocation();
+function initMaps() {
+    showMap();
 }
+
+const showMap = (() => {
+    const createMap = (() => {
+        return {
+            parseData(data) {
+                const coords = {
+                    lat: data.coords.latitude,
+                    lon: data.coords.longitude
+                };
+
+                return coords;
+            },
+            createMap(coords) {
+                map = new google.maps.Map(document.getElementById('map'), {
+                    center: { lat: coords.lat, lng: coords.lon },
+                    zoom: 18
+                });
+            }
+        };
+    })();
+
+    const mapPromise = new Promise(function (resolve, reject) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(resolve);
+        } else {
+            reject();
+        }
+    });
+
+    mapPromise
+        .then(createMap.parseData)
+        .then(createMap.createMap);
+
+    return mapPromise;
+});
