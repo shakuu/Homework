277c7dd8d10249696029f0e6734c4c87e725f7ed
shakuu <todorov.h@gmail.com>
2016-09-18 01:02:29 +0300

    up

--- javascript-applications/workshop-forum-again/public/scripts/data.js
+++ javascript-applications/workshop-forum-again/public/scripts/data.js
@@ -1,65 +1,112 @@
 var data = (function () {
-  const USERNAME_STORAGE_KEY = 'username-key';
+    const USERNAME_STORAGE_KEY = 'username-key';
 
-  // start users
-  function userLogin(user) {
-    localStorage.setItem(USERNAME_STORAGE_KEY, user);
-    return Promise.resolve(user);
-  }
-
-  function userLogout() {
-    localStorage.removeItem(USERNAME_STORAGE_KEY)
-    return Promise.resolve();
-  }
-
-  function userGetCurrent() {
-    return Promise.resolve(localStorage.getItem(USERNAME_STORAGE_KEY));
-  }
-  // end users
-
-  // start threads
-  function threadsGet() {
-    return new Promise((resolve, reject) => {
-      $.getJSON('api/threads')
-        .done(resolve)
-        .fail(reject);
-    })
-  }
-
-  function threadsAdd(title) {
-    
-  }
-
-  function threadById(id) {
-
-  }
-
-  function threadsAddMessage(threadId, content) {
-
-  }
-  // end threads
-
-  // start gallery
-  function galleryGet() {
-    const REDDIT_URL = `https://www.reddit.com/r/aww.json?jsonp=?`;
-    
-  }
-  // end gallery
-
-  return {
-    users: {
-      login: userLogin,
-      logout: userLogout,
-      current: userGetCurrent
-    },
-    threads: {
-      get: threadsGet,
-      add: threadsAdd,
-      getById: threadById,
-      addMessage: threadsAddMessage
-    },
-    gallery: {
-      get: galleryGet,
+    // start users
+    function userLogin(user) {
+        localStorage.setItem(USERNAME_STORAGE_KEY, user);
+        return Promise.resolve(user);
     }
-  }
+
+    function userLogout() {
+        localStorage.removeItem(USERNAME_STORAGE_KEY)
+        return Promise.resolve();
+    }
+
+    function userGetCurrent() {
+        return Promise.resolve(localStorage.getItem(USERNAME_STORAGE_KEY));
+    }
+    // end users
+
+    // start threads
+    function threadsGet() {
+        return new Promise((resolve, reject) => {
+            $.getJSON('api/threads')
+                .done(resolve)
+                .fail(reject);
+        });
+    }
+
+    function threadsAdd(title) {
+        const apiUrl = 'api/threads';
+        const requestBody = {
+            title: title,
+            username: 'anonymous'
+        };
+
+        const myPromise = new Promise((resolve, reject) => {
+            $.ajax({
+                type: 'POST',
+                url: apiUrl,
+                contentType: 'application/json',
+                data: JSON.stringify(requestBody)
+            })
+                .done(resolve)
+                .fail(reject);
+        });
+
+        return myPromise;
+    }
+
+    function threadById(id) {
+        const baseApiUrl = 'api/threads/';
+        const urlToAccess = baseApiUrl + id;
+        return new Promise((resolve, reject) => {
+            $.getJSON(urlToAccess)
+                .done(resolve)
+                .fail(reject);
+        });
+    }
+
+    function threadsAddMessage(threadId, content) {
+        const apiUrl = `api/threads/${threadId}/messages`;
+        const requestBody = {
+            username: 'anonymous',
+            content: content
+        };
+
+        const myPromise = new Promise((resolve, reject) => {
+            $.ajax({
+                type: 'POST',
+                url: apiUrl,
+                data: JSON.stringify(requestBody),
+                contentType: 'application/json'
+            })
+                .done(resolve)
+                .fail(reject);
+        });
+
+        return myPromise;
+    }
+    // end threads
+
+    // start gallery
+    function galleryGet() {
+        const REDDIT_URL = `https://www.reddit.com/r/aww.json?jsonp=?`;
+
+        const myPromise = new Promise((resolve, reject) => {
+            $.getJSON(REDDIT_URL)
+                .done(resolve)
+                .fail(reject);
+        });
+
+        return myPromise;
+    }
+    // end gallery
+
+    return {
+        users: {
+            login: userLogin,
+            logout: userLogout,
+            current: userGetCurrent
+        },
+        threads: {
+            get: threadsGet,
+            add: threadsAdd,
+            getById: threadById,
+            addMessage: threadsAddMessage
+        },
+        gallery: {
+            get: galleryGet,
+        }
+    }
 })();
--- javascript-applications/workshop-forum-again/public/scripts/main.js
+++ javascript-applications/workshop-forum-again/public/scripts/main.js
@@ -1,15 +1,15 @@
 ﻿$(() => { // on document ready
   const GLYPH_UP = 'glyphicon-chevron-up',
-        GLYPH_DOWN = 'glyphicon-chevron-down',
-        root = $('#root'),
-        navbar = root.find('nav.navbar'),
-        mainNav = navbar.find('#main-nav'),
-        contentContainer = $('#root #content'),
-        loginForm = $('#login'),
-        logoutForm = $('#logout'),
-        usernameSpan = $('#span-username'),
-        usernameInput = $('#login input'),
-        alertTemplate = $($('#alert-template').text());
+    GLYPH_DOWN = 'glyphicon-chevron-down',
+    root = $('#root'),
+    navbar = root.find('nav.navbar'),
+    mainNav = navbar.find('#main-nav'),
+    contentContainer = $('#root #content'),
+    loginForm = $('#login'),
+    logoutForm = $('#logout'),
+    usernameSpan = $('#span-username'),
+    usernameInput = $('#login input'),
+    alertTemplate = $($('#alert-template').text());
 
   (function checkForLoggedUser() {
     data.users.current()
@@ -24,8 +24,8 @@
 
   function showMsg(msg, type, cssClass, delay) {
     let container = alertTemplate.clone(true)
-        .addClass(cssClass).text(`${type}: ${msg}`)
-        .appendTo(root);
+      .addClass(cssClass).text(`${type}: ${msg}`)
+      .appendTo(root);
 
     setTimeout(() => {
       container.remove();
@@ -35,13 +35,13 @@
   // start threads
   function loadThreadsContent(threads) {
     let container = $($('#threads-container-template').text()),
-        threadsContainer = container.find('#threads');
+      threadsContainer = container.find('#threads');
 
     function getThreadUI(title, id, creator, date) {
       let template = $($('#thread-template').text()).attr('data-id', id),
-          threadTitle = template.find('.thread-title').text(title),
-          threadCreator = template.find('.thread-creator').text(creator || 'anonymous'),
-          threadDate = template.find('.thread-date').text(date || 'unknown');
+        threadTitle = template.find('.thread-title').text(title),
+        threadCreator = template.find('.thread-creator').text(creator || 'anonymous'),
+        threadDate = template.find('.thread-date').text(date || 'unknown');
 
       return template.clone(true);
     }
@@ -62,14 +62,14 @@
 
   function loadMessagesContent(data) {
     let container = $($('#messages-container-template').text()),
-        messagesContainer = container.find('.panel-body');
+      messagesContainer = container.find('.panel-body');
     container.attr('data-thread-id', data.result.id);
 
     function getMsgUI(msg, author, date) {
       let template = $($('#messages-template').text());
-      template.find('.message-content').text(msg);
-      template.find('.message-creator').text(author || 'anonymous');
-      template.find('.message-date').text(date || 'unknown');
+      template.find('.message-content').text(msg.content);
+      template.find('.message-creator').text(author || msg.username || 'anonymous');
+      template.find('.message-date').text(date || msg.postDate || 'unknown');
       return template.clone(true);
     }
     function getAddNewMsgUI() {
@@ -93,11 +93,11 @@
 
   function loadGalleryContent(data) {
     let list = data.data.children,
-        containerGallery = $($('#gallery-container-tempalte').text()),
-        containerImgs = containerGallery.find('#gallery-imgs'),
-        item = $($('#gallery-img-tempalte').text()),
-        itemImg = item.find('img.gallery-item-img'),
-        itemTitle = item.find('.gallery-item-title')
+      containerGallery = $($('#gallery-container-tempalte').text()),
+      containerImgs = containerGallery.find('#gallery-imgs'),
+      item = $($('#gallery-img-tempalte').text()),
+      itemImg = item.find('img.gallery-item-img'),
+      itemTitle = item.find('.gallery-item-title')
 
     list.forEach((el) => {
       itemTitle.text(el.data.title);
@@ -117,22 +117,24 @@
 
   navbar.on('click', '#btn-threads', (ev) => {
     data.threads.get()
-        .then((data) => {
-          loadThreadsContent(data.result)
-        })
+      .then((data) => {
+        loadThreadsContent(data.result)
+      })
   });
 
   contentContainer.on('click', '#btn-add-thread', (ev) => {
     let title = $(ev.target).parents('form').find('input#input-add-thread').val() || null;
     data.threads.add(title)
-        .then(/* add to UI */)
-        .then(showMsg('Successfuly added the new thread', 'Success', 'alert-success'))
-        .catch((err) => showMsg(JSON.parse(err.responseText).err, 'Error', 'alert-danger'));
+      .then((thread) => {
+        loadThreadsContent([thread]);
+      })
+      .then(showMsg('Successfuly added the new thread', 'Success', 'alert-success'))
+      .catch((err) => showMsg(JSON.parse(err.responseText).err, 'Error', 'alert-danger'));
   })
 
   contentContainer.on('click', 'a.thread-title', (ev) => {
     let $target = $(ev.target),
-        threadId = $target.parents('.thread').attr('data-id');
+      threadId = $target.parents('.thread').attr('data-id');
 
     data.threads.getById(threadId)
       .then(loadMessagesContent)
@@ -141,14 +143,16 @@
 
   contentContainer.on('click', '.btn-add-message', (ev) => {
     let $target = $(ev.target),
-        $container = $target.parents('.container-messages'),
-        thId = $container.attr('data-thread-id'),
-        msg = $container.find('.input-add-message').val();
+      $container = $target.parents('.container-messages'),
+      thId = $container.attr('data-thread-id'),
+      msg = $container.find('.input-add-message').val();
 
     data.threads.addMessage(thId, msg)
-        .then(/* add to UI */)
-        .then(showMsg('Successfuly added the new mssagee', 'Success', 'alert-success'))
-        .catch((err) => showMsg(JSON.parse(err.responseText).err, 'Error', 'alert-danger'));
+      .then((message) => {
+        loadMessagesContent([message]);
+      })
+      .then(showMsg('Successfuly added the new mssagee', 'Success', 'alert-success'))
+      .catch((err) => showMsg(JSON.parse(err.responseText).err, 'Error', 'alert-danger'));
   })
 
   contentContainer.on('click', '.btn-close-msg', (ev) => {
@@ -188,11 +192,11 @@
   });
   navbar.on('click', '#btn-logout', (ev) => {
     data.users.logout()
-    .then(() => {
-      usernameSpan.text('');
-      loginForm.removeClass('hidden');
-      logoutForm.addClass('hidden');
-    })
+      .then(() => {
+        usernameSpan.text('');
+        loginForm.removeClass('hidden');
+        logoutForm.addClass('hidden');
+      })
   });
   // end login/logout
 });
