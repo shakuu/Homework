8ef5d302ef4dd344480baf1323162c1c7ed0bfcf
shakuu <todorov.h@gmail.com>
2016-09-21 12:38:47 +0300

    reformat

--- HQC-Part-II/homework-refactoring/Matrica.cs
+++ HQC-Part-II/homework-refactoring/Matrica.cs
@@ -1,48 +1,84 @@
 ﻿using System;
 
-namespace Task3{
-    class WalkInMatrica {
-        static void change( ref int dx,ref int dy ) {
-            int[] dirX = {1,1,1,0, -1,-1,-1,0 };
-            
-			
-			int[] dirY = { 1,0,-1,-1, -1,0,1,1 };
-            int cd = 0;
-            for( int count = 0; count < 8; count++ )
-                if( dirX[count] == dx && dirY[count] == dy ) { cd = count; break; }
-            if( cd == 7 ) { dx = dirX[0]; dy = dirY[0]; return; }
-            dx = dirX[cd+1];
-
-
-            dy = dirY[cd + 1];
-        }
-        static bool proverka( int[,] arr, int x, int y ){
+namespace Task3
+{
+    class WalkInMatrica
+    {
+        static void change(ref int dx, ref int dy)
+        {
             int[] dirX = { 1, 1, 1, 0, -1, -1, -1, 0 };
             int[] dirY = { 1, 0, -1, -1, -1, 0, 1, 1 };
-            for( int i = 0; i < 8; i++ ){
-                if( x + dirX[i] >= arr.GetLength( 0 ) || x + dirX[i] < 0 ) dirX[i] = 0;
 
-				
-				if( y + dirY[i] >= arr.GetLength( 0 ) || y + dirY[i] < 0 )dirY[i] = 0;
+            int cd = 0;
+            for (int count = 0; count < 8; count++)
+            {
+                if (dirX[count] == dx && dirY[count] == dy)
+                {
+                    cd = count; break;
+                }
             }
-            for( int i = 0; i < 8; i++ )
-                if( arr[x + dirX[i], y + dirY[i]] == 0 )return true;
+
+            if (cd == 7)
+            {
+                dx = dirX[0];
+                dy = dirY[0];
+                return;
+            }
+
+            dx = dirX[cd + 1];
+            dy = dirY[cd + 1];
+        }
+
+        static bool proverka(int[,] arr, int x, int y)
+        {
+            int[] dirX = { 1, 1, 1, 0, -1, -1, -1, 0 };
+            int[] dirY = { 1, 0, -1, -1, -1, 0, 1, 1 };
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (x + dirX[i] >= arr.GetLength(0) || x + dirX[i] < 0)
+                {
+                    dirX[i] = 0;
+                }
+
+
+                if (y + dirY[i] >= arr.GetLength(0) || y + dirY[i] < 0)
+                {
+                    dirY[i] = 0;
+                }
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (arr[x + dirX[i], y + dirY[i]] == 0)
+                {
+                    return true;
+                }
+            }
 
             return false;
         }
 
-        static void find_cell ( int[,] arr, out int x, out int y )
+        static void find_cell(int[,] arr, out int x, out int y)
         {
             x = 0;
             y = 0;
-            for( int i = 0; i < arr.GetLength( 0 ); i++ )
-
-                for( int j = 0; j < arr.GetLength( 0 ); j++ )
-                    if( arr[i, j] == 0 ) {x = i;y = j;return;}
-
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(0); j++)
+                {
+                    if (arr[i, j] == 0)
+                    {
+                        x = i;
+                        y = j;
+                        return;
+                    }
+                }
+            }
         }
 
-        static void Main( string[] args ){
+        static void Main(string[] args)
+        {
             //Console.WriteLine( "Enter a positive number " );
             //string input = Console.ReadLine(  );
             //int n = 0;
@@ -53,40 +89,82 @@ namespace Task3{
             //}
             int n = 3;
             int[,] matrica = new int[n, n];
-            int step = n, k = 1, i = 0, j = 0, dx = 1, dy = 1;
-            while( true ){ //malko e kofti tova uslovie, no break-a raboti 100% : )
+            int step = n;
+            var k = 1;
+            var i = 0;
+            var j = 0;
+            var dx = 1;
+            var dy = 1;
+
+            while (true)
+            { //malko e kofti tova uslovie, no break-a raboti 100% : )
                 matrica[i, j] = k;
 
-                if( !proverka( matrica, i, j ) ){ break; } // prekusvame ako sme se zadunili
-                if( i+dx >= n || i+dx < 0 || j+dy >= n || j+dy < 0 || matrica[i+dx, j+dy] != 0 )
+                if (!proverka(matrica, i, j))
+                {
+                    break;
+                } // prekusvame ako sme se zadunili
 
-
-                while ( ( i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrica[i + dx, j + dy] != 0 ) ) {change( ref dx, ref dy );}
-                i += dx; j += dy; k++;
-            }
-            for( int p = 0; p < n; p++ ){
-                for( int q = 0; q < n; q++ ) Console.Write( "{0,3}", matrica[p, q] );
-                Console.WriteLine(  );                
-            }
-            find_cell( matrica, out i, out j );
-            if( i != 0 && j != 0 ) { // taka go napravih, zashtoto funkciqta ne mi davashe da ne si definiram out parametrite
-                dx = 1; dy = 1;
-
-
-                while ( true ) { //malko e kofti tova uslovie, no break-a raboti 100% : )
-                    matrica[i, j] = k;
-                    if( !proverka( matrica, i, j ) ){break;}// prekusvame ako sme se zadunili
-                    if( i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrica[i + dx, j + dy] != 0 )
-
-
-                    while ( ( i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrica[i + dx, j + dy] != 0 ) ) change( ref dx, ref dy );
-                    i += dx; j += dy; k++;
+                if (i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrica[i + dx, j + dy] != 0)
+                {
+                    while ((i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrica[i + dx, j + dy] != 0))
+                    {
+                        change(ref dx, ref dy);
+                    }
                 }
-            }            
-            for( int pp = 0; pp < n; pp++ ){
-                for( int qq = 0; qq < n; qq++ )Console.Write( "{0,3}", matrica[pp, qq] );
 
-                Console.WriteLine(  );
+                i += dx;
+                j += dy;
+                k++;
+            }
+
+            for (int p = 0; p < n; p++)
+            {
+                for (int q = 0; q < n; q++)
+                {
+                    Console.Write("{0,3}", matrica[p, q]);
+                }
+
+                Console.WriteLine();
+            }
+
+            find_cell(matrica, out i, out j);
+
+            if (i != 0 && j != 0)
+            { // taka go napravih, zashtoto funkciqta ne mi davashe da ne si definiram out parametrite
+                dx = 1;
+                dy = 1;
+
+                while (true)
+                { //malko e kofti tova uslovie, no break-a raboti 100% : )
+                    matrica[i, j] = k;
+                    if (!proverka(matrica, i, j))
+                    {
+                        break;
+                    }// prekusvame ako sme se zadunili
+
+                    if (i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrica[i + dx, j + dy] != 0)
+                    {
+                        while ((i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrica[i + dx, j + dy] != 0))
+                        {
+                            change(ref dx, ref dy);
+                        }
+                    }
+
+                    i += dx;
+                    j += dy;
+                    k++;
+                }
+            }
+
+            for (int pp = 0; pp < n; pp++)
+            {
+                for (int qq = 0; qq < n; qq++)
+                {
+                    Console.Write("{0,3}", matrica[pp, qq]);
+                }
+
+                Console.WriteLine();
             }
         }
     }
