187fe8e27d0b3c5b1d2b89678abc4dd5d203d073
shakuu <todorov.h@gmail.com>
2016-09-22 11:57:33 +0300

    more tests

---
+++ HQC-Part-II/homework-refactoring/MatrixPath.Logic.Tests/Matrices/BasicMatrixTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+using MatrixPath.Logic.Cells.Contracts;
+using MatrixPath.Logic.Directions.Contracts;
+using MatrixPath.Logic.Matrices;
+using MatrixPath.Logic.Utils;
+using MatrixPath.Logic.Values.Contracts;
+
+using Moq;
+using NUnit.Framework;
+
+namespace MatrixPath.Logic.Tests.Matrices
+{
+    [TestFixture]
+    public class BasicMatrixTests
+    {
+        [Test]
+        public void Constructor_ShouldThrow_WhenMatrixSizeParameterIsLessThanOrEqualToZero()
+        {
+            var invalidMatrixSize = 0;
+            Func<int, int, int, ICell> createCellMethod = InstantiatingMethods.CreateMatrixCell;
+
+            Assert.That(
+                () => new BasicMatrix(invalidMatrixSize, createCellMethod),
+                Throws.InstanceOf<ArgumentOutOfRangeException>().With.Message.Contains("Matrix size must be larger than 0"));
+        }
+
+        [Test]
+        public void Constructor_ShouldThrow_WhenCreateCellParameterIsInvalid()
+        {
+            var validMatrixSize = 10;
+
+            Assert.That(
+                () => new BasicMatrix(validMatrixSize, null),
+                Throws.ArgumentNullException.With.Message.Contains("createCell"));
+        }
+
+        [Test]
+        public void Constructor_ShouldReturnANewBasicMatrixInstance_WhenAllParametersAreValid()
+        {
+            var validMatrixSize = 12;
+            Func<int, int, int, ICell> createCellMethod = InstantiatingMethods.CreateMatrixCell;
+
+            var actualMatrix = new BasicMatrix(validMatrixSize, createCellMethod);
+
+            Assert.That(actualMatrix, Is.InstanceOf<BasicMatrix>());
+        }
+
+        [Test]
+        public void Populate_ShouldThrow_WhenStartPositionParameterIsInvalid()
+        {
+            var validMatrixSize = 12;
+            Func<int, int, int, ICell> createCellMethod = InstantiatingMethods.CreateMatrixCell;
+            var matrix = new BasicMatrix(validMatrixSize, createCellMethod);
+
+            var mockSequence = new Mock<IDirectionSequence>();
+            var mockValues = new Mock<ICellValueSequence>();
+
+            Assert.That(
+                () => matrix.Populate(null, mockSequence.Object, mockValues.Object),
+                Throws.ArgumentNullException.With.Message.Contains("startPosition"));
+        }
+
+        [Test]
+        public void Populate_ShouldThrow_WhenDirectionsInstructionsParameterIsInvalid()
+        {
+            var validMatrixSize = 12;
+            Func<int, int, int, ICell> createCellMethod = InstantiatingMethods.CreateMatrixCell;
+            var matrix = new BasicMatrix(validMatrixSize, createCellMethod);
+
+            var mockPosition = new Mock<IPosition>();
+            var mockValues = new Mock<ICellValueSequence>();
+
+            Assert.That(
+                () => matrix.Populate(mockPosition.Object, null, mockValues.Object),
+                Throws.ArgumentNullException.With.Message.Contains("directionsInstructions"));
+        }
+
+        [Test]
+        public void Populate_ShouldThrow_WhenValueProviderParameterIsInvalid()
+        {
+            var validMatrixSize = 12;
+            Func<int, int, int, ICell> createCellMethod = InstantiatingMethods.CreateMatrixCell;
+            var matrix = new BasicMatrix(validMatrixSize, createCellMethod);
+
+            var mockPosition = new Mock<IPosition>();
+            var mockSequence = new Mock<IDirectionSequence>();
+
+            Assert.That(
+                () => matrix.Populate(mockPosition.Object, mockSequence.Object, null),
+                Throws.ArgumentNullException.With.Message.Contains("valueProvider"));
+        }
+    }
+}
