2c3404cc6694674f6d4ef6caa48539b1bc9fc036
shakuu <todorov.h@gmail.com>
2016-10-02 19:23:42 +0300

    update

--- javascript-applications/self_manager/unit-tests/services/login-service-tests.js
+++ javascript-applications/self_manager/unit-tests/services/login-service-tests.js
@@ -1,6 +1,6 @@
 describe('loginService', () => {
     describe('login', () => {
-        it('Should invoke ajaxRequester.putJSON method.', () => {
+        it('Should invoke ajaxRequester.putJSON method.', (done) => {
             const fakeUser = {
                 username: 'fake',
                 authKey: 'fake'
@@ -19,14 +19,17 @@ describe('loginService', () => {
                 }))
                 .once();
 
-            loginService.login();
-
-            mockRequester.verify();
-
-            credentialManager.save.restore();
+            loginService.login()
+                .then(() => {
+                    mockRequester.verify();
+                })
+                .then(() => {
+                    credentialManager.save.restore();
+                })
+                .then(done, done);
         });
 
-        it('Should invoke ajaxRequester.putJSON method with correct url.', () => {
+        it('Should invoke ajaxRequester.putJSON method with correct url.', (done) => {
             const fakeUser = {
                 username: 'fake',
                 authKey: 'fake'
@@ -46,14 +49,17 @@ describe('loginService', () => {
                 .withArgs('api/users/auth')
                 .once();
 
-            loginService.login();
-
-            mockRequester.verify();
-
-            credentialManager.save.restore();
+            loginService.login()
+                .then(() => {
+                    mockRequester.verify();
+                })
+                .then(() => {
+                    credentialManager.save.restore();
+                })
+                .then(done, done);
         });
 
-        it('Should invoke ajaxRequester.putJSON method with correct user.', () => {
+        it('Should invoke ajaxRequester.putJSON method with correct user.', (done) => {
             const fakeUser = {
                 username: 'fake',
                 authKey: 'fake'
@@ -78,11 +84,51 @@ describe('loginService', () => {
                 .withArgs('api/users/auth', fakeUserParameter)
                 .once();
 
-            loginService.login(fakeUserParameter);
+            loginService.login(fakeUserParameter)
+                .then(() => {
+                    mockRequester.verify();
+                })
+                .then(() => {
+                    credentialManager.save.restore();
+                })
+                .then(done, done);
+        });
 
-            mockRequester.verify();
+        it('Should invoke credentialManager.save method with correct user.', (done) => {
+            const fakeUser = {
+                username: 'fake',
+                authKey: 'fake'
+            };
 
-            credentialManager.save.restore();
+            const fakeResponse = {
+                result: fakeUser
+            };
+
+            const fakeUserParameter = {
+                username: 'parameter',
+                passHash: 'password'
+            };
+
+            const stubAjaxRequesterPutJSON = sinon.stub(ajaxRequester, 'putJSON');
+            stubAjaxRequesterPutJSON
+                .returns(new Promise((resolve, reject) => {
+                    resolve(fakeResponse);
+                }));
+
+            const mockCredentialManager = sinon.mock(credentialManager);
+            mockCredentialManager
+                .expects('save')
+                .withArgs(fakeUser)
+                .once();
+
+            loginService.login(fakeUserParameter)
+                .then(() => {
+                    mockCredentialManager.verify();
+                })
+                .then(() => {
+                    ajaxRequester.putJSON.restore();
+                })
+                .then(done, done);
         });
     });
 });
