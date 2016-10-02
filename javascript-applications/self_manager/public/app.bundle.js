d25acef25f2ce8c3b0f8c79142d2775c6618875a
shakuu <todorov.h@gmail.com>
2016-10-02 14:42:52 +0300

    update

--- javascript-applications/self_manager/public/app.bundle.js
+++
@@ -1,1 +0,0 @@
-!function(t){function r(n){if(e[n])return e[n].exports;var o=e[n]={exports:{},id:n,loaded:!1};return t[n].call(o.exports,o,o.exports,r),o.loaded=!0,o.exports}var e={};return r.m=t,r.c=e,r.p="",r(0)}([function(t,r){"use strict";$(function(){appRouter.start("#content")})}]);
