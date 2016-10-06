1d6d46c79a6888a94b1892e209ecbf4cc6af6c4a
shakuu <todorov.h@gmail.com>
2016-10-07 01:19:20 +0300

    update

---
+++ HQC-Part-II/HQC-Exam-ConsoleWebServer/ConsoleWebServer/ConsoleWebServer.Tests/Http/ResponseProviderTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+
+using ConsoleWebServer.Framework;
+using ConsoleWebServer.Framework.Http;
+using ConsoleWebServer.Framework.Http.Contracts;
+
+using NUnit.Framework;
+using Moq;
+
+namespace ConsoleWebServer.Tests.Http
+{
+    [TestFixture]
+    public class ResponseProviderTests
+    {
+        [Test]
+        public void Constructor_ShouldThrow_WhenIHttpRequestManagerParameterIsNotValid()
+        {
+            IHttpRequestManager requestManager = null;
+            var actionInvoker = new Mock<IActionInvoker>();
+            var staticFileHandler = new Mock<IStaticFileHandler>();
+            var createResponse = new Mock<Func<Version, HttpStatusCode, string, IHttpResponse>>();
+
+            staticFileHandler.Setup(handler => handler.CanHandle(It.IsAny<IHttpRequest>())).Returns(true);
+
+            Assert.That(
+                () => new ResponseProvider(requestManager, actionInvoker.Object, staticFileHandler.Object, createResponse.Object),
+                Throws.ArgumentNullException.With.Message.Contain("requestManager"));
+        }
+    }
+}
