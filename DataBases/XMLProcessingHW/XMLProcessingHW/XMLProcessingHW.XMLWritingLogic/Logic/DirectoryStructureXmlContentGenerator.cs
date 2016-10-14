4313dcb42a8a3a9433ca0b6a027944068b299de5
shakuu <todorov.h@gmail.com>
2016-10-14 21:04:46 +0300

    уп

--- DataBases/XMLProcessingHW/XMLProcessingHW/XMLProcessingHW.XMLWritingLogic/Logic/DirectoryStructureXmlContentGenerator.cs
+++ DataBases/XMLProcessingHW/XMLProcessingHW/XMLProcessingHW.XMLWritingLogic/Logic/DirectoryStructureXmlContentGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 using XMLProcessingHW.XMLWritingLogic.Contracts;
 
@@ -7,18 +8,28 @@ namespace XMLProcessingHW.XMLWritingLogic.Logic
 {
     public class DirectoryStructureXmlContentGenerator : IXmlContentGenerator
     {
+        private const string ElementNameDirectory = "directory";
+        private const string ElementNameFile = "file";
+
         private readonly string rootDirectory;
+        private readonly Func<string, string, IXmlContent> contentCreator;
 
-        private IEnumerable<IXmlContent> generatedContent;
+        private ICollection<IXmlContent> generatedContent;
 
-        public DirectoryStructureXmlContentGenerator(string rootDirectory)
+        public DirectoryStructureXmlContentGenerator(string rootDirectory, Func<string, string, IXmlContent> contentCreator)
         {
             if (string.IsNullOrEmpty(rootDirectory))
             {
                 throw new ArgumentNullException(nameof(rootDirectory));
             }
 
+            if (contentCreator == null)
+            {
+                throw new ArgumentNullException(nameof(contentCreator));
+            }
+
             this.rootDirectory = rootDirectory;
+            this.contentCreator = contentCreator;
         }
 
         public IEnumerable<IXmlContent> GeneratedContent
@@ -35,9 +46,26 @@ namespace XMLProcessingHW.XMLWritingLogic.Logic
             }
         }
 
-        private IEnumerable<IXmlContent> GenerateContent(string rootDirectory)
+        private ICollection<IXmlContent> GenerateContent(string rootDirectory)
         {
             throw new NotImplementedException();
+        }
+
+        private void ParseDirectory(string currentDirectory)
+        {
+            var directoryInfo = new DirectoryInfo(currentDirectory);
+
+            var contentElementName = DirectoryStructureXmlContentGenerator.ElementNameDirectory;
+            var contentElementText = directoryInfo.FullName;
+            var content = this.contentCreator(contentElementName, contentElementText);
+            this.generatedContent.Add(content);
+
+            var subDirectories = directoryInfo.GetDirectories();
+            foreach (var directory in subDirectories)
+            {
+                var directoryName = directory.FullName;
+                this.ParseDirectory(directoryName);
+            }
         }
     }
 }
