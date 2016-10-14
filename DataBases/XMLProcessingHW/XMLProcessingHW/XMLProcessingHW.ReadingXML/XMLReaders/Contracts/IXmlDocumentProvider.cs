e177d1a669834718741fff76a86659b43a8f1c21
shakuu <todorov.h@gmail.com>
2016-10-14 09:06:34 +0300

    up

--- DataBases/XMLProcessingHW/XMLProcessingHW/XMLProcessingHW.ReadingXML/XMLReaders/Contracts/IXmlDocumentProvider.cs
+++ DataBases/XMLProcessingHW/XMLProcessingHW/XMLProcessingHW.ReadingXML/XMLReaders/Contracts/IXmlDocumentProvider.cs
@@ -4,6 +4,8 @@ namespace XMLProcessingHW.ReadingXML.XMLReaders.Contracts
 {
     public interface IXmlDocumentProvider
     {
+        XmlReader GetXmlReader(string fileName);
+
         XmlDocument GetXmlDocument(string fileName);
 
         void SetXmlDocument(string fileName, XmlDocument document);
--- DataBases/XMLProcessingHW/XMLProcessingHW/XMLProcessingHW.ReadingXML/XMLReaders/XmlDocumentProvider.cs
+++ DataBases/XMLProcessingHW/XMLProcessingHW/XMLProcessingHW.ReadingXML/XMLReaders/XmlDocumentProvider.cs
@@ -6,6 +6,12 @@ namespace XMLProcessingHW.ReadingXML.XMLReaders
 {
     public class XmlDocumentProvider : IXmlDocumentProvider
     {
+        public XmlReader GetXmlReader(string fileName)
+        {
+            var reader = XmlReader.Create(fileName);
+            return reader;
+        }
+
         public XmlDocument GetXmlDocument(string fileName)
         {
             var xmlDocument = new XmlDocument();
