8a54cb2e56232bf5aeefd4a7b784a22135eb3b4c
shakuu <todorov.h@gmail.com>
2016-10-16 01:07:54 +0300

    up

--- DataBases/JSONProcessingHW/JSONProcessingHW/JSONProcessingHW.Logic/DataServices/WebClientDataService.cs
+++ DataBases/JSONProcessingHW/JSONProcessingHW/JSONProcessingHW.Logic/DataServices/WebClientDataService.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net;
 
 using JSONProcessingHW.Logic.DataServices.Contracts;
@@ -9,6 +10,16 @@ namespace JSONProcessingHW.Logic.DataServices
     {
         public void GetData(string url, string fileName)
         {
+            if (url == null)
+            {
+                throw new ArgumentNullException(nameof(url));
+            }
+
+            if (fileName == null)
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+
             var fileExists = File.Exists(fileName);
             if (!fileExists)
             {
