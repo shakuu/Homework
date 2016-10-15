79a82ca569bd90c813422078cff7fafd776dbda6
shakuu <todorov.h@gmail.com>
2016-10-16 01:13:46 +0300

    up

--- DataBases/JSONProcessingHW/JSONProcessingHW/JSONProcessingHW.Logic/Parsers/JsonParser.cs
+++ DataBases/JSONProcessingHW/JSONProcessingHW/JSONProcessingHW.Logic/Parsers/JsonParser.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 using JSONProcessingHW.Logic.Models.Contracts;
@@ -8,14 +9,31 @@ using Newtonsoft.Json.Linq;
 
 namespace JSONProcessingHW.Logic.Parsers
 {
-    public class JsonParser<T> : IJsonParser<T> where T : IModel, new()
+    public class JsonParser<ModelType> : IJsonParser<ModelType> where ModelType : IModel, new()
     {
-        public IEnumerable<T> ParseJson(string json, string rootName, string elementName)
+        public IEnumerable<ModelType> ParseJson(string json, string rootName, string elementName)
         {
-            var jsonObject = JObject.Parse(json);
-            var elements = jsonObject.SelectToken(rootName).SelectTokens(elementName);
+            if (json == null)
+            {
+                throw new ArgumentNullException(nameof(json));
+            }
 
-            var result = elements.Children().Select(el => new T
+            if (rootName == null)
+            {
+                throw new ArgumentNullException(nameof(rootName));
+            }
+
+            if (elementName == null)
+            {
+                throw new ArgumentNullException(nameof(elementName));
+            }
+
+            var jsonObject = JObject.Parse(json);
+            var rootElement = jsonObject.SelectToken(rootName);
+            var modelElements = rootElement.SelectTokens(elementName);
+
+            var modelElementsChildren = modelElements.Children();
+            var result = modelElementsChildren.Select(el => new ModelType
             {
                 Title = (string)el.SelectToken("title"),
                 Url = (string)el.SelectToken("link").SelectToken("@href")
