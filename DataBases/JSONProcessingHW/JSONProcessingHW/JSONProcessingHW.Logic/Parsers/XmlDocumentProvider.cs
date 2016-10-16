5ba2a339f0261df642be794676b9a15b819c4962
shakuu <todorov.h@gmail.com>
2016-10-17 00:20:42 +0300

    up

--- DataBases/JSONProcessingHW/JSONProcessingHW/JSONProcessingHW.Logic/Parsers/XmlDocumentProvider.cs
+++ DataBases/JSONProcessingHW/JSONProcessingHW/JSONProcessingHW.Logic/Parsers/XmlDocumentProvider.cs
@@ -18,25 +18,37 @@ namespace JSONProcessingHW.Logic.Parsers
             var validationSchemaIsProvided = validationSchema != null;
             if (validationSchemaIsProvided)
             {
-                var xmlReaderSettings = new XmlReaderSettings();
-                xmlReaderSettings.Schemas.Add("http://www.w3.org/2005/Atom", validationSchema);
-                xmlReaderSettings.ValidationType = ValidationType.Schema;
-
-                var xmlReader = XmlReader.Create(fileName, xmlReaderSettings);
-                var xmlDocument = new XmlDocument();
-                xmlDocument.Load(xmlReader);
-
-                var failedValidationEventHandler = new ValidationEventHandler(OnFailedValidation);
-                xmlDocument.Validate(failedValidationEventHandler);
-
+                var xmlDocument = this.GetXmlDocumentWithValidation(fileName, validationSchema);
                 return xmlDocument;
             }
             else
             {
-                var xmlDocument = new XmlDocument();
-                xmlDocument.Load(fileName);
+                var xmlDocument = this.GetXmlDocumentWithoutValidation(fileName);
                 return xmlDocument;
             }
+        }
+
+        private XmlDocument GetXmlDocumentWithoutValidation(string fileName)
+        {
+            var xmlDocument = new XmlDocument();
+            xmlDocument.Load(fileName);
+            return xmlDocument;
+        }
+
+        private XmlDocument GetXmlDocumentWithValidation(string fileName, string validationSchema)
+        {
+            var xmlReaderSettings = new XmlReaderSettings();
+            xmlReaderSettings.Schemas.Add("http://www.w3.org/2005/Atom", validationSchema);
+            xmlReaderSettings.ValidationType = ValidationType.Schema;
+
+            var xmlReader = XmlReader.Create(fileName, xmlReaderSettings);
+            var xmlDocument = new XmlDocument();
+            xmlDocument.Load(xmlReader);
+
+            var failedValidationEventHandler = new ValidationEventHandler(OnFailedValidation);
+            xmlDocument.Validate(failedValidationEventHandler);
+
+            return xmlDocument;
         }
 
         private void OnFailedValidation(object sender, ValidationEventArgs e)
