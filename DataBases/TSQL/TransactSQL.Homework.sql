d14db2a6f21b260619f0de1e07edba288cffe8b7
shakuu <todorov.h@gmail.com>
2016-10-20 00:13:07 +0300

    up

--- DataBases/TSQL/TransactSQL.Homework.sql
+++ DataBases/TSQL/TransactSQL.Homework.sql
@@ -65,13 +65,88 @@ GO
 /* Create a function that accepts as parameters – sum, yearly interest rate and number of months.
 	It should calculate and return the new sum.
 	Write a SELECT to test whether the function works as expected. */
---CREATE FUNCTION ufn_CalculateInterest(@amount money, @interestPerYear int, @periodInMonths int)
---	RETURNS money
---	AS
---		BEGIN
---			DECLARE @result money, @interest int
---			SET @interest = 
---			SET @result = 
+USE TSQL_HW
+GO
+CREATE FUNCTION ufn_CalculateInterest(@amount money, @interestPerYear money, @periodInMonths int)
+	RETURNS money
+	AS
+		BEGIN
+			DECLARE @result money, @interest money
+			SET @interest = @interestPerYear / 12 * @periodInMonths
+			SET @result = (@amount * @interest) + @amount
 
---			RETURN 0
---		END
+			RETURN @result
+		END
+GO
+
+USE TSQL_HW
+SELECT dbo.ufn_CalculateInterest(1000, 10, 12)
+
+GO
+/* Create a stored procedure that uses the function from the previous example to give an 
+	interest to a person's account for one month.
+	It should take the AccountId and the interest rate as parameters. */
+CREATE PROCEDURE usp_ApplyInterest(@accountId int, @interestRate money)
+	AS
+		UPDATE Accounts 
+		SET Balance = dbo
+			.ufn_CalculateInterest(
+				(SELECT a.Balance 
+					FROM Accounts a 
+					WHERE a.Id = @accountId), 
+				@interestRate, 
+				1)
+		WHERE Id = @accountId
+GO
+
+EXEC usp_ApplyInterest 1, 10
+
+SELECT *
+FROM Accounts a
+WHERE a.Id = 1
+
+/* Add two more stored procedures WithdrawMoney(AccountId, money) 
+	and DepositMoney(AccountId, money) that operate in transactions. */
+GO
+
+CREATE PROCEDURE usp_WithdrawMoney(@accountId int, @amount money)
+	AS
+	BEGIN TRANSACTION
+		UPDATE Accounts
+		SET Balance = (-@amount) + (
+			SELECT a.Balance
+			FROM Accounts a
+			WHERE a.Id = @accountId
+		)
+	COMMIT
+
+GO
+
+EXEC usp_WithdrawMoney 1, 100
+
+SELECT Balance
+FROM Accounts a
+WHERE a.Id = 1
+
+GO
+
+CREATE PROCEDURE usp_DepositMoney(@accountId int, @amount money)
+	AS
+	BEGIN TRANSACTION
+		UPDATE Accounts
+		SET Balance = (@amount) + (
+			SELECT a.Balance
+			FROM Accounts a
+			WHERE a.Id = @accountId
+		)
+	COMMIT
+
+GO
+
+EXEC usp_DepositMoney 1, 100
+
+SELECT Balance
+FROM Accounts a
+WHERE a.Id = 1
+
+GO
