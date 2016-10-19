acfd352651d2b1540ac0e67bd2db556d710f0fbd
shakuu <todorov.h@gmail.com>
2016-10-19 18:08:22 +0300

    up

--- DataBases/TSQL/TransactSQL.Homework.sql
+++ DataBases/TSQL/TransactSQL.Homework.sql
@@ -59,3 +59,19 @@ CREATE PROCEDURE usp_PersonsWithBalanceHigherThen(@minimumAmount money = 0)
 GO
 
 EXEC usp_PersonsWithBalanceHigherThen 5000
+
+GO
+
+/* Create a function that accepts as parameters – sum, yearly interest rate and number of months.
+	It should calculate and return the new sum.
+	Write a SELECT to test whether the function works as expected. */
+--CREATE FUNCTION ufn_CalculateInterest(@amount money, @interestPerYear int, @periodInMonths int)
+--	RETURNS money
+--	AS
+--		BEGIN
+--			DECLARE @result money, @interest int
+--			SET @interest = 
+--			SET @result = 
+
+--			RETURN 0
+--		END
