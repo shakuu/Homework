8b4172a271333c168ae714a77b4b6fe3eae0f1ff
shakuu <todorov.h@gmail.com>
2016-10-20 20:51:26 +0300

    up

--- DataBases/JSONProcessingHW/ADONET.Homework/ADONET.Homework.ConsoleClient/Program.cs
+++ DataBases/JSONProcessingHW/ADONET.Homework/ADONET.Homework.ConsoleClient/Program.cs
@@ -2,6 +2,10 @@
 
 using Ninject;
 
+using ADONET.Homework.Logic.Models;
+using ADONET.Homework.Logic.QueryEngines.Contract;
+using ADONET.Homework.Logic.CommandProviders.Contracts;
+
 namespace ADONET.Homework.ConsoleClient
 {
     public class Program
@@ -10,6 +14,12 @@ namespace ADONET.Homework.ConsoleClient
         {
             var ninject = new StandardKernel();
             ninject.Load(Assembly.GetExecutingAssembly());
+
+            var commandProvider = ninject.Get<ICommandProvider>();
+            var command = commandProvider.CreateCommand("SELECT * FROM Categories", null);
+
+            var queryEngine = ninject.Get<IQueryEngine>();
+            queryEngine.ExecuteCommand<Category>(command);
         }
     }
 }
