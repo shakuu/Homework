936adbda3631845c87b0b2661099730adf9ae225
shakuu <todorov.h@gmail.com>
2016-10-20 20:47:29 +0300

    up

---
+++ DataBases/JSONProcessingHW/ADONET.Homework/ADONET.Homework.ConsoleClient/Bindings/NinjectBindings.cs
@@ -0,0 +1,32 @@
+﻿using Ninject;
+using Ninject.Modules;
+
+using ADONET.Homework.Logic.ConnectionProviders.Contracts;
+using ADONET.Homework.Logic.ConnectionProviders;
+using ADONET.Homework.Logic.CommandProviders.Contracts;
+using ADONET.Homework.Logic.CommandProviders;
+using ADONET.Homework.Logic.DataHandlers.Contracts;
+using ADONET.Homework.Logic.DataHandlers;
+using ADONET.Homework.Logic.QueryServices.Contracts;
+using ADONET.Homework.Logic.QueryServices;
+using ADONET.Homework.Logic.QueryEngines.Contract;
+using ADONET.Homework.Logic.QueryEngines;
+
+namespace ADONET.Homework.ConsoleClient.Bindings
+{
+    public class NinjectBindings : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<IConnectionProvider>().To<DefaultSqlServerConnectionProvider>();
+            this.Bind<ICommandProvider>().To<CommandProvider>();
+            this.Bind<IDataHandler>().To<DataHandler>();
+            this.Bind<IQueryService>().To<QueryService>();
+
+            this.Bind<IQueryEngine>().To<SimpleQueryEngine>()
+                .WithConstructorArgument("connectionProvider", ctx => ctx.Kernel.Get<IConnectionProvider>())
+                .WithConstructorArgument("queryService", ctx => ctx.Kernel.Get<IQueryService>())
+                .WithConstructorArgument("dataHandler", ctx => ctx.Kernel.Get<IDataHandler>());
+        }
+    }
+}
--- DataBases/JSONProcessingHW/ADONET.Homework/ADONET.Homework.ConsoleClient/Program.cs
+++ DataBases/JSONProcessingHW/ADONET.Homework/ADONET.Homework.ConsoleClient/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace ADONET.Homework.ConsoleClient
+﻿using System.Reflection;
+
+using Ninject;
+
+namespace ADONET.Homework.ConsoleClient
 {
     public class Program
     {
         public static void Main()
         {
+            var ninject = new StandardKernel();
+            ninject.Load(Assembly.GetExecutingAssembly());
         }
     }
 }
