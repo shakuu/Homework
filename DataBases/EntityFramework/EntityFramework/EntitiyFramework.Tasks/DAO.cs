60d0089b111dfddf01f788b1dcefa008777205a3
shakuu <todorov.h@gmail.com>
2016-10-26 16:58:35 +0300

    up

---
+++ DataBases/EntityFramework/EntityFramework/EntitiyFramework.Tasks/DAO.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+using EntityFramework.Data;
+
+namespace EntitiyFramework.Tasks
+{
+    public class DAO
+    {
+        private static NorthwindEntities GetContext()
+        {
+            var dbContext = new NorthwindEntities();
+            return dbContext;
+        }
+
+        public static Customer GetById(string id)
+        {
+            var context = DAO.GetContext();
+            var customer = context.Customers.FirstOrDefault(c => c.CustomerID == id);
+            return customer;
+        }
+
+        public static void InsertCustomer(Customer customer)
+        {
+            if (customer == null)
+            {
+                throw new ArgumentNullException(nameof(customer));
+            }
+
+            var context = DAO.GetContext();
+            context.Customers.Add(customer);
+            context.SaveChanges();
+        }
+
+        public static void ModifyCustomer(Customer modifiedCustomer)
+        {
+            if (modifiedCustomer == null)
+            {
+                throw new ArgumentNullException(nameof(modifiedCustomer));
+            }
+
+            var context = DAO.GetContext();
+            var customerWithId = context.Customers
+                .FirstOrDefault(customer => customer.CustomerID == modifiedCustomer.CustomerID);
+
+            if (customerWithId == null)
+            {
+                throw new ArgumentException("Customer does not exist.");
+            }
+
+            var values = context.Entry(customerWithId).CurrentValues;
+            values.SetValues(modifiedCustomer);
+            context.SaveChanges();
+        }
+
+        public static void RemoveCustomer(Customer customer)
+        {
+            if (customer == null)
+            {
+                throw new ArgumentNullException(nameof(customer));
+            }
+
+            var context = DAO.GetContext();
+            var matchingCustomer = context.Customers.FirstOrDefault(c => c.CustomerID == customer.CustomerID);
+            context.Customers.Remove(matchingCustomer);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ DataBases/EntityFramework/EntityFramework/EntityFramework.ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using EntityFramework.Data;
+using EntitiyFramework.Tasks;
+
+namespace EntityFramework.ConsoleApp
+{
+    public class Program
+    {
+        private const string CustomerId = "ASDFG";
+
+        public static void Main()
+        {
+            Program.InsertCustomer(Program.CustomerId, "Company");
+            Program.ModifyCustomer(Program.CustomerId);
+            Program.RemoveCustomer(Program.CustomerId);
+        }
+
+        private static void InsertCustomer(string id, string companyName)
+        {
+            var customer = new Customer()
+            {
+                CustomerID = id,
+                CompanyName = companyName
+            };
+
+            DAO.InsertCustomer(customer);
+        }
+
+        private static void ModifyCustomer(string id)
+        {
+            var customer = DAO.GetById(id);
+            customer.ContactName = "contact name";
+            DAO.ModifyCustomer(customer);
+        }
+
+        private static void RemoveCustomer(string id)
+        {
+            var customer = DAO.GetById(id);
+            DAO.RemoveCustomer(customer);
+        }
+    }
+}
