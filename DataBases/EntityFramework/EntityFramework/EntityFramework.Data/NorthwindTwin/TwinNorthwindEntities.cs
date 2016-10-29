4389d1752ae2a5ee6ff225428b5e800548f19f53
shakuu <todorov.h@gmail.com>
2016-10-29 03:14:28 +0300

    up

---
+++ DataBases/EntityFramework/EntityFramework/EntityFramework.Data/NorthwindTwin/TwinNorthwindEntities.cs
@@ -0,0 +1,12 @@
+﻿using EntityFramework.Data;
+
+namespace EntityFramework.Data.NorthwindTwin
+{
+    public class TwinNorthwindEntities : NorthwindEntities
+    {
+        public TwinNorthwindEntities(string connectionString)
+           : base($"name={connectionString}")
+        {
+        }
+    }
+}
