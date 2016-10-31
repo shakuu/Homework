5c2348d03ae3c21d9f3ba497955a37a24aaa86b6
shakuu <todorov.h@gmail.com>
2016-10-31 23:27:15 +0200

    up

---
+++ DataBases/WorkshopEntityFramework-Code-first/SocialNetwork/SocialNetwork.ConsoleClient/XmlDeserializers/XmlToSqlModelConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using SocialNetwork.ConsoleClient.XmlDeserializers.Models;
+using SocialNetwork.Data;
+using SocialNetwork.Models;
+
+namespace SocialNetwork.ConsoleClient.XmlDeserializers
+{
+    public class XmlToSqlModelConverter
+    {
+        private readonly Func<SocialNetworkContext> contextCreator;
+
+        public XmlToSqlModelConverter(Func<SocialNetworkContext> contextCreator)
+        {
+            this.contextCreator = contextCreator;
+        }
+
+        public void ConvertXmlFriendshipToSqlFriendship(IEnumerable<XmlFriendship> xmlFriendships)
+        {
+            if (xmlFriendships == null)
+            {
+                throw new ArgumentNullException(nameof(xmlFriendships));
+            }
+
+            var context = this.contextCreator();
+
+            var friendshipCount = 0;
+            foreach (var friendship in xmlFriendships)
+            {
+                friendshipCount++;
+
+                var newFriendship = new Friendship();
+
+
+            }
+        }
+    }
+}
