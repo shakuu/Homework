7ad4c4aea433f7b8eceabc923711d8e58bd82cd8
shakuu <todorov.h@gmail.com>
2016-11-02 00:18:18 +0200

    update

---
+++ DesignPatterns/Dealership-AuthorSolution/Dealership/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Dealership.Engine;
+
+namespace Dealership
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            DealershipEngine.Instance.Start();
+        }
+    }
+}
