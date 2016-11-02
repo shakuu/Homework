47518f12679d731f04c4fc014ba326eb9d14fda8
shakuu <todorov.h@gmail.com>
2016-11-02 15:40:20 +0200

    up

--- DesignPatterns/Dealership-AuthorSolution/Dealership/Startup.cs
+++ DesignPatterns/Dealership-AuthorSolution/Dealership/Startup.cs
@@ -13,8 +13,19 @@ namespace Dealership
             var ninject = new StandardKernel();
             ninject.Load(Assembly.GetExecutingAssembly());
 
+            Startup.SingletonTest(ninject);
+
             var engine = ninject.Get<IEngine>();
             engine.Start();
+        }
+
+        private static void SingletonTest(StandardKernel ninjectKernel)
+        {
+            var firstInstace = ninjectKernel.Get<IEngine>();
+            var secondInstance = ninjectKernel.Get<IEngine>();
+            var isSameReference = firstInstace == secondInstance;
+
+            System.Console.WriteLine($"{nameof(firstInstace)} and {nameof(secondInstance)} reference the same instance: {isSameReference}");
         }
     }
 }
