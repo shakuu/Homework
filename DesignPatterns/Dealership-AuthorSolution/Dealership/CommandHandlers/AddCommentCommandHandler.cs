2f79a096e6bce7707cd1204dafb51ab7b04aa98c
shakuu <todorov.h@gmail.com>
2016-11-02 13:49:37 +0200

    up

--- DesignPatterns/Dealership-AuthorSolution/Dealership/CommandHandlers/AddCommentCommandHandler.cs
+++ DesignPatterns/Dealership-AuthorSolution/Dealership/CommandHandlers/AddCommentCommandHandler.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+
 using Dealership.CommandHandlers.Base;
 using Dealership.CommandHandlers.Contracts;
 using Dealership.Engine;
