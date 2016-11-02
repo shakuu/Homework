3389e1d85997fdda7360bacfbbbc7de9ee5eaa4a
shakuu <todorov.h@gmail.com>
2016-11-02 14:24:17 +0200

    up

--- DesignPatterns/Dealership-AuthorSolution/Dealership/NinjectBindings/DealershipBindings.cs
+++ DesignPatterns/Dealership-AuthorSolution/Dealership/NinjectBindings/DealershipBindings.cs
@@ -2,6 +2,8 @@
 
 using Dealership.Common.Contracts;
 using Dealership.Common;
+using Dealership.CommandHandlers.Contracts;
+using Dealership.CommandHandlers;
 using Dealership.Factories;
 
 using Ninject.Modules;
@@ -14,6 +16,29 @@ namespace Dealership.NinjectBindings
         {
             this.Bind<IDealershipFactory>().To<DealershipFactory>();
             this.Bind<IIOProvider>().ToMethod(io => new GenericIOProvider(Console.Write, Console.ReadLine));
+            this.Bind<ICommandHandler>().ToMethod(ch =>
+            {
+                var registerUserHandler = new RegisterUserCommandHandler();
+                var loginHandler = new LoginCommandHandler();
+                var logoutHandler = new LogoutCommandHandler();
+                var addVehicleHandler = new AddVehicleCommandHandler();
+                var removeVehicleHandler = new RemoveVehicleCommandHandler();
+                var addCommnentHandler = new AddCommentCommandHandler();
+                var removeCommentHandler = new RemoveCommentCommandHandler();
+                var showUsersHandler = new ShowUsersCommandHandler();
+                var showVehiclesHandler = new ShowVehiclesCommandHandler();
+
+                registerUserHandler.AddCommandHandler(loginHandler);
+                loginHandler.AddCommandHandler(logoutHandler);
+                logoutHandler.AddCommandHandler(addVehicleHandler);
+                addVehicleHandler.AddCommandHandler(removeVehicleHandler);
+                removeVehicleHandler.AddCommandHandler(addCommnentHandler);
+                addCommnentHandler.AddCommandHandler(removeCommentHandler);
+                removeCommentHandler.AddCommandHandler(showUsersHandler);
+                showUsersHandler.AddCommandHandler(showVehiclesHandler);
+
+                return registerUserHandler;
+            });
         }
     }
 }
