569206763565c7e30c0d9f4b09243227c4eea594
shakuu <todorov.h@gmail.com>
2016-11-08 01:20:15 +0200

    up

---
+++ DataBases/AaxamPrep/ExamPrep/ExamPrep.Data.Utils/XmlDeserializers/Contracts/IXmlParser.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ExamPrep.Data.Utils.XmlDeserializers.Contracs
+{
+    public interface IXmlParser
+    {
+        IEnumerable<TModel> Deserialize<TModel>(string fileName, string rootElement);
+    }
+}
---
+++ DataBases/AaxamPrep/ExamPrep/ExamPrep.Data.Utils/XmlDeserializers/XmlParser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+using ExamPrep.Data.Utils.XmlDeserializers.Contracs;
+
+namespace ExamPrep.Data.Utils.XmlDeserializers
+{
+    public class XmlParser : IXmlParser
+    {
+        public IEnumerable<TModel> Deserialize<TModel>(string fileName, string rootElement)
+        {
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException("File not found!", fileName);
+            }
+
+            var serializer = new XmlSerializer(typeof(List<TModel>), new XmlRootAttribute(rootElement));
+            IEnumerable<TModel> result;
+            using (var fs = new FileStream(fileName, FileMode.Open))
+            {
+                result = (IEnumerable<TModel>)serializer.Deserialize(fs);
+            }
+
+            return result;
+        }
+    }
+}
