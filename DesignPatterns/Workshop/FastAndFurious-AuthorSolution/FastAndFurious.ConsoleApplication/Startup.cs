a44d2479e017600494ca97233d80f69af1cf312b
shakuu <todorov.h@gmail.com>
2016-11-10 18:43:59 +0200

    up

---
+++ DesignPatterns/Workshop/FastAndFurious-AuthorSolution/FastAndFurious.ConsoleApplication/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+using FastAndFurious.ConsoleApplication.Engine;
+
+using Ninject;
+
+namespace FastAndFurious.ConsoleApplication
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var ninject = new StandardKernel();
+            ninject.Load(Assembly.GetExecutingAssembly());
+
+            var engine = ninject.Get<IEngine>();
+            engine.Start();
+        }
+    }
+}
