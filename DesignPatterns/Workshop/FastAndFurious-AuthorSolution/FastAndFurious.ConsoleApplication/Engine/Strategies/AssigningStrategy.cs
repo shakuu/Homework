fd93ab9d7ce4e3f2dc35f5790b5e7909957e279f
shakuu <todorov.h@gmail.com>
2016-11-11 10:25:43 +0200

    up

--- DesignPatterns/Workshop/FastAndFurious-AuthorSolution/FastAndFurious.ConsoleApplication/Engine/Strategies/AssigningStrategy.cs
+++ DesignPatterns/Workshop/FastAndFurious-AuthorSolution/FastAndFurious.ConsoleApplication/Engine/Strategies/AssigningStrategy.cs
@@ -3,18 +3,16 @@ using System.Collections.Generic;
 
 using FastAndFurious.ConsoleApplication.Engine.Contracts;
 using FastAndFurious.ConsoleApplication.Common.Constants;
-using FastAndFurious.ConsoleApplication.Common.Extensions;
-using System.Linq;
 
 namespace FastAndFurious.ConsoleApplication.Engine.Strategies
 {
     public class AssigningStrategy : Strategy
     {
-        private readonly IEnumerable<ICommand> commands;
+        private readonly ICommand command;
 
-        public AssigningStrategy(IEnumerable<IAssigningCommand> commands)
+        public AssigningStrategy(ICommand command)
         {
-            this.commands = commands;
+            this.command = command;
         }
 
         protected override bool CanExecute(IList<string> commandParameters)
@@ -28,16 +26,15 @@ namespace FastAndFurious.ConsoleApplication.Engine.Strategies
             var objectToAssignId = int.Parse(commandParameters[2]);
             var ownerToAssignToId = int.Parse(commandParameters[5]);
 
-            var matchingCommand = this.commands.FirstOrDefault(c => c.IsCommand(commandParameters));
-            if (matchingCommand != null)
+            if (command.IsCommand(commandParameters))
             {
-                matchingCommand.ExecuteCommand(commandParameters, engineCollections);
+                command.ExecuteCommand(commandParameters, engineCollections);
             }
             else
             {
                 throw new NotSupportedException(GlobalConstants.AssigningOperationNotSupportedExceptionMessage);
             }
-            
+
             engineCollections.IoProvider.WriteLine(
                 String.Format(
                     GlobalConstants.ItemAssignedSuccessfullyMessage,
