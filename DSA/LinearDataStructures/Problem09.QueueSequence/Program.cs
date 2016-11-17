4624b86dedb52259a7b39e62dc996d678bd2579f
shakuu <todorov.h@gmail.com>
2016-11-17 09:58:47 +0200

    up

---
+++ DSA/LinearDataStructures/Problem09.QueueSequence/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem09.QueueSequence
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var initialValue = int.Parse(Console.ReadLine());
+
+            var sequence = new Queue<int>();
+            sequence.Enqueue(initialValue);
+
+            // value + 1
+            // value * 2 + 1
+            // value + 2
+
+            var resultingSequence = new List<int>();
+            resultingSequence.Add(initialValue);
+            while (resultingSequence.Count < 50)
+            {
+                var nextValue = sequence.Dequeue();
+
+                var firstOperation = nextValue + 1;
+                sequence.Enqueue(firstOperation);
+
+                var secondOperation = (nextValue * 2) + 1;
+                sequence.Enqueue(secondOperation);
+
+                var thirdOperation = nextValue + 2;
+                sequence.Enqueue(thirdOperation);
+
+                resultingSequence.AddRange(new[]
+                {
+                    firstOperation,
+                    secondOperation,
+                    thirdOperation
+                });
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, resultingSequence.Take(50)));
+        }
+    }
+}
