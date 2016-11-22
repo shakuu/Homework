f28ad0484927d11cbfbdd870faadc7b02c66ad69
shakuu <todorov.h@gmail.com>
2016-11-22 13:19:34 +0200

    permutations

---
+++ DSA/02Recursion/Recursion/04Permutations/Permutations.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04Permutations
+{
+    public class Permutations
+    {
+        static void Main()
+        {
+            var elementsCount = int.Parse(Console.ReadLine());
+
+            var elements = Enumerable.Range(1, elementsCount).ToArray();
+            Permutations.PermutationWrite(elements, 0);
+        }
+
+        private static void PermutationWrite(int[] range, int currentIndex)
+        {
+            if (range.Length <= currentIndex)
+            {
+                // Full permutation found -> print it.
+                Console.WriteLine(string.Join(" ", range));
+            }
+            else
+            {
+                // Find the end of the array.
+                Permutations.PermutationWrite(range, currentIndex + 1);
+                for (int swapWithIndex = currentIndex + 1; swapWithIndex < range.Length; swapWithIndex++)
+                {
+                    // Swap the current element with each of the element following it.
+                    Permutations.SwapElements(range, currentIndex, swapWithIndex);
+
+                    // Find each of the permutation for the current configuration.
+                    Permutations.PermutationWrite(range, currentIndex + 1);
+
+                    // Swap the elements back.
+                    Permutations.SwapElements(range, currentIndex, swapWithIndex);
+                }
+            }
+        }
+
+        private static void SwapElements(int[] range, int indexA, int indexB)
+        {
+            range[indexA] ^= range[indexB];
+            range[indexB] ^= range[indexA];
+            range[indexA] ^= range[indexB];
+        }
+    }
+}
