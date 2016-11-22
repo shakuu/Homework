c0f8eae0531e828e0a64c821a861387a6dd9a3fa
shakuu <todorov.h@gmail.com>
2016-11-22 19:49:42 +0200

    up

---
+++ DSA/02Recursion/Recursion/05Subsets/Subsets.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05Subsets
+{
+    public class Subsets
+    {
+        private static IList<string> elementsToDisplay;
+
+        public static void Main()
+        {
+            var elementsCount = int.Parse(Console.ReadLine());
+            var combinationLength = int.Parse(Console.ReadLine());
+
+            elementsToDisplay = Enumerable.Range(0, elementsCount).Select(x => Console.ReadLine()).ToArray();
+            var range = Enumerable.Range(0, combinationLength).Select(x => 1).ToArray();
+
+            Subsets.GenerateCombination(range, combinationLength, elementsCount);
+        }
+
+        private static void GenerateCombination(int[] range, int current, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (current > 1)
+                {
+                    Subsets.GenerateCombination(range, current - 1, count);
+                }
+                else
+                {
+                    Subsets.PrintElements(range);
+                }
+
+                range[range.Length - current]++;
+            }
+
+            range[range.Length - current] = 1;
+        }
+
+        private static void PrintElements(int[] range)
+        {
+            foreach (var index in range)
+            {
+                Console.Write($"{Subsets.elementsToDisplay[index - 1]} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
