1dd3930c1e3445ddcc6f1f6472e760ee794eb785
shakuu <todorov.h@gmail.com>
2016-11-23 02:53:34 +0200

    up

---
+++ DSA/02Recursion/Recursion/08SinglePath/SinglePath.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _08SinglePath
+{
+    public class SinglePath
+    {
+        private const int MatrixSize = 100;
+
+        private static bool[,] matrix = new bool[SinglePath.MatrixSize, SinglePath.MatrixSize];
+
+        private static bool isPossible;
+
+        private static int goalRowCoordinate = 80;
+        private static int goalColCoordinate = 90;
+
+        public static void Main()
+        {
+            SinglePath.FindPath(0, 0);
+
+            Console.WriteLine(SinglePath.isPossible);
+        }
+
+        private static void FindPath(int row, int col)
+        {
+            if (SinglePath.isPossible)
+            {
+                return;
+            }
+
+            if (row == SinglePath.goalRowCoordinate && col == SinglePath.goalColCoordinate)
+            {
+                SinglePath.isPossible = true;
+                return;
+            }
+
+            if (row + 1 < SinglePath.MatrixSize && !SinglePath.matrix[row + 1, col])
+            {
+
+                SinglePath.matrix[row + 1, col] = true;
+                SinglePath.FindPath(row + 1, col);
+                SinglePath.matrix[row + 1, col] = false;
+            }
+
+            if (col + 1 < SinglePath.MatrixSize && !SinglePath.matrix[row, col + 1])
+            {
+                SinglePath.matrix[row, col + 1] = true;
+                SinglePath.FindPath(row, col + 1);
+                SinglePath.matrix[row, col + 1] = false;
+            }
+
+            if (row - 1 >= 0 && !SinglePath.matrix[row - 1, col])
+            {
+                SinglePath.matrix[row - 1, col] = true;
+                SinglePath.FindPath(row - 1, col);
+                SinglePath.matrix[row - 1, col] = false;
+            }
+
+            if (col - 1 >= 0 && !SinglePath.matrix[row, col - 1])
+            {
+                SinglePath.matrix[row, col - 1] = true;
+                SinglePath.FindPath(row, col - 1);
+                SinglePath.matrix[row, col - 1] = false;
+            }
+        }
+    }
+}
---
+++ DSA/02Recursion/Recursion/09LargestConnectedArea/LargestConnectedArea.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _09LargestConnectedArea
+{
+    public class LargestConnectedArea
+    {
+        private const int MatrixSize = 6;
+
+        private static bool[,] matrix =
+        {
+            { true, false, false, false, true, true },
+            { true, true, true, false, false, false },
+            { false, false, false, true, true, true },
+            { true, true, true, false, false, true },
+            { true, true, true, false, false, true },
+            { true, true, true, true, true, true },
+        };
+
+        private static int maxSteps = 0;
+
+        public static void Main()
+        {
+            LargestConnectedArea.FindPath(0, 0, 0);
+
+            Console.WriteLine(LargestConnectedArea.maxSteps);
+        }
+
+        private static void FindPath(int row, int col, int steps)
+        {
+            if (row + 1 < LargestConnectedArea.MatrixSize && LargestConnectedArea.matrix[row + 1, col])
+            {
+                LargestConnectedArea.matrix[row + 1, col] = false;
+                LargestConnectedArea.FindPath(row + 1, col, steps + 1);
+                LargestConnectedArea.matrix[row + 1, col] = true;
+            }
+
+            if (col + 1 < LargestConnectedArea.MatrixSize && LargestConnectedArea.matrix[row, col + 1])
+            {
+                LargestConnectedArea.matrix[row, col + 1] = false;
+                LargestConnectedArea.FindPath(row, col + 1, steps + 1);
+                LargestConnectedArea.matrix[row, col + 1] = true;
+            }
+
+            if (row - 1 >= 0 && LargestConnectedArea.matrix[row - 1, col])
+            {
+                LargestConnectedArea.matrix[row - 1, col] = false;
+                LargestConnectedArea.FindPath(row - 1, col, steps + 1);
+                LargestConnectedArea.matrix[row - 1, col] = true;
+            }
+
+            if (col - 1 >= 0 && LargestConnectedArea.matrix[row, col - 1])
+            {
+                LargestConnectedArea.matrix[row, col - 1] = false;
+                LargestConnectedArea.FindPath(row, col - 1, steps + 1);
+                LargestConnectedArea.matrix[row, col - 1] = true;
+            }
+
+            if (LargestConnectedArea.maxSteps < steps)
+            {
+                LargestConnectedArea.maxSteps = steps;
+            }
+        }
+    }
+}
