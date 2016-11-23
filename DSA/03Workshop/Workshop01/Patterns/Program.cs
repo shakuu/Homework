6941a099b2113e4a79e718b554570c04c2457a2d
shakuu <todorov.h@gmail.com>
2016-11-23 20:57:58 +0200

    up

---
+++ DSA/03Workshop/Workshop01/Patterns/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+
+namespace Patterns
+{
+    class Program
+    {
+        private static string figure = "urd";
+
+        static void Main(string[] args)
+        {
+            // Right
+            // -- connection
+            // Up
+            // -- connection
+            // Up
+            // -- connection
+            // Left
+            var input = int.Parse(Console.ReadLine());
+            PrintFigure(input - 1);
+        }
+
+        private static void PrintFigure(int iterations)
+        {
+            if (iterations == 0)
+            {
+                Console.WriteLine(figure);
+                Environment.Exit(0);
+            }
+
+            var nextFigure = new StringBuilder();
+
+            // right
+            for (int i = figure.Length - 1; i >= 0; i--)
+            {
+                var letter = figure[i];
+                nextFigure.Append(RotateRight(letter.ToString()));
+            }
+
+            // connection up
+            nextFigure.Append("u");
+
+            // up
+            nextFigure.Append(figure);
+
+            // connection right
+            nextFigure.Append("r");
+
+            // up
+            nextFigure.Append(figure);
+
+            // connection down
+            nextFigure.Append("d");
+
+            // left
+            for (int i = figure.Length - 1; i >= 0; i--)
+            {
+                var letter = figure[i];
+                nextFigure.Append(RotateLeft(letter.ToString()));
+            }
+
+            figure = nextFigure.ToString();
+
+            PrintFigure(iterations - 1);
+        }
+
+        private static string RotateLeft(string letter)
+        {
+            switch (letter)
+            {
+                case "u":
+                    return "r";
+                case "r":
+                    return "d";
+                case "d":
+                    return "l";
+                case "l":
+                    return "u";
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        private static string RotateRight(string letter)
+        {
+            switch (letter)
+            {
+                case "u":
+                    return "l";
+                case "r":
+                    return "u";
+                case "d":
+                    return "r";
+                case "l":
+                    return "d";
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
---
+++ DSA/03Workshop/Workshop01/ReversePolishNotation/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversePolishNotation
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var stack = new Stack<int>();
+            var result = 0;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                var nextElement = input[i];
+
+                int parsed;
+                var isParsed = int.TryParse(nextElement, out parsed);
+                if (isParsed)
+                {
+                    stack.Push(parsed);
+                }
+                else
+                {
+                    var b = stack.Pop();
+                    var a = stack.Pop();
+
+                    // +, -, *, /) and bitwise (&, |, ^)
+                    switch (nextElement)
+                    {
+                        case "+":
+                            stack.Push(a + b);
+                            break;
+                        case "-":
+                            stack.Push(a - b);
+                            break;
+                        case "*":
+                            stack.Push(a * b);
+                            break;
+                        case "/":
+                            stack.Push(a / b);
+                            break;
+                        case "&":
+                            stack.Push(a & b);
+                            break;
+                        case "|":
+                            stack.Push(a | b);
+                            break;
+                        case "^":
+                            stack.Push(a ^ b);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
