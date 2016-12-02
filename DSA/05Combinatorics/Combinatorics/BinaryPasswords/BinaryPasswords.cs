38bac59ea44c62be8faaf39074939cef0a7e9415
shakuu <todorov.h@gmail.com>
2016-12-02 21:53:44 +0200

    combinatorics

---
+++ DSA/05Combinatorics/Combinatorics/BinaryPasswords/BinaryPasswords.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BinaryPasswords
+{
+    public class BinaryPasswords
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            long result = 1;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '*')
+                {
+                    result *= 2;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ DSA/05Combinatorics/Combinatorics/ColorfulBunnies/ColorfulBunnies.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ColorfulBunnies
+{
+    public class ColorfulBunnies
+    {
+        public static void Main()
+        {
+            var answersDictionary = new Dictionary<long, long>();
+
+            var inputCount = long.Parse(Console.ReadLine());
+            for (long i = 0; i < inputCount; i++)
+            {
+                var nextValue = long.Parse(Console.ReadLine());
+                if (answersDictionary.ContainsKey(nextValue))
+                {
+                    answersDictionary[nextValue]++;
+                }
+                else
+                {
+                    answersDictionary.Add(nextValue, 1);
+                }
+            }
+
+            long bunnies = 0;
+            foreach (var pair in answersDictionary)
+            {
+                var key = pair.Key;
+                var value = pair.Value;
+
+                if (value <= key + 1)
+                {
+                    bunnies += key + 1;
+                }
+                else
+                {
+                    var mathcingBunniesCount = Math.Ceiling((double)value / (double)(key + 1));
+                    var bunniesToAdd = (long)(mathcingBunniesCount * (key + 1));
+                    bunnies += bunniesToAdd;
+                }
+            }
+
+            Console.WriteLine(bunnies);
+        }
+    }
+}
