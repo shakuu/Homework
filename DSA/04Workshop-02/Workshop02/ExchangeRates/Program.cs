3e0a07025eac770361c9dc0b8c2ac2229fa5a9ae
shakuu <todorov.h@gmail.com>
2016-12-06 01:59:09 +0200

    up

---
+++ DSA/04Workshop-02/Workshop02/ExchangeRates/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRates
+{
+    public class Program
+    {
+
+        private static List<double> currencyOneRates = new List<double>();
+        private static List<double> currencyTwoRates = new List<double>();
+        private static int daysCount;
+
+        private static double maxAmount;
+        private static double currentAmount;
+
+        public static void Main()
+        {
+            maxAmount = double.Parse(Console.ReadLine());
+            currentAmount = maxAmount;
+
+            daysCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < daysCount; i++)
+            {
+                var rates = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(double.Parse).ToArray();
+
+                currencyOneRates.Add(rates[0]);
+                currencyTwoRates.Add(rates[1]);
+            }
+
+            findMaxAmount(0, true);
+            Console.WriteLine("{0:F2}", maxAmount);
+        }
+
+        public static void findMaxAmount(int index, bool isCurrencyOne)
+        {
+            if (maxAmount < currentAmount && isCurrencyOne)
+            {
+                maxAmount = currentAmount;
+            }
+
+            if (index == daysCount)
+            {
+                return;
+            }
+
+            for (int i = index; i < daysCount; i++)
+            {
+                if (isCurrencyOne && i != daysCount - 1)
+                {
+                    currentAmount *= currencyOneRates[i];
+                    findMaxAmount(i + 1, !isCurrencyOne);
+                    currentAmount /= currencyOneRates[i];
+                }
+                else
+                {
+                    currentAmount *= currencyTwoRates[i];
+                    findMaxAmount(i + 1, !isCurrencyOne);
+                    currentAmount /= currencyTwoRates[i];
+                }
+            }
+        }
+    }
+}
---
+++ DSA/04Workshop-02/Workshop02/ExchangeRates2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRates2
+{
+    class Program
+    {
+        private static List<double> currencyOneRates = new List<double>();
+        private static List<double> currencyTwoRates = new List<double>();
+        private static int daysCount;
+
+        private static double maxAmount;
+        private static double currentAmount;
+
+        public static void Main()
+        {
+            maxAmount = double.Parse(Console.ReadLine());
+            currentAmount = maxAmount;
+
+            daysCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < daysCount; i++)
+            {
+                var rates = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(double.Parse).ToArray();
+
+                currencyOneRates.Add(rates[0]);
+                currencyTwoRates.Add(rates[1]);
+            }
+            
+            Console.WriteLine("{0:F2}", maxAmount);
+        }
+    }
+}
