5c6c114f6db6e2ebca4a5bcbc083dcd8afe87a67
shakuu <todorov.h@gmail.com>
2016-12-07 01:58:27 +0200

    up

--- DSA/06Sorting/project/MergeSorter.cs
+++ DSA/06Sorting/project/MergeSorter.cs
@@ -10,7 +10,59 @@
     {
         public void Sort(IList<T> collection)
         {
-            throw new NotImplementedException();
+            MergeSort_Recursive(collection, 0, collection.Count - 1);
+        }
+
+        private void DoMerge(IList<T> numbers, int left, int mid, int right)
+        {
+            IList<T> temp = new T[numbers.Count];
+            int i, left_end, num_elements, tmp_pos;
+
+            left_end = (mid - 1);
+            tmp_pos = left;
+            num_elements = (right - left + 1);
+
+            while ((left <= left_end) && (mid <= right))
+            {
+                if (numbers[left].CompareTo(numbers[mid]) <= 0)
+                {
+                    temp[tmp_pos++] = numbers[left++];
+                }
+                else
+                {
+                    temp[tmp_pos++] = numbers[mid++];
+                }
+            }
+
+            while (left <= left_end)
+            {
+                temp[tmp_pos++] = numbers[left++];
+            }
+
+            while (mid <= right)
+            {
+                temp[tmp_pos++] = numbers[mid++];
+            }
+
+            for (i = 0; i < num_elements; i++)
+            {
+                numbers[right] = temp[right];
+                right--;
+            }
+        }
+
+        private void MergeSort_Recursive(IList<T> numbers, int left, int right)
+        {
+            int mid;
+
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                MergeSort_Recursive(numbers, left, mid);
+                MergeSort_Recursive(numbers, (mid + 1), right);
+
+                DoMerge(numbers, left, (mid + 1), right);
+            }
         }
     }
 }
--- DSA/06Sorting/project/Program.cs
+++ DSA/06Sorting/project/Program.cs
@@ -20,17 +20,17 @@
             //collection.PrintAllItemsOnConsole();
             //Console.WriteLine();
 
-            collection = new SortableCollection<int>(new[] { 22, 11, 101, 33, 0, 101 });
-            Console.WriteLine("Quicksorter result:");
-            collection.Sort(new Quicksorter<int>());
-            collection.PrintAllItemsOnConsole();
-            Console.WriteLine();
-
             //collection = new SortableCollection<int>(new[] { 22, 11, 101, 33, 0, 101 });
-            //Console.WriteLine("MergeSorter result:");
-            //collection.Sort(new MergeSorter<int>());
+            //Console.WriteLine("Quicksorter result:");
+            //collection.Sort(new Quicksorter<int>());
             //collection.PrintAllItemsOnConsole();
             //Console.WriteLine();
+
+            collection = new SortableCollection<int>(new[] { 22, 11, 101, 33, 0, 101 });
+            Console.WriteLine("MergeSorter result:");
+            collection.Sort(new MergeSorter<int>());
+            collection.PrintAllItemsOnConsole();
+            Console.WriteLine();
 
             //Console.WriteLine("Linear search 101:");
             //Console.WriteLine(collection.LinearSearch(101));
