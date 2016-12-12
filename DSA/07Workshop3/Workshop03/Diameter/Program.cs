e09eee98d3ebf73544723ccf027313e816339c59
shakuu <todorov.h@gmail.com>
2016-12-13 01:52:02 +0200

    up

---
+++ DSA/07Workshop3/Workshop03/Diameter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+using Wintellect.PowerCollections;
+
+namespace Diameter
+{
+    public class Program
+    {
+        private static int[,] table;
+        private static bool[,] visited;
+        private static new OrderedDictionary<int, int>[] nodes;
+
+        private static long currentRoute;
+        private static long longestRoute;
+
+        public static void Main()
+        {
+            var nodesCount = int.Parse(Console.ReadLine());
+
+            nodes = new OrderedDictionary<int, int>[nodesCount];
+            for (var i = 0; i < nodesCount - 1; i++)
+            {
+                var nextConnection = Console.ReadLine()
+                    .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                if (nodes[nextConnection[0]] == null)
+                {
+                    nodes[nextConnection[0]] = new OrderedDictionary<int, int>();
+                }
+
+                if (nodes[nextConnection[1]] == null)
+                {
+                    nodes[nextConnection[1]] = new OrderedDictionary<int, int>();
+                }
+
+                nodes[nextConnection[0]].Add(nextConnection[2], nextConnection[1]);
+                nodes[nextConnection[1]].Add(nextConnection[2], nextConnection[0]);
+            }
+        }
+    }
+}
--- DSA/07Workshop3/Workshop03/OddNumber/Program.cs
+++ DSA/07Workshop3/Workshop03/OddNumber/Program.cs
@@ -16,8 +16,6 @@ namespace OddNumber
             }
 
             Console.WriteLine(number);
-
-
         }
     }
 }
