6afc757e7882e20a3d5a49b1f2374c5b0b71c38e
shakuu <todorov.h@gmail.com>
2016-12-12 09:29:42 +0200

    up

---
+++ DSA/07Workshop3/Workshop03/OddNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OddNumber
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbersCount = int.Parse(Console.ReadLine());
+
+            var number = long.Parse(Console.ReadLine());
+            for (var i = 1; i < numbersCount; i++)
+            {
+                var next = long.Parse(Console.ReadLine());
+                number ^= next;
+            }
+
+            Console.WriteLine(number);
+        }
+    }
+}
