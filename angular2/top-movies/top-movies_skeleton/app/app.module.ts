6bd64cedd0f22d7253886e0a9308d9084d0e2740
shakuu <todorov.h@gmail.com>
2016-12-19 09:18:08 +0200

    up

--- angular2/top-movies/top-movies_skeleton/app/app.module.ts
+++ angular2/top-movies/top-movies_skeleton/app/app.module.ts
@@ -1,9 +1,9 @@
-import { MoviesModule } from './core/movie/movies.module';
-import { NavigationComponent } from './core/navigation.component';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MoviesModule } from './core/movie/movies.module';
+import { NavigationComponent } from './core/navigation.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
