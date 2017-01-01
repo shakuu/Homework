1cc6b6666e8d2d3425c8c18f26b49ce4517e08da
shakuu <todorov.h@gmail.com>
2017-01-01 10:44:39 +0200

    up

--- DSA/04Workshop-02/Workshop02/ExchageRates4/ExchangeRates.cs
+++ DSA/04Workshop-02/Workshop02/ExchageRates4/ExchangeRates.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExchageRates4
 {
-    public class Program
+    public class ExchangeRates
     {
         public static void Main()
         {
@@ -16,11 +13,27 @@ namespace ExchageRates4
             // [0] -> C1 -> C2; [1] C2 -> C1
             var firstLinePair = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
             var maximumCurrencyTwo = initialCurrency * firstLinePair[0];
+            var maximumCurrencyOne = initialCurrency;
 
             for (int i = 1; i < numberOfDays; i++)
             {
                 var nextLinePair = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+                var currentCurrencyTwo = maximumCurrencyOne * nextLinePair[0];
+                var currentCurrencyOne = maximumCurrencyTwo * nextLinePair[1];
+
+                if (maximumCurrencyTwo < currentCurrencyTwo)
+                {
+                    maximumCurrencyTwo = currentCurrencyTwo;
+                }
+
+                if (maximumCurrencyOne < currentCurrencyOne)
+                {
+                    maximumCurrencyOne = currentCurrencyOne;
+                }
             }
+
+            Console.WriteLine("{0:F2}", maximumCurrencyOne);
         }
     }
 }
