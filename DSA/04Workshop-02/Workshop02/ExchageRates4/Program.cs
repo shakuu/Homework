72b40a986de2f0a897a9e84bac3784db4ddcd480
shakuu <todorov.h@gmail.com>
2017-01-01 10:16:16 +0200

    up

---
+++ DSA/04Workshop-02/Workshop02/ExchageRates4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchageRates4
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var initialCurrency = double.Parse(Console.ReadLine());
+            var numberOfDays = int.Parse(Console.ReadLine());
+
+            // [0] -> C1 -> C2; [1] C2 -> C1
+            var firstLinePair = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            var maximumCurrencyTwo = initialCurrency * firstLinePair[0];
+
+            for (int i = 1; i < numberOfDays; i++)
+            {
+                var nextLinePair = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            }
+        }
+    }
+}
