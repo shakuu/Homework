9211a8ba18a57f007b1fc593de1dbeae2ca3f619
shakuu <todorov.h@gmail.com>
2017-01-05 14:19:00 +0200

    up

---
+++ DSA/Exam2016/UnitsOfWork/GameUnit.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitsOfWork
+{
+    public class GameUnit
+    {
+        private int? hash;
+
+        public GameUnit(string name, string type, int attack)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Attack = attack;
+        }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int Attack { get; set; }
+
+        public override int GetHashCode()
+        {
+            if (this.hash.HasValue)
+            {
+                return this.hash.Value;
+            }
+
+            this.hash = 103;
+            unchecked
+            {
+                hash = hash * 599 + this.Name.GetHashCode();
+                hash = hash * 599 + this.Type.GetHashCode();
+                hash = hash * 599 + this.Attack.GetHashCode();
+            }
+
+            return this.hash.Value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as GameUnit;
+            if (other == null)
+            {
+                return false;
+            }
+
+            var nameIsEqual = this.Name == other.Name;
+            var typeIsEqual = this.Type == other.Type;
+            var attackIsEqual = this.Attack == other.Attack;
+
+            return nameIsEqual && typeIsEqual && attackIsEqual;
+        }
+
+        public override string ToString()
+        {
+            var toString = string.Format("{0}[{1}]({2})", this.Name, this.Type, this.Attack);
+            return toString;
+        }
+    }
+}
---
+++ DSA/Exam2016/UnitsOfWork/UnitsOfWork.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitsOfWork
+{
+    public class GameUnitAttackComparer : IComparer<GameUnit>
+    {
+        public int Compare(GameUnit x, GameUnit y)
+        {
+            return y.Attack - x.Attack;
+        }
+    }
+
+    public class IntReverseComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            return y - x;
+        }
+    }
+
+    public class UnitsOfWork
+    {
+        private const string EndCommand = "end";
+        private const string AddCommand = "add";
+        private const string RemoveCommand = "remove";
+        private const string FindCommand = "find";
+        private const string PowerCommand = "power";
+
+        private static Dictionary<string, GameUnit> unitsDictionaryByUniqueName;
+        private static SortedDictionary<int, HashSet<GameUnit>> unitsDictionaryByAttack;
+
+        public static void Main()
+        {
+            unitsDictionaryByUniqueName = new Dictionary<string, GameUnit>();
+            unitsDictionaryByAttack = new SortedDictionary<int, HashSet<GameUnit>>(new IntReverseComparer());
+
+            var engineIsRunning = true;
+            while (engineIsRunning)
+            {
+                var nextCommandWords = Console.ReadLine().Split(' ');
+
+                switch (nextCommandWords[0])
+                {
+                    case UnitsOfWork.EndCommand:
+                        engineIsRunning = false;
+                        break;
+                    case UnitsOfWork.AddCommand:
+                        UnitsOfWork.HandleAddCommand(nextCommandWords);
+                        break;
+                    case UnitsOfWork.RemoveCommand:
+                        UnitsOfWork.HandleRemoveCommand(nextCommandWords);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static void HandleAddCommand(string[] commandWords)
+        {
+            var unitExists = UnitsOfWork.unitsDictionaryByUniqueName.ContainsKey(commandWords[1]);
+            if (unitExists)
+            {
+                // FAIL: Spiderman already exists!
+                Console.WriteLine("FAIL: {0} already exists!", commandWords[1]);
+            }
+            else
+            {
+                var commandWordsAttackValue = int.Parse(commandWords[3]);
+                var newUnit = new GameUnit(commandWords[1], commandWords[2], commandWordsAttackValue);
+
+                UnitsOfWork.unitsDictionaryByUniqueName.Add(newUnit.Name, newUnit);
+
+                if (!UnitsOfWork.unitsDictionaryByAttack.ContainsKey(commandWordsAttackValue))
+                {
+                    UnitsOfWork.unitsDictionaryByAttack.Add(commandWordsAttackValue, new HashSet<GameUnit>());
+                }
+
+                // Save names only ? 
+                unitsDictionaryByAttack[commandWordsAttackValue].Add(newUnit);
+
+                // SUCCESS: TheMightyThor added!
+                Console.WriteLine("SUCCESS: {0} added", newUnit.Name);
+            }
+        }
+
+        private static void HandleRemoveCommand(string[] commandWords)
+        {
+            var unitExists = UnitsOfWork.unitsDictionaryByUniqueName.ContainsKey(commandWords[1]);
+            if (!unitExists)
+            {
+                Console.WriteLine("FAIL: {0} could not be found!", commandWords[1]);
+            }
+            else
+            {
+                var unit = UnitsOfWork.unitsDictionaryByUniqueName[commandWords[1]];
+
+                UnitsOfWork.unitsDictionaryByAttack[unit.Attack].RemoveWhere(x => x.Name == unit.Name);
+                UnitsOfWork.unitsDictionaryByUniqueName.Remove(commandWords[1]);
+
+                Console.WriteLine("SUCCESS: {0} removed!", unit.Name);
+            }
+        }
+    }
+}
