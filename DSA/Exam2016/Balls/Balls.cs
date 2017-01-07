6fb7d19d9b7b3de93d04878fdaf608be95a66437
shakuu <todorov.h@gmail.com>
2017-01-07 18:56:44 +0200

    up

---
+++ DSA/Exam2016/Balls/Balls.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace Balls
+{
+    public class Balls
+    {
+        public static void Main()
+        {
+            var possibleTakeList = Console.ReadLine().Split(' ').Select(int.Parse).OrderBy(x => -x).ToArray();
+            var possibleBallsCount = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            var possibleTakeCount = possibleTakeList.Length;
+
+            var firstPlayerWins = 0;
+            for (int ballCount = possibleBallsCount[0]; ballCount <= possibleBallsCount[1]; ballCount++)
+            {
+                var currentGameBallsCount = ballCount;
+
+                var currentTakeIndex = 0;
+                var isFirstPlayersTurn = true;
+                while (true)
+                {
+                    var maximumPossibleTake = possibleTakeList[currentTakeIndex];
+                    while (maximumPossibleTake > currentGameBallsCount)
+                    {
+                        currentTakeIndex++;
+                        maximumPossibleTake = possibleTakeList[currentTakeIndex];
+                    }
+
+                    if (currentGameBallsCount - maximumPossibleTake == 1 && currentTakeIndex < possibleTakeCount - 1)
+                    {
+                        currentTakeIndex++;
+                        maximumPossibleTake = possibleTakeList[currentTakeIndex];
+                    }
+
+                    currentGameBallsCount -= maximumPossibleTake;
+                    if (currentGameBallsCount == 0)
+                    {
+                        if (isFirstPlayersTurn)
+                        {
+                            firstPlayerWins++;
+                        }
+
+                        break;
+                    }
+
+                    isFirstPlayersTurn = !isFirstPlayersTurn;
+                }
+            }
+
+            Console.WriteLine(firstPlayerWins);
+        }
+    }
+}
