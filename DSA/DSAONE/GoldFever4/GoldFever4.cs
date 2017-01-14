96fda87fd028a3a1334a868040bfc086b9d5001d
shakuu <todorov.h@gmail.com>
2017-01-14 23:45:18 +0200

    up

---
+++ DSA/DSAONE/GoldFever4/GoldFever4.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldFever4
+{
+    public class ResultPair
+    {
+        public ResultPair(long profit, int ounces)
+        {
+            this.Profit = profit;
+            this.Ounces = ounces;
+        }
+
+        public long Profit { get; set; }
+
+        public int Ounces { get; set; }
+
+        public override int GetHashCode()
+        {
+            var hash = 199;
+
+            unchecked
+            {
+                hash = hash * 257 + this.Profit.GetHashCode();
+                hash = hash * 257 + this.Ounces.GetHashCode();
+            }
+
+            return hash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var otherResultPair = obj as ResultPair;
+            if (otherResultPair == null)
+            {
+                return false;
+            }
+
+            var profitIsEqual = this.Profit == otherResultPair.Profit;
+            var ouncesAreEqual = this.Ounces == otherResultPair.Ounces;
+
+            return profitIsEqual && ouncesAreEqual;
+        }
+    }
+
+    public class GoldFever4
+    {
+        public static void Main()
+        {
+            var daysCount = int.Parse(Console.ReadLine());
+            var quotes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            // item1 = profit, item2 = ounces
+            long bestProfit = 0;
+
+            var results = new Dictionary<long, long>();
+            results.Add(0, 0);
+            results.Add(1, -quotes[0]);
+
+            for (int quoteIndex = 1; quoteIndex < quotes.Length; quoteIndex++)
+            {
+                var nextQuote = quotes[quoteIndex];
+
+                var nextResultsSet = new Dictionary<long, long>();
+                foreach (var result in results)
+                {
+                    var buyKey = result.Key + 1;
+                    var containsBuyKey = nextResultsSet.ContainsKey(buyKey);
+                    if (!containsBuyKey)
+                    {
+                        nextResultsSet.Add(buyKey, result.Value - nextQuote);
+                    }
+                    else
+                    {
+                        var potentialValue = result.Value - nextQuote;
+                        nextResultsSet[buyKey] = nextResultsSet[buyKey] >= potentialValue ? nextResultsSet[buyKey] : potentialValue;
+                    }
+
+                    var holdKey = result.Key;
+                    var containsHoldKey = nextResultsSet.ContainsKey(holdKey);
+                    if (!containsHoldKey)
+                    {
+                        nextResultsSet.Add(holdKey, result.Value);
+                    }
+                    else
+                    {
+                        var potentialValue = result.Value;
+                        nextResultsSet[holdKey] = nextResultsSet[holdKey] >= potentialValue ? nextResultsSet[holdKey] : potentialValue;
+                    }
+
+                    var sellKey = 0;
+                    var sellValue = result.Key * nextQuote + result.Value;
+                    var containsSellKey = nextResultsSet.ContainsKey(sellKey);
+                    if (!containsSellKey)
+                    {
+                        nextResultsSet.Add(sellKey, sellValue);
+                    }
+                    else
+                    {
+                        nextResultsSet[sellKey] = nextResultsSet[sellKey] >= sellValue ? nextResultsSet[sellKey] : sellValue;
+                    }
+                }
+
+                results = nextResultsSet;
+            }
+
+            foreach (var result in results)
+            {
+                if (bestProfit < result.Value)
+                {
+                    bestProfit = result.Value;
+                }
+            }
+            
+            Console.WriteLine(bestProfit);
+        }
+    }
+}
---
+++ DSA/DSAONE/GoldFever5/GoldFever5.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldFever5
+{
+    public class GoldFever5
+    {
+        public static void Main()
+        {
+            var daysCount = int.Parse(Console.ReadLine());
+            var quotes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            // item1 = profit, item2 = ounces
+            long bestProfit = 0;
+
+            var results = Enumerable.Range(0, daysCount + 1).Select(x => long.MinValue).ToList();
+            results[0] = 0;
+            results[1] = -quotes[0];
+
+            var keysToCheck = new HashSet<int>() { 0, 1 };
+
+            for (int quoteIndex = 1; quoteIndex < quotes.Length; quoteIndex++)
+            {
+                var nextQuote = quotes[quoteIndex];
+
+                var nextKeysToCheck = new HashSet<int>();
+                var newResults = Enumerable.Range(0, quoteIndex + 2).Select(x => long.MinValue).ToList();
+                //foreach (var key in keysToCheck)
+                for (int key = 0; key <= quoteIndex; key++)
+                {
+                    var ounces = key;
+                    var profit = results[key];
+
+                    var buyKey = ounces + 1;
+                    var potentialBuyValue = profit - nextQuote;
+
+                    var holdKey = ounces;
+                    var potentialHoldValue = profit;
+
+                    var sellKey = 0;
+                    var sellValue = ounces * nextQuote + profit;
+
+                    newResults[buyKey] = results[buyKey] >= potentialBuyValue ? results[buyKey] : potentialBuyValue;
+                    newResults[holdKey] = results[holdKey] >= potentialHoldValue ? results[holdKey] : potentialHoldValue;
+                    newResults[sellKey] = results[sellKey] >= sellValue ? results[sellKey] : sellValue;
+                }
+
+                results = newResults;
+                results.Add(long.MinValue);
+            }
+
+            foreach (var key in keysToCheck)
+            {
+                if (bestProfit < results[key])
+                {
+                    bestProfit = results[key];
+                }
+            }
+
+            Console.WriteLine(bestProfit);
+        }
+    }
+}
