468c9ed93bbaf80d660ac5789407ef8b8b55c2ae
shakuu <todorov.h@gmail.com>
2017-01-15 19:30:20 +0200

    up

--- DSA/DSAONE/GoldFever3/GoldFever3.cs
+++ DSA/DSAONE/GoldFever3/GoldFever3.cs
@@ -6,148 +6,79 @@ using System.Threading.Tasks;
 
 namespace GoldFever3
 {
-    public class ResultPair
-    {
-        public ResultPair(long profit, int ounces)
-        {
-            this.Profit = profit;
-            this.Ounces = ounces;
-        }
-
-        public long Profit { get; set; }
-
-        public int Ounces { get; set; }
-
-        public override int GetHashCode()
-        {
-            var hash = 199;
-
-            unchecked
-            {
-                hash = hash * 257 + this.Profit.GetHashCode();
-                hash = hash * 257 + this.Ounces.GetHashCode();
-            }
-
-            return hash;
-        }
-
-        public override bool Equals(object obj)
-        {
-            var otherResultPair = obj as ResultPair;
-            if (otherResultPair == null)
-            {
-                return false;
-            }
-
-            var profitIsEqual = this.Profit == otherResultPair.Profit;
-            var ouncesAreEqual = this.Ounces == otherResultPair.Ounces;
-
-            return profitIsEqual && ouncesAreEqual;
-        }
-    }
-
     public class GoldFever3
     {
         public static void Main()
         {
             var daysCount = int.Parse(Console.ReadLine());
-            var quotes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var quotes = Console.ReadLine().Split(' ').Select(int.Parse);
 
             // item1 = profit, item2 = ounces
-            long bestProfit = 0;
+            var results = new LinkedList<KeyValuePair<long, int>>();
+            results.AddLast(new KeyValuePair<long, int>(0, 0));
+            results.AddLast(new KeyValuePair<long, int>(-quotes.First(), 1));
 
-            var results = new HashSet<ResultPair>();
-            results.Add(new ResultPair(0, 0));
-            results.Add(new ResultPair(-quotes[0], 1));
+            quotes = quotes.Skip(1);
 
-            for (int quoteIndex = 1; quoteIndex < quotes.Length; quoteIndex++)
+            foreach (var nextQuote in quotes)
             {
-                var nextQuote = quotes[quoteIndex];
+                //var nextQuote = quotes[quoteIndex];
 
-                var currentBestOunces = new ResultPair(long.MinValue, -1);
-                var currentBestProfit = new ResultPair(long.MinValue, 1);
-                var currentBestCoefficient = new ResultPair(long.MinValue, 1);
+                var currentBestOunces = new KeyValuePair<long, int>(long.MinValue, -1);
+                var currentBestProfit = new KeyValuePair<long, int>(long.MinValue, 1);
 
-                var nextResultsSet = new HashSet<ResultPair>();
                 foreach (var result in results)
                 {
-                    var currentPairs = new LinkedList<ResultPair>();
-                    var buyPair = new ResultPair(result.Profit - nextQuote, result.Ounces + 1);
-                    var sellPair = new ResultPair(result.Ounces * nextQuote + result.Profit, 0);
+                    var buyPair = new KeyValuePair<long, int>(result.Key - nextQuote, result.Value + 1);
+                    var sellPair = new KeyValuePair<long, int>(result.Value * nextQuote + result.Key, 0);
 
-                    currentPairs.AddLast(buyPair);
-                    currentPairs.AddLast(sellPair);
-                    currentPairs.AddLast(result);
+                    var currentPairs = new KeyValuePair<long, int>[3]
+                    {
+                        buyPair,
+                        sellPair,
+                        result
+                    };
+
+                    //currentPairs.AddLast(buyPair);
+                    //currentPairs.AddLast(sellPair);
+                    //currentPairs.AddLast(result);
 
                     foreach (var pair in currentPairs)
                     {
-                        if (currentBestOunces.Ounces < pair.Ounces)
+                        if (currentBestOunces.Value < pair.Value)
+                        {
+                            currentBestOunces = pair;
+                        }
+                        else if (currentBestOunces.Value == pair.Value && currentBestOunces.Key < pair.Key)
                         {
                             currentBestOunces = pair;
                         }
 
-                        //var bestProfitCoefficient = (double)(currentBestProfit.Profit) / (double)currentBestProfit.Ounces;
-                        //var pairCoefficient = (double)(pair.Profit) / (double)pair.Ounces;
-                        //if (bestProfitCoefficient < pairCoefficient)
-                        //{
-                        //    currentBestCoefficient = pair;
-                        //}
-
-                        if (currentBestProfit.Profit < pair.Profit)
+                        if (currentBestProfit.Key < pair.Key)
+                        {
+                            currentBestProfit = pair;
+                        }
+                        else if (currentBestProfit.Key == pair.Key && currentBestProfit.Value < pair.Value)
                         {
                             currentBestProfit = pair;
                         }
                     }
-                    //nextResultsSet.Add(new ResultPair(result.Profit - nextQuote, result.Ounces + 1));
-                    //nextResultsSet.Add(result);
-                    //nextResultsSet.Add(new ResultPair(result.Ounces * nextQuote + result.Profit, 0));
                 }
 
                 // best ounces + best profit only
-                results = new HashSet<ResultPair>() { currentBestOunces, currentBestProfit, currentBestCoefficient };
+                results = new LinkedList<KeyValuePair<long, int>>();
+                results.AddLast(currentBestOunces);
+                results.AddLast(currentBestProfit);
             }
 
+            long bestProfit = 0;
             foreach (var result in results)
             {
-                if (bestProfit < result.Profit)
+                if (bestProfit < result.Key)
                 {
-                    bestProfit = result.Profit;
+                    bestProfit = result.Key;
                 }
             }
-
-            //var tupleQueue = new Queue<Tuple<long, long>>();
-            //tupleQueue.Enqueue(new Tuple<long, long>(0, 0));
-            //tupleQueue.Enqueue(new Tuple<long, long>(-quotes[0], 1));
-
-            //for (int quoteIndex = 1; quoteIndex < quotes.Length; quoteIndex++)
-            //{
-            //    var nextQuote = quotes[quoteIndex];
-
-            //    var resultingQueue = new Queue<Tuple<long, long>>();
-            //    while (tupleQueue.Count > 0)
-            //    {
-            //        var nextTuple = tupleQueue.Dequeue();
-            //        if (bestProfit < nextTuple.Item1)
-            //        {
-            //            bestProfit = nextTuple.Item1;
-            //        }
-
-            //        resultingQueue.Enqueue(new Tuple<long, long>(nextTuple.Item1 - nextQuote, nextTuple.Item2 + 1));
-            //        resultingQueue.Enqueue(nextTuple);
-            //        resultingQueue.Enqueue(new Tuple<long, long>(nextTuple.Item2 * nextQuote + nextTuple.Item1, 0));
-            //    }
-
-            //    tupleQueue = resultingQueue;
-            //}
-
-            //while (tupleQueue.Count > 0)
-            //{
-            //    var nextTuple = tupleQueue.Dequeue();
-            //    if (bestProfit < nextTuple.Item1)
-            //    {
-            //        bestProfit = nextTuple.Item1;
-            //    }
-            //}
 
             Console.WriteLine(bestProfit);
         }
---
+++ DSA/DSAONE/GoldFever6/GoldFever6.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldFever6
+{
+    public class ResultPair
+    {
+        public ResultPair(long profit, int ounces)
+        {
+            this.Profit = profit;
+            this.Ounces = ounces;
+        }
+
+        public long Profit { get; set; }
+
+        public int Ounces { get; set; }
+
+        public override int GetHashCode()
+        {
+            var hash = 199;
+
+            unchecked
+            {
+                hash = hash * 257 + this.Profit.GetHashCode();
+                hash = hash * 257 + this.Ounces.GetHashCode();
+            }
+
+            return hash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var otherResultPair = obj as ResultPair;
+            if (otherResultPair == null)
+            {
+                return false;
+            }
+
+            var profitIsEqual = this.Profit == otherResultPair.Profit;
+            var ouncesAreEqual = this.Ounces == otherResultPair.Ounces;
+
+            return profitIsEqual && ouncesAreEqual;
+        }
+    }
+
+    public class GoldFever6
+    {
+        public static void Main()
+        {
+            var daysCount = int.Parse(Console.ReadLine());
+            var quotes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            // item1 = profit, item2 = ounces
+            long bestProfit = 0;
+
+            var results = new HashSet<ResultPair>();
+            results.Add(new ResultPair(0, 0));
+            results.Add(new ResultPair(-quotes[0], 1));
+
+            for (int quoteIndex = 1; quoteIndex < quotes.Length; quoteIndex++)
+            {
+                var nextQuote = quotes[quoteIndex];
+
+                var nextResultsSet = new HashSet<ResultPair>();
+                foreach (var result in results)
+                {
+                    nextResultsSet.Add(new ResultPair(result.Profit - nextQuote, result.Ounces + 1));
+                    nextResultsSet.Add(result);
+                    nextResultsSet.Add(new ResultPair(result.Ounces * nextQuote + result.Profit, 0));
+                }
+
+                results = nextResultsSet;
+            }
+
+            foreach (var result in results)
+            {
+                if (bestProfit < result.Profit)
+                {
+                    bestProfit = result.Profit;
+                }
+            }
+
+            //var tupleQueue = new Queue<Tuple<long, long>>();
+            //tupleQueue.Enqueue(new Tuple<long, long>(0, 0));
+            //tupleQueue.Enqueue(new Tuple<long, long>(-quotes[0], 1));
+
+            //for (int quoteIndex = 1; quoteIndex < quotes.Length; quoteIndex++)
+            //{
+            //    var nextQuote = quotes[quoteIndex];
+
+            //    var resultingQueue = new Queue<Tuple<long, long>>();
+            //    while (tupleQueue.Count > 0)
+            //    {
+            //        var nextTuple = tupleQueue.Dequeue();
+            //        if (bestProfit < nextTuple.Item1)
+            //        {
+            //            bestProfit = nextTuple.Item1;
+            //        }
+
+            //        resultingQueue.Enqueue(new Tuple<long, long>(nextTuple.Item1 - nextQuote, nextTuple.Item2 + 1));
+            //        resultingQueue.Enqueue(nextTuple);
+            //        resultingQueue.Enqueue(new Tuple<long, long>(nextTuple.Item2 * nextQuote + nextTuple.Item1, 0));
+            //    }
+
+            //    tupleQueue = resultingQueue;
+            //}
+
+            //while (tupleQueue.Count > 0)
+            //{
+            //    var nextTuple = tupleQueue.Dequeue();
+            //    if (bestProfit < nextTuple.Item1)
+            //    {
+            //        bestProfit = nextTuple.Item1;
+            //    }
+            //}
+
+            Console.WriteLine(bestProfit);
+        }
+    }
+}
