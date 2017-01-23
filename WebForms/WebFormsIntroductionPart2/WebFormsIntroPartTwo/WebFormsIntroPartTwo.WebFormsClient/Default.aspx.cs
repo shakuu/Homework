1a81921ef4c66e43ecaa129163fd9ff12d11df34
shakuu <todorov.h@gmail.com>
2017-01-23 16:51:57 +0200

    up

--- WebForms/WebFormsIntroductionPart2/WebFormsIntroPartTwo/WebFormsIntroPartTwo.WebFormsClient/Default.aspx.cs
+++ WebForms/WebFormsIntroductionPart2/WebFormsIntroPartTwo/WebFormsIntroPartTwo.WebFormsClient/Default.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -7,76 +8,84 @@ namespace WebFormsIntroPartTwo.WebFormsClient
 {
     public partial class _Default : Page
     {
+        private LinkedList<ListItem> labels = new LinkedList<ListItem>();
+
         protected override void OnPreInit(EventArgs e)
         {
-            var label = new Label();
+            var label = new ListItem();
             label.Text = "1. OnPreInit";
 
-            this.PrintPanel.Controls.Add(label);
+            this.labels.AddLast(label);
         }
 
         protected override void OnInit(EventArgs e)
         {
-            var label = new Label();
+            var label = new ListItem();
             label.Text = "2. OnInit";
 
-            this.PrintPanel.Controls.Add(label);
+            this.labels.AddLast(label);
         }
 
         protected override void OnInitComplete(EventArgs e)
         {
-            var label = new Label();
+            var label = new ListItem();
             label.Text = "3. OnInitComplete";
 
-            this.PrintPanel.Controls.Add(label);
+            this.labels.AddLast(label);
         }
 
         protected override void OnPreLoad(EventArgs e)
         {
-            var label = new Label();
+            var label = new ListItem();
             label.Text = "4. OnPreLoad";
 
-            this.PrintPanel.Controls.Add(label);
+            this.labels.AddLast(label);
         }
 
         protected override void OnLoad(EventArgs e)
         {
-            var label = new Label();
+            var label = new ListItem();
             label.Text = "5. OnLoad";
 
-            this.PrintPanel.Controls.Add(label);
+            this.labels.AddLast(label);
         }
 
         protected override void OnLoadComplete(EventArgs e)
         {
-            var label = new Label();
+            var label = new ListItem();
             label.Text = "6. OnLoadComplete";
 
-            this.PrintPanel.Controls.Add(label);
+            this.labels.AddLast(label);
         }
 
         protected override void OnPreRender(EventArgs e)
         {
-            var label = new Label();
+            var label = new ListItem();
             label.Text = "7. OnPreRender";
 
-            this.PrintPanel.Controls.Add(label);
+            this.labels.AddLast(label);
         }
 
         protected override void OnPreRenderComplete(EventArgs e)
         {
-            var label = new Label();
+            var label = new ListItem();
             label.Text = "8. OnPreRenderComplete";
 
-            this.PrintPanel.Controls.Add(label);
+            this.labels.AddLast(label);
         }
 
         protected override void OnSaveStateComplete(EventArgs e)
         {
-            var label = new Label();
+            var label = new ListItem();
             label.Text = "9. OnSaveStateComplete";
 
-            this.PrintPanel.Controls.Add(label);
+            this.labels.AddLast(label);
+
+            this.ExecutingAssembly.Text = $"Executing assembly location: {Assembly.GetExecutingAssembly().Location}";
+            foreach (var lbl in this.labels)
+            {
+                this.PrintList.Items.Add(lbl);
+            }
         }
 
         protected override void OnUnload(EventArgs e)
@@ -87,7 +96,7 @@ namespace WebFormsIntroPartTwo.WebFormsClient
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            this.ExecutingAssembly.Text = $"Executing assembly location: {Assembly.GetExecutingAssembly().Location}";
+            
         }
 
         protected void ButtonPrintName_Click(object sender, EventArgs e)
