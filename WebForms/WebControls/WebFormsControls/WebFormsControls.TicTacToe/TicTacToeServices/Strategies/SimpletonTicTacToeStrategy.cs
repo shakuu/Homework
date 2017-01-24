695eca5402d6ff43c8acbd5aa9822e561ea9a61c
shakuu <todorov.h@gmail.com>
2017-01-25 00:55:09 +0200

    up

---
+++ WebForms/WebControls/WebFormsControls/WebFormsControls.TicTacToe/TicTacToeServices/Strategies/SimpletonTicTacToeStrategy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using WebFormsControls.TicTacToe.TicTacToeServices.Contracts;
+
+namespace WebFormsControls.TicTacToe.TicTacToeServices.Strategies
+{
+    public class SimpletonTicTacToeStrategy : ITicTacToeStrategy
+    {
+        private static Random Random = new Random();
+
+        public IList<IList<string>> GetNextMove(IList<IList<string>> currentGameBoard)
+        {
+            while (true)
+            {
+                var row = SimpletonTicTacToeStrategy.Random.Next(0, 4);
+                var col = SimpletonTicTacToeStrategy.Random.Next(0, 4);
+
+                if (currentGameBoard[row][col] != "O")
+                {
+                    currentGameBoard[row][col] = "X";
+                    break;
+                }
+            }
+
+            return currentGameBoard;
+        }
+    }
+}
--- WebForms/WebControls/WebFormsControls/WebFormsControls.WebFormsClient/App_Start/NinjectModules/ServicesNinjectModule.cs
+++ WebForms/WebControls/WebFormsControls/WebFormsControls.WebFormsClient/App_Start/NinjectModules/ServicesNinjectModule.cs
@@ -3,6 +3,9 @@
 using WebFormsControls.Calculator.CalculatorServices.Contracts;
 using WebFormsControls.Calculator.CalculatorServices;
 
+using WebFormsControls.TicTacToe.TicTacToeServices.Contracts;
+using WebFormsControls.TicTacToe.TicTacToeServices.Strategies;
+
 namespace WebFormsControls.WebFormsClient.App_Start.NinjectModules
 {
     public class ServicesNinjectModule : NinjectModule
@@ -10,6 +13,8 @@ namespace WebFormsControls.WebFormsClient.App_Start.NinjectModules
         public override void Load()
         {
             this.Bind<ICalculatorService>().To<CalculatorService>();
+
+            this.Bind<ITicTacToeStrategy>().To<SimpletonTicTacToeStrategy>().InSingletonScope();
         }
     }
 }
