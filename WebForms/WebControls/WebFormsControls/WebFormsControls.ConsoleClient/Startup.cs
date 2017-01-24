37d9d3c36c8231c3c8fde5a1e4b1f70b1a01a7c2
shakuu <todorov.h@gmail.com>
2017-01-24 10:04:14 +0200

    up

---
+++ WebForms/WebControls/WebFormsControls/WebFormsControls.ConsoleClient/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using WebFormsControls.Calculator.CalculatorServices;
+
+namespace WebFormsControls.ConsoleClient
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var calculator = new CalculatorService();
+
+            while (true)
+            {
+                var nextCommand = Console.ReadLine();
+
+                var output = calculator.HandleInput(nextCommand);
+
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
