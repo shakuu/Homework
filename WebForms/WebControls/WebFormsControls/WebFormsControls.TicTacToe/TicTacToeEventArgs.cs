5087e48657e44c38289541f12c69db9770b7d6d4
shakuu <todorov.h@gmail.com>
2017-01-25 18:32:57 +0200

    up

--- WebForms/WebControls/WebFormsControls/WebFormsControls.TicTacToe/TicTacToeEventArgs.cs
+++ WebForms/WebControls/WebFormsControls/WebFormsControls.TicTacToe/TicTacToeEventArgs.cs
@@ -5,6 +5,14 @@ namespace WebFormsControls.TicTacToe
 {
     public class TicTacToeEventArgs : EventArgs
     {
+        public TicTacToeEventArgs(string row, string col, string content, IList<IList<string>> currentGameBoard)
+        {
+            this.PlayerTurnRow = row;
+            this.PlayerTurnCol = col;
+            this.PlayerInputContent = content;
+            this.CurrentGameBoard = currentGameBoard;
+        }
+
         public string PlayerTurnRow { get; set; }
 
         public string PlayerTurnCol { get; set; }
--- WebForms/WebControls/WebFormsControls/WebFormsControls.WebFormsClient/UserControls/TicTacToe/TicTacToeUserControl.ascx.cs
+++ WebForms/WebControls/WebFormsControls/WebFormsControls.WebFormsClient/UserControls/TicTacToe/TicTacToeUserControl.ascx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Web.UI.WebControls;
 using WebFormsControls.TicTacToe;
 
 using WebFormsMvp;
@@ -10,16 +11,48 @@ namespace WebFormsControls.WebFormsClient.UserControls.TicTacToe
     [PresenterBinding(typeof(ITicTacToePresenter))]
     public partial class TicTacToeUserControl : MvpUserControl<TicTacToeViewModel>, ITicTacToeView
     {
+        private const string SessionStateFieldName = "TicTacToe";
+
         public event EventHandler<TicTacToeEventArgs> PlayerTurn;
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            this.Model.GameBoard = this.RestoreGameBoardFromSession();
             this.Model.Message = "Welcome!";
         }
 
         protected void OnUserInput(object sender, EventArgs e)
         {
+            var senderButton = sender as Button;
+            if (sender == null)
+            {
+                return;
+            }
 
+            this.Model.GameBoard = this.RestoreGameBoardFromSession();
+
+            var senderId = senderButton.ID;
+            var userInputRow = senderId[1].ToString();
+            var userInputCol = senderId[2].ToString();
+            var userInputContent = senderButton.Text;
+
+            var args = new TicTacToeEventArgs(userInputRow, userInputCol, userInputContent, this.Model.GameBoard);
+            this.PlayerTurn(this, args);
+
+            Session[TicTacToeUserControl.SessionStateFieldName] = this.Model.GameBoard;
+        }
+
+        private IList<IList<string>> RestoreGameBoardFromSession()
+        {
+            var gameBoardFromSession = Session[TicTacToeUserControl.SessionStateFieldName];
+            if (gameBoardFromSession != null)
+            {
+                return (IList<IList<string>>)gameBoardFromSession;
+            }
+            else
+            {
+                return this.Model.GameBoard;
+            }
         }
     }
 }
