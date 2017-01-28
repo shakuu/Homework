d0c8456809956e82a0c6bd4cfbd0e61d4dc30336
shakuu <todorov.h@gmail.com>
2017-01-28 14:47:46 +0200

    up

--- WebForms/DataBinding/WebFormsDataBinding/WebFormsDataBinding/WebFormsDataBinding.WebClient/App_Start/NinjectModules/ActualCarsNinjectModule.cs
+++ WebForms/DataBinding/WebFormsDataBinding/WebFormsDataBinding/WebFormsDataBinding.WebClient/App_Start/NinjectModules/ActualCarsNinjectModule.cs
@@ -4,6 +4,7 @@ using Ninject.Modules;
 
 using WebFormsDataBinding.ActualCars.Models.Factories;
 using WebFormsDataBinding.ActualCars.Services;
+using WebFormsDataBinding.ActualCars.Services.Contracts;
 
 namespace WebFormsDataBinding.WebClient.App_Start.NinjectModules
 {
@@ -13,7 +14,7 @@ namespace WebFormsDataBinding.WebClient.App_Start.NinjectModules
         {
             this.Kernel.Bind(x => x.FromAssemblyContaining<ICreateCarFormModelsFactory>().SelectAllClasses().BindDefaultInterface());
 
-            this.Rebind<CarsInformationService>().ToSelf().InSingletonScope();
+            this.Rebind<ICarsInformationService>().To<CarsInformationService>().InSingletonScope();
 
             this.Bind<ICreateCarFormModelsFactory>().ToFactory().InSingletonScope();
         }
