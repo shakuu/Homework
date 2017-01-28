fe17c53ff323aef30c4fa076f19497bd4cccd6a9
shakuu <todorov.h@gmail.com>
2017-01-28 13:53:41 +0200

    up

--- WebForms/DataBinding/WebFormsDataBinding/WebFormsDataBinding/WebFormsDataBinding.ActualCars/Presenters/CreateCarFormPresenter.cs
+++ WebForms/DataBinding/WebFormsDataBinding/WebFormsDataBinding/WebFormsDataBinding.ActualCars/Presenters/CreateCarFormPresenter.cs
@@ -1,4 +1,7 @@
-﻿using WebFormsDataBinding.ActualCars.Presenters.Contracts;
+﻿using System;
+
+using WebFormsDataBinding.ActualCars.Presenters.Contracts;
+using WebFormsDataBinding.ActualCars.Services.Contracts;
 using WebFormsDataBinding.ActualCars.Views;
 
 using WebFormsMvp;
@@ -8,15 +11,25 @@ namespace WebFormsDataBinding.ActualCars.Presenters
     public class CreateCarFormPresenter : Presenter<ICreateCarFormView>, ICreateCarFormPresenter
     {
         private readonly ICreateCarFormView view;
+        private readonly ICarsInformationService carsInformationService;
 
-        public CreateCarFormPresenter(ICreateCarFormView view)
+        public CreateCarFormPresenter(ICreateCarFormView view, ICarsInformationService carsInformationService)
             : base(view)
         {
             this.view = view;
 
             //this.view.MakeSelectionChanged;
             //this.view.CreateCarFormSubmit;
-            //this.view.InitialState;
+            this.view.InitialState += this.OnInitialState;
+
+            this.carsInformationService = carsInformationService;
+        }
+
+        private void OnInitialState(object sender, EventArgs args)
+        {
+            this.view.Model.AvailableMakes = this.carsInformationService.FindAvailableMakes();
+            this.view.Model.AvailableModels = this.carsInformationService.FindAvaialbleModels();
+            this.view.Model.AvailableOptions = this.carsInformationService.FindAvailableOptions();
         }
     }
 }
