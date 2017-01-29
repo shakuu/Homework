763588554a0f64910d407389c796aea5afc207fb
shakuu <todorov.h@gmail.com>
2017-01-29 12:43:45 +0200

    up

---
+++ WebForms/DataBinding/WebFormsDataBinding/WebFormsDataBinding/WebFormsDataBinding.WebClient/Trees.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page
+    MasterPageFile="~/Site.Master"
+    Title="Trees"
+    Language="C#"
+    AutoEventWireup="true"
+    CodeBehind="Trees.aspx.cs"
+    Inherits="WebFormsDataBinding.WebClient.Trees" %>
+
+<asp:Content ContentPlaceHolderID="MainContent" runat="server">
+    <asp:TreeView ID="MyTree" runat="server">
+        
+    </asp:TreeView>
+</asp:Content>
---
+++ WebForms/DataBinding/WebFormsDataBinding/WebFormsDataBinding/WebFormsDataBinding.WebClient/Trees.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormsDataBinding.WebClient
+{
+    public partial class Trees : System.Web.UI.Page
+    {
+        private string myXml =
+            @"<asp:Content ContentPlaceHolderID=""MainContent"" runat=""server"">
+                <asp:TreeView ID = ""MyTree"" runat=""server"">
+
+                </asp:TreeView>
+              </asp:Content>";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
