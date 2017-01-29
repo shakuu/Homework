a6d5182370d1c10cb6232e3f2191ef94db5e2254
shakuu <todorov.h@gmail.com>
2017-01-29 13:04:29 +0200

    up

--- WebForms/DataBinding/WebFormsDataBinding/WebFormsDataBinding/WebFormsDataBinding.WebClient/Trees.aspx
+++ WebForms/DataBinding/WebFormsDataBinding/WebFormsDataBinding/WebFormsDataBinding.WebClient/Trees.aspx
@@ -7,7 +7,7 @@
     Inherits="WebFormsDataBinding.WebClient.Trees" %>
 
 <asp:Content ContentPlaceHolderID="MainContent" runat="server">
-    <asp:TreeView ID="MyTree" runat="server">
-        
+    <asp:TreeView ID="MyTree" runat="server" DataSourceID="BookstoreXmlDataSource">
     </asp:TreeView>
+    <asp:XmlDataSource ID="BookstoreXmlDataSource" runat="server" DataFile="~/Bookstore.xml"></asp:XmlDataSource>
 </asp:Content>
--- WebForms/DataBinding/WebFormsDataBinding/WebFormsDataBinding/WebFormsDataBinding.WebClient/Trees.aspx.designer.cs
+++ WebForms/DataBinding/WebFormsDataBinding/WebFormsDataBinding/WebFormsDataBinding.WebClient/Trees.aspx.designer.cs
@@ -20,5 +20,14 @@ namespace WebFormsDataBinding.WebClient {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TreeView MyTree;
+        
+        /// <summary>
+        /// BookstoreXmlDataSource control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.XmlDataSource BookstoreXmlDataSource;
     }
 }
