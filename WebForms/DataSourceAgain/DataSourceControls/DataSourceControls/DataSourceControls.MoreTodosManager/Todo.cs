9e34299f4e9e06b50de516f680181c27f025e8e4
shakuu <todorov.h@gmail.com>
2017-02-04 16:49:37 +0200

    up

--- WebForms/DataSourceAgain/DataSourceControls/DataSourceControls/DataSourceControls.MoreTodosManager/Todo.cs
+++ WebForms/DataSourceAgain/DataSourceControls/DataSourceControls/DataSourceControls.MoreTodosManager/Todo.cs
@@ -11,18 +11,13 @@ namespace DataSourceControls.MoreTodosManager
 {
     using System;
     using System.Collections.Generic;
-
+    
     public partial class Todo
     {
-        public Todo()
-        {
-            this.Id = Guid.NewGuid();
-        }
-
         public System.Guid Id { get; set; }
         public string Body { get; set; }
         public int CategoryType { get; set; }
         public string Title { get; set; }
-        public Nullable<System.DateTime> LastChange { get; set; }
+        public System.DateTime LastChange { get; set; }
     }
 }
---
+++ WebForms/DataSourceAgain/DataSourceControls/DataSourceControls/DataSourceControls.MoreTodosManager/TodoCtor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSourceControls.MoreTodosManager
+{
+    public partial class Todo
+    {
+        public Todo()
+        {
+            this.Id = Guid.NewGuid();
+            this.LastChange = DateTime.UtcNow;
+        }
+    }
+}
