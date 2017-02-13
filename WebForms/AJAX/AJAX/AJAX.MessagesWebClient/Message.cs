25d137537e429b230b974247d9d6bd1de3437c00
shakuu <todorov.h@gmail.com>
2017-02-13 14:56:32 +0200

    u

---
+++ WebForms/AJAX/AJAX/AJAX.MessagesWebClient/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AJAX.MessagesWebClient
+{
+    public class Message
+    {
+        public Message()
+        {
+            this.Id = Guid.NewGuid();
+            this.PostedOn = DateTime.UtcNow;
+        }
+
+        public Guid Id { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime PostedOn { get; set; }
+    }
+}
---
+++ WebForms/AJAX/AJAX/AJAX.MessagesWebClient/MessagesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace AJAX.MessagesWebClient
+{
+    public class MessagesDbContext : DbContext
+    {
+        public MessagesDbContext()
+            : base("name=MessagesDb")
+        {
+
+        }
+
+        public virtual DbSet<Message> Messages { get; set; }
+    }
+}
