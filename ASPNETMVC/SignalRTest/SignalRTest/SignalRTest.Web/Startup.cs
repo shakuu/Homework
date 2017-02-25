f1d39fc6b86610a613ee11cef7fe7b232df05b2d
shakuu <todorov.h@gmail.com>
2017-02-25 19:37:59 +0200

    up

---
+++ ASPNETMVC/SignalRTest/SignalRTest/SignalRTest.Web/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRTest.Web.Startup))]
+
+namespace SignalRTest.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
