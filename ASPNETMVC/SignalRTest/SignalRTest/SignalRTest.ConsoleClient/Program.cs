fef59e29a34425f5736a0083f9b733dd2657b025
shakuu <todorov.h@gmail.com>
2017-02-26 14:07:20 +0200

    up

---
+++ ASPNETMVC/SignalRTest/SignalRTest/SignalRTest.ConsoleClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace SignalRTest.ConsoleClient
+{
+    public class Program
+    {
+        private const string Url = "http://localhost:57920";
+
+        static void Main(string[] args)
+        {
+            CreateConnection().Wait();
+
+            while (true)
+            {
+
+            }
+        }
+
+        private static async Task CreateConnection()
+        {
+            var connection = new HubConnection(Program.Url);
+            connection.TraceWriter = Console.Out;
+
+            var proxy = connection.CreateHubProxy("ChatHub");
+
+            proxy.On<string, string>("broadcastMessage", (name, message) =>
+            {
+                Console.WriteLine(name + ": " + message);
+            });
+
+            await connection.Start();
+            await proxy.Invoke("Send", "me", "console");
+        }
+    }
+}
