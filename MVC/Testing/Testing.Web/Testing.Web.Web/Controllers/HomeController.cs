28845232272bca1a410a6bece787849a07c4ace4
shakuu <todorov.h@gmail.com>
2017-03-02 12:39:29 +0200

    up

---
+++ MVC/Testing/Testing.Web/Testing.Web.Web/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Testing.Web.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(IActionFilter filter)
+        {
+
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
