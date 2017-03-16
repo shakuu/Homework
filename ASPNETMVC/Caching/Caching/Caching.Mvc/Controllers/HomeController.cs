a0af0171915fe705eb8e338e13899d6d98785229
shakuu <todorov.h@gmail.com>
2017-03-16 17:43:34 +0200

    up

--- ASPNETMVC/Caching/Caching/Caching.Mvc/Controllers/HomeController.cs
+++ ASPNETMVC/Caching/Caching/Caching.Mvc/Controllers/HomeController.cs
@@ -1,16 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Caching.Mvc.Models;
 
 namespace Caching.Mvc.Controllers
 {
     public class HomeController : Controller
     {
+        private const int CacheDurationInSeconds = 1 * 60 * 60;
+
+        [HttpGet]
+        [OutputCache(Duration = HomeController.CacheDurationInSeconds, VaryByParam = "none")]
         public ActionResult Index()
         {
-            return View();
+            var viewModel = new HomeIndexViewModel();
+            viewModel.LastCacheTimestamp = DateTime.UtcNow;
+
+            return View(viewModel);
+        }
+
+        [ChildActionOnly]
+        [OutputCache(Duration = HomeController.CacheDurationInSeconds, VaryByParam = "none")]
+        public ActionResult Partial()
+        {
+            var viewModel = new HomeIndexViewModel();
+            viewModel.LastCacheTimestamp = DateTime.UtcNow;
+
+            return this.PartialView("_CachedPartial", viewModel);
         }
 
         public ActionResult About()
---
+++ ASPNETMVC/Caching/Caching/Caching.Mvc/Models/HomeIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Caching.Mvc.Models
+{
+    public class HomeIndexViewModel
+    {
+        public DateTime LastCacheTimestamp { get; set; }
+    }
+}
